15a212177ce82b8d5a58127bad4537a6fb834a08
marcelabc <marcela.cunha@liferay.com>
2022-01-06 18:07:13 -0300

    LPS-144868 Create the interface for the ObjectFieldBusinessType

---
+++ modules/apps/object/object-api/src/main/java/com/liferay/object/field/business/type/ObjectFieldBusinessType.java
@@ -0,0 +1,45 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.object.field.business.type;
+
+import com.liferay.dynamic.data.mapping.form.field.type.constants.DDMFormFieldTypeConstants;
+import com.liferay.dynamic.data.mapping.model.DDMFormField;
+
+import java.util.Collections;
+import java.util.Locale;
+import java.util.Map;
+
+/**
+ * @author Marcela Cunha
+ */
+public interface ObjectFieldBusinessType {
+
+	public String getDBType();
+
+	public default DDMFormField getDDMFormField(String name) {
+		return new DDMFormField(name, DDMFormFieldTypeConstants.TEXT);
+	}
+
+	public String getDDMFormFieldTypeName();
+
+	public String getLabel(Locale locale);
+
+	public String getName();
+
+	public default Map<String, Object> getObjectFieldProperties() {
+		return Collections.emptyMap();
+	}
+
+}
