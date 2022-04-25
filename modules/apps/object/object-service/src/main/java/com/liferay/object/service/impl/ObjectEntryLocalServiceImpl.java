18fae040303293d7f75cabeacb02b52ed2db8010
sontruongces <son.truong@codeenginestudio.com>
2022-04-25 14:49:40 +0700

    LPS-149228 Validate null/empty value for required field in object

--- modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectEntryLocalServiceImpl.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectEntryLocalServiceImpl.java
@@ -2293,6 +2293,21 @@ public class ObjectEntryLocalServiceImpl
 			Map<String, Serializable> values)
 		throws PortalException {
 
+		DynamicObjectDefinitionTable dynamicObjectDefinitionTable =
+			_getDynamicObjectDefinitionTable(objectDefinitionId);
+
+		List<ObjectField> objectFields =
+			dynamicObjectDefinitionTable.getObjectFields();
+
+		for (ObjectField objectField : objectFields) {
+			Object value = values.get(objectField.getName());
+
+			if (Validator.isNull(value) && objectField.isRequired()) {
+				throw new ObjectEntryValuesException.Required(
+					objectField.getName());
+			}
+		}
+
 		for (Map.Entry<String, Serializable> entry : values.entrySet()) {
 			_validateValues(
 				defaultUser, entry, objectDefinitionId, portletId,
