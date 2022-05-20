e4045d07a0da11bc155a7b31027c1caa86df2075
marcelabc <marcela.cunha@liferay.com>
2022-05-20 08:32:13 -0300

    LPS-154049 Create a ObjectRelatedModelsProvider to a system object when the relationship is M-M

--- modules/apps/object/object-service/src/main/java/com/liferay/object/internal/instance/lifecycle/SystemObjectDefinitionMetadataPortalInstanceLifecycleListener.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/internal/instance/lifecycle/SystemObjectDefinitionMetadataPortalInstanceLifecycleListener.java
@@ -21,6 +21,7 @@ import com.liferay.object.constants.ObjectSAPConstants;
 import com.liferay.object.internal.item.selector.SystemObjectEntryItemSelectorView;
 import com.liferay.object.internal.related.models.ObjectEntry1toMObjectRelatedModelsProviderImpl;
 import com.liferay.object.internal.related.models.SystemObject1toMObjectRelatedModelsProviderImpl;
+import com.liferay.object.internal.related.models.SystemObjectMtoMObjectRelatedModelsProviderImpl;
 import com.liferay.object.internal.rest.context.path.RESTContextPathResolverImpl;
 import com.liferay.object.model.ObjectDefinition;
 import com.liferay.object.related.models.ObjectRelatedModelsProvider;
@@ -227,6 +228,20 @@ public class SystemObjectDefinitionMetadataPortalInstanceLifecycleListener
 						objectDefinition, _objectEntryLocalService,
 						_objectFieldLocalService,
 						_objectRelationshipLocalService),
+					null);
+			}
+
+			if (GetterUtil.getBoolean(
+					PropsUtil.get("feature.flag.LPS-146754"))) {
+
+				_bundleContext.registerService(
+					ObjectRelatedModelsProvider.class,
+					new SystemObjectMtoMObjectRelatedModelsProviderImpl(
+						objectDefinition, _objectDefinitionLocalService,
+						_objectFieldLocalService,
+						_objectRelationshipLocalService,
+						_persistedModelLocalServiceRegistry,
+						systemObjectDefinitionMetadata),
 					null);
 			}
 
