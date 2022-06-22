1868ee3a4f5e9407e9fd3a3bbdbf0cced196db62
Mateus Santana <mateus.santana@liferay.com>
2022-06-22 17:25:32 -0300

    LPS-155118 Add upgrade step for Metadata fields

---
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/internal/upgrade/v3_15_0/ObjectFieldUpgradeProcess.java
@@ -0,0 +1,135 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.object.internal.upgrade.v3_15_0;
+
+import com.liferay.object.model.ObjectEntryTable;
+import com.liferay.object.service.ObjectFieldLocalService;
+import com.liferay.object.util.LocalizedMapUtil;
+import com.liferay.petra.string.StringBundler;
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.upgrade.UpgradeProcess;
+import com.liferay.portal.kernel.upgrade.util.UpgradeProcessUtil;
+import com.liferay.portal.kernel.util.LocaleUtil;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+/**
+ * @author Mateus Santana
+ */
+public class ObjectFieldUpgradeProcess extends UpgradeProcess {
+
+	public ObjectFieldUpgradeProcess(
+		ObjectFieldLocalService objectFieldLocalService) {
+
+		_objectFieldLocalService = objectFieldLocalService;
+	}
+
+	@Override
+	protected void doUpgrade() throws Exception {
+		try (PreparedStatement preparedStatement = connection.prepareStatement(
+			StringBundler.concat("SELECT ObjectDefinition.companyId, ",
+				"ObjectDefinition.dbTableName, ",
+				"ObjectDefinition.objectDefinitionId, ",
+				"ObjectDefinition.system_, ObjectDefinition.userId FROM ",
+				"ObjectDefinition WHERE ObjectDefinition.objectDefinitionId ",
+				"NOT IN (SELECT DISTINCT ObjectField.objectDefinitionId FROM ",
+				"ObjectField WHERE (ObjectField.name = \"creator\" or ",
+				"ObjectField.name = \"dateCreated\" or ObjectField.name = ",
+				"\"dateModified\" or ObjectField.name = \"id\" or ",
+				"ObjectField.name = \"status\") AND ObjectField.system_ = ",
+				"true)"));
+
+			 ResultSet resultSet = preparedStatement.executeQuery()) {
+
+			while (resultSet.next()) {
+				long companyId = resultSet.getLong("companyId");
+				String dbTableName = resultSet.getString("dbTableName");
+				long objectDefinitionId = resultSet.getLong(
+					"objectDefinitionId");
+				long system = resultSet.getLong("system_");
+				long userId = resultSet.getLong("userId");
+
+				if (system == 0) {
+					dbTableName = "ObjectEntry";
+				}
+
+				_objectFieldLocalService.addSystemObjectField(
+					userId, objectDefinitionId, "Date", null,
+					ObjectEntryTable.INSTANCE.createDate.getName(), dbTableName,
+					"Date", false, false, null,
+					LocalizedMapUtil.getLocalizedMap(
+						LanguageUtil.get(
+							LocaleUtil.fromLanguageId(
+								UpgradeProcessUtil.getDefaultLanguageId(
+									companyId)),
+							"create-date")),
+					"createDate", false);
+
+				_objectFieldLocalService.addSystemObjectField(
+					userId, objectDefinitionId, "Integer", null,
+					ObjectEntryTable.INSTANCE.objectEntryId.getName(),
+					resultSet.getString("dbTableName"), "Integer", false, false,
+					null,
+					LocalizedMapUtil.getLocalizedMap(
+						LanguageUtil.get(
+							LocaleUtil.fromLanguageId(
+								UpgradeProcessUtil.getDefaultLanguageId(
+									companyId)),
+							"id")),
+					"id", false);
+
+				_objectFieldLocalService.addSystemObjectField(
+					userId, objectDefinitionId, "Date", null,
+					ObjectEntryTable.INSTANCE.modifiedDate.getName(),
+					dbTableName, "Date", false, false, null,
+					LocalizedMapUtil.getLocalizedMap(
+						LanguageUtil.get(
+							LocaleUtil.fromLanguageId(
+								UpgradeProcessUtil.getDefaultLanguageId(
+									companyId)),
+							"modified-date")),
+					"modifiedDate", false);
+
+				_objectFieldLocalService.addSystemObjectField(
+					userId, objectDefinitionId, "Text", null,
+					ObjectEntryTable.INSTANCE.status.getName(), dbTableName,
+					"Integer", false, false, null,
+					LocalizedMapUtil.getLocalizedMap(
+						LanguageUtil.get(
+							LocaleUtil.fromLanguageId(
+								UpgradeProcessUtil.getDefaultLanguageId(
+									companyId)),
+							"status")),
+					"status", false);
+
+				_objectFieldLocalService.addSystemObjectField(
+					userId, objectDefinitionId, "Text", null,
+					ObjectEntryTable.INSTANCE.userName.getName(), dbTableName,
+					"String", false, false, null,
+					LocalizedMapUtil.getLocalizedMap(
+						LanguageUtil.get(
+							LocaleUtil.fromLanguageId(
+								UpgradeProcessUtil.getDefaultLanguageId(
+									companyId)),
+							"author")),
+					"userName", false);
+			}
+		}
+	}
+
+	private final ObjectFieldLocalService _objectFieldLocalService;
+
+}
