e7d8938a74f91c42ca666e1a05b243332d56fc1f
marcelabc <marcela.cunha@liferay.com>
2022-06-30 17:44:04 -0300

    LPS-157168 Adapt logic when adding an aggregation field with the filter parameter

--- modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectFieldSettingLocalServiceImpl.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectFieldSettingLocalServiceImpl.java
@@ -14,15 +14,26 @@
 
 package com.liferay.object.service.impl;
 
+import com.liferay.object.constants.ObjectFieldConstants;
+import com.liferay.object.constants.ObjectFilterConstants;
+import com.liferay.object.model.ObjectField;
 import com.liferay.object.model.ObjectFieldSetting;
+import com.liferay.object.model.ObjectFilter;
+import com.liferay.object.service.ObjectFilterLocalService;
 import com.liferay.object.service.base.ObjectFieldSettingLocalServiceBaseImpl;
 import com.liferay.object.service.persistence.ObjectFieldPersistence;
 import com.liferay.portal.aop.AopService;
 import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.json.JSONArray;
+import com.liferay.portal.kernel.json.JSONFactoryUtil;
+import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.service.UserLocalService;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.PropsUtil;
 
 import java.util.List;
+import java.util.Objects;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -38,11 +49,27 @@ public class ObjectFieldSettingLocalServiceImpl
 	extends ObjectFieldSettingLocalServiceBaseImpl {
 
 	@Override
-	public ObjectFieldSetting addObjectFieldSetting(
+	public void addObjectFieldSetting(
 			long userId, long objectFieldId, String name, String value)
 		throws PortalException {
 
-		_objectFieldPersistence.findByPrimaryKey(objectFieldId);
+		ObjectField objectField = _objectFieldPersistence.findByPrimaryKey(
+			objectFieldId);
+
+		if (GetterUtil.getBoolean(PropsUtil.get("feature.flag.LPS-156704")) &&
+			Objects.equals(
+				objectField.getBusinessType(),
+				ObjectFieldConstants.BUSINESS_TYPE_AGGREGATION) &&
+			Objects.equals(name, "filters")) {
+
+			_addFilterObjectFieldSettings(userId, objectFieldId, value);
+
+			return;
+		}
+
+		_addObjectFieldSetting(userId, objectFieldId, name, value);
+	}
+
 
 		ObjectFieldSetting objectFieldSetting =
 			objectFieldSettingPersistence.create(
@@ -87,8 +114,54 @@ public class ObjectFieldSettingLocalServiceImpl
 		return objectFieldSettingPersistence.update(objectFieldSetting);
 	}
 
+	private void _addFilterObjectFieldSettings(
+			long userId, long objectFieldId, String value)
+		throws PortalException {
+
+		JSONArray jsonArray = JSONFactoryUtil.createJSONArray(value);
+
+		for (int i = 0; i < jsonArray.length(); i++) {
+			JSONObject jsonObject = jsonArray.getJSONObject(i);
+
+			ObjectFilter objectFilter =
+				_objectFilterLocalService.addObjectFieldSetting(
+					userId,
+					jsonObject.getString(ObjectFilterConstants.FILTER_BY),
+					jsonObject.getString(ObjectFilterConstants.FILTER_TYPE),
+					jsonObject.getString(ObjectFilterConstants.JSON));
+
+			_addObjectFieldSetting(
+				userId, objectFieldId, "filter",
+				String.valueOf(objectFilter.getObjectFilterId()));
+		}
+	}
+
+	private void _addObjectFieldSetting(
+			long userId, long objectFieldId, String name, String value)
+		throws PortalException {
+
+		ObjectFieldSetting objectFieldSetting =
+			objectFieldSettingPersistence.create(
+				counterLocalService.increment());
+
+		User user = _userLocalService.getUser(userId);
+
+		objectFieldSetting.setCompanyId(user.getCompanyId());
+		objectFieldSetting.setUserId(user.getUserId());
+		objectFieldSetting.setUserName(user.getFullName());
+
+		objectFieldSetting.setObjectFieldId(objectFieldId);
+		objectFieldSetting.setName(name);
+		objectFieldSetting.setValue(value);
+
+		objectFieldSettingPersistence.update(objectFieldSetting);
+	}
+
 	@Reference
 	private ObjectFieldPersistence _objectFieldPersistence;
+
+	@Reference
+	private ObjectFilterLocalService _objectFilterLocalService;
 
 	@Reference
 	private UserLocalService _userLocalService;
