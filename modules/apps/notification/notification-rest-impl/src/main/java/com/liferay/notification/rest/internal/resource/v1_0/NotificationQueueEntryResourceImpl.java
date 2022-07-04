b61d888b8f3226c76b096ee511a485cdfef71053
p-albuquerque <paulo.albuquerque@liferay.com>
2022-07-04 14:29:21 -0300

    LPS-155950 Add feature flag inside PanelApp and Resource

--- modules/apps/notification/notification-rest-impl/src/main/java/com/liferay/notification/rest/internal/resource/v1_0/NotificationQueueEntryResourceImpl.java
+++ modules/apps/notification/notification-rest-impl/src/main/java/com/liferay/notification/rest/internal/resource/v1_0/NotificationQueueEntryResourceImpl.java
@@ -26,6 +26,7 @@ import com.liferay.portal.kernel.security.permission.ActionKeys;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.HashMapBuilder;
 import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.kernel.util.PropsUtil;
 import com.liferay.portal.vulcan.pagination.Page;
 import com.liferay.portal.vulcan.pagination.Pagination;
 import com.liferay.portal.vulcan.util.SearchUtil;
@@ -49,6 +50,8 @@ public class NotificationQueueEntryResourceImpl
 	public void deleteNotificationQueueEntry(Long notificationQueueEntryId)
 		throws Exception {
 
+		_checkFeatureFlag();
+
 		_notificationQueueEntryService.deleteNotificationQueueEntry(
 			notificationQueueEntryId);
 	}
@@ -57,6 +60,8 @@ public class NotificationQueueEntryResourceImpl
 	public Page<NotificationQueueEntry> getNotificationQueueEntriesPage(
 			String search, Filter filter, Pagination pagination, Sort[] sorts)
 		throws Exception {
+
+		_checkFeatureFlag();
 
 		return SearchUtil.search(
 			HashMapBuilder.put(
@@ -96,8 +101,16 @@ public class NotificationQueueEntryResourceImpl
 	public void putNotificationQueueEntryResend(Long notificationQueueEntryId)
 		throws Exception {
 
+		_checkFeatureFlag();
+
 		_notificationQueueEntryService.resendNotificationQueueEntry(
 			notificationQueueEntryId);
+	}
+
+	private void _checkFeatureFlag() throws Exception {
+		if (!GetterUtil.getBoolean(PropsUtil.get("feature.flag.LPS-149051"))) {
+			throw new UnsupportedOperationException();
+		}
 	}
 
 	private NotificationQueueEntry _toNotificationQueueEntry(
