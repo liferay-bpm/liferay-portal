bf6f6e4f29442cab4d74c9c0269bec82620a4596
Mateus Santana <mateus.santana@liferay.com>
2022-08-24 10:22:54 -0300

    LPS-159545 Add Notification Template integration tests

--- modules/apps/notification/notification-test/src/testIntegration/java/com/liferay/notification/service/test/NotificationTemplateLocalServiceTest.java
+++ modules/apps/notification/notification-test/src/testIntegration/java/com/liferay/notification/service/test/NotificationTemplateLocalServiceTest.java
@@ -21,15 +21,41 @@ import com.liferay.notification.model.NotificationQueueEntry;
 import com.liferay.notification.model.NotificationTemplate;
 import com.liferay.notification.service.NotificationQueueEntryLocalService;
 import com.liferay.notification.service.NotificationTemplateLocalService;
+import com.liferay.object.action.util.ObjectActionVariablesUtil;
+import com.liferay.object.constants.ObjectActionExecutorConstants;
+import com.liferay.object.constants.ObjectActionTriggerConstants;
+import com.liferay.object.constants.ObjectDefinitionConstants;
+import com.liferay.object.constants.ObjectFieldConstants;
+import com.liferay.object.model.ObjectDefinition;
+import com.liferay.object.model.ObjectEntry;
+import com.liferay.object.model.ObjectField;
+import com.liferay.object.service.ObjectActionLocalService;
+import com.liferay.object.service.ObjectDefinitionLocalService;
+import com.liferay.object.service.ObjectEntryLocalService;
+import com.liferay.object.service.ObjectFieldLocalService;
+import com.liferay.object.util.LocalizedMapUtil;
+import com.liferay.petra.string.StringBundler;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.dao.orm.QueryUtil;
 import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.json.JSONFactory;
+import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
 import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.kernel.util.HashMapBuilder;
 import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.util.UnicodePropertiesBuilder;
 import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 
+import java.io.Serializable;
+
 import java.util.Collections;
+import java.util.List;
+
+import org.apache.commons.lang.StringUtils;
 
 import org.junit.Assert;
 import org.junit.ClassRule;
@@ -117,6 +143,95 @@ public class NotificationTemplateLocalServiceTest {
 			0, notificationQueueEntry.getNotificationTemplateId());
 	}
 
+	@Test
+	public void testSendNotificationTemplate() throws Exception {
+		ObjectDefinition objectDefinition =
+			_objectDefinitionLocalService.addCustomObjectDefinition(
+				TestPropsValues.getUserId(),
+				LocalizedMapUtil.getLocalizedMap(RandomTestUtil.randomString()),
+				"A" + RandomTestUtil.randomString(), null, null,
+				LocalizedMapUtil.getLocalizedMap(RandomTestUtil.randomString()),
+				ObjectDefinitionConstants.SCOPE_COMPANY,
+				ObjectDefinitionConstants.STORAGE_TYPE_DEFAULT,
+				Collections.emptyList());
+
+		ObjectField objectField = _objectFieldLocalService.addCustomObjectField(
+			TestPropsValues.getUserId(), 0,
+			objectDefinition.getObjectDefinitionId(),
+			ObjectFieldConstants.BUSINESS_TYPE_TEXT,
+			ObjectFieldConstants.DB_TYPE_STRING, null, true, true, null,
+			LocalizedMapUtil.getLocalizedMap(RandomTestUtil.randomString()),
+			"ableField", false, false, Collections.emptyList());
+
+		_objectDefinitionLocalService.publishCustomObjectDefinition(
+			TestPropsValues.getUserId(),
+			objectDefinition.getObjectDefinitionId());
+
+		String objectDefinitionName = objectDefinition.getName();
+
+		String term = StringUtils.upperCase(
+			StringBundler.concat(
+				"[%", objectDefinitionName.substring(2), "_",
+				objectField.getName(), "%]"));
+
+		NotificationTemplate notificationTemplate =
+			_notificationTemplateLocalService.addNotificationTemplate(
+				TestPropsValues.getUserId(),
+				objectDefinition.getObjectDefinitionId(), term,
+				Collections.singletonMap(LocaleUtil.US, term), term, "",
+				term + "@liferay.com",
+				Collections.singletonMap(LocaleUtil.US, term),
+				objectDefinition.getName() + "Template",
+				Collections.singletonMap(LocaleUtil.US, term),
+				Collections.singletonMap(LocaleUtil.US, term + "@liferay.com"),
+				Collections.emptyList());
+
+		_objectActionLocalService.addObjectAction(
+			TestPropsValues.getUserId(),
+			objectDefinition.getObjectDefinitionId(), true, StringPool.BLANK,
+			RandomTestUtil.randomString(), RandomTestUtil.randomString(),
+			ObjectActionExecutorConstants.KEY_WEBHOOK,
+			ObjectActionTriggerConstants.KEY_ON_AFTER_ADD,
+			UnicodePropertiesBuilder.put(
+				"secret", "onafteradd"
+			).put(
+				"url", "https://onafteradd.com"
+			).build());
+
+		ObjectEntry objectEntry = _objectEntryLocalService.addObjectEntry(
+			TestPropsValues.getUserId(), 0,
+			objectDefinition.getObjectDefinitionId(),
+			HashMapBuilder.<String, Serializable>put(
+				"ableField", "ableValue"
+			).build(),
+			ServiceContextTestUtil.getServiceContext());
+
+		_notificationTemplateLocalService.sendNotificationTemplate(
+			TestPropsValues.getUserId(),
+			notificationTemplate.getNotificationTemplateId(),
+			objectDefinition.getClassName(),
+			ObjectActionVariablesUtil.toVariables(
+				null, objectDefinition,
+				_getPayloadJSONObject(objectDefinition, objectEntry), null));
+
+		List<NotificationQueueEntry> notificationQueueEntries =
+			_notificationQueueEntryLocalService.getNotificationQueueEntries(
+				QueryUtil.ALL_POS, QueryUtil.ALL_POS);
+
+		NotificationQueueEntry notificationQueueEntry =
+			notificationQueueEntries.get(1);
+
+		Assert.assertNotNull(notificationQueueEntry);
+
+		Assert.assertEquals(1, notificationQueueEntry.getStatus());
+		Assert.assertEquals("ableValue", notificationQueueEntry.getBcc());
+		Assert.assertEquals("ableValue", notificationQueueEntry.getCc());
+		Assert.assertEquals(
+			"ableValue@liferay.com", notificationQueueEntry.getFrom());
+		Assert.assertEquals(
+			"ableValue@liferay.com", notificationQueueEntry.getTo());
+	}
+
 	private NotificationTemplate _addNotificationTemplate(
 			String name, String from)
 		throws PortalException {
@@ -136,11 +251,51 @@ public class NotificationTemplateLocalServiceTest {
 			Collections.emptyList());
 	}
 
+	private JSONObject _getPayloadJSONObject(
+		ObjectDefinition objectDefinition, ObjectEntry objectEntry) {
+
+		JSONObject jsonObject = _jsonFactory.createJSONObject();
+
+		jsonObject.put(
+			"classPK", objectEntry.getObjectEntryId()
+		).put(
+			"companyId", objectDefinition.getCompanyId()
+		).put(
+			"objectActionTriggerKey", "onAfterAdd"
+		).put(
+			"objectDefinitionId", objectDefinition.getObjectDefinitionId()
+		).put(
+			"objectEntry",
+			HashMapBuilder.putAll(
+				objectEntry.getModelAttributes()
+			).put(
+				"values", objectEntry.getValues()
+			).build()
+		);
+
+		return jsonObject;
+	}
+
+	@Inject
+	private JSONFactory _jsonFactory;
+
 	@Inject
 	private NotificationQueueEntryLocalService
 		_notificationQueueEntryLocalService;
 
 	@Inject
 	private NotificationTemplateLocalService _notificationTemplateLocalService;
+
+	@Inject
+	private ObjectActionLocalService _objectActionLocalService;
+
+	@Inject
+	private ObjectDefinitionLocalService _objectDefinitionLocalService;
+
+	@Inject
+	private ObjectEntryLocalService _objectEntryLocalService;
+
+	@Inject
+	private ObjectFieldLocalService _objectFieldLocalService;
 
 }
