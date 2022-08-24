d74b809df335aaebab653d65ce56fa4fece8a7b6
Jose A Jimenez <jose.jimenez@liferay.com>
2022-08-24 08:28:51 +0200

    LPS-161392 Only skip non-provided values and allow to update provided empty/null field values

--- modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectEntryLocalServiceImpl.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectEntryLocalServiceImpl.java
@@ -2487,7 +2487,7 @@ public class ObjectEntryLocalServiceImpl
 		for (ObjectField objectField : objectFields) {
 			Object value = values.get(objectField.getName());
 
-			if (value == null) {
+			if (!values.containsKey(objectField.getName())) {
 				if (_log.isDebugEnabled()) {
 					_log.debug(
 						"No value was provided for object field \"" +
@@ -2552,7 +2552,7 @@ public class ObjectEntryLocalServiceImpl
 			for (ObjectField objectField : objectFields) {
 				Object value = values.get(objectField.getName());
 
-				if (value == null) {
+				if (!values.containsKey(objectField.getName())) {
 					continue;
 				}
 
