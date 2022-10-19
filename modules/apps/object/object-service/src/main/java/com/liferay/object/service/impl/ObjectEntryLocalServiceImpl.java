063009f1d61a89c922fed8cdde0a2dd8cffe317a
Selton Guedes <sfgs@ecomp.poli.br>
2022-10-19 18:18:27 -0300

    LPS-165571 Get evaluate value as string

--- modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectEntryLocalServiceImpl.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectEntryLocalServiceImpl.java
@@ -1890,7 +1890,7 @@ public class ObjectEntryLocalServiceImpl
 		if (StringUtil.equals(
 				outputType, ObjectFieldConstants.BUSINESS_TYPE_TEXT)) {
 
-			return GetterUtil.getString(value);
+			return value.toString();
 		}
 
 		if (StringUtil.equals(
