f4157e4adf07149f3d738f96b85f0725e04fb2fc
Carolina Barbosa <carolina.barbosa@liferay.com>
2022-10-21 17:52:05 -0300

    LPS-166304 Set externalReferenceCode when creating new object entry

--- modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectEntryLocalServiceImpl.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectEntryLocalServiceImpl.java
@@ -217,6 +217,8 @@ public class ObjectEntryLocalServiceImpl
 
 		ObjectEntry objectEntry = objectEntryPersistence.create(objectEntryId);
 
+		_setExternalReferenceCode(objectEntry, values);
+
 		objectEntry.setGroupId(groupId);
 		objectEntry.setCompanyId(user.getCompanyId());
 		objectEntry.setUserId(user.getUserId());
