08e84baec03838aede2e948548441d0443d51f0d
Mateus Santana <mateus.santana@liferay.com>
2022-10-31 13:58:07 -0300

    LPS-140522 Use ThreadLocal to skip relationship permission

--- modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectEntryLocalServiceImpl.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectEntryLocalServiceImpl.java
@@ -491,9 +491,20 @@ public class ObjectEntryLocalServiceImpl
 						objectDefinition2.getClassName(),
 						objectRelationship.getType());
 
-			objectRelatedModelsProvider.deleteRelatedModel(
-				PrincipalThreadLocal.getUserId(), groupId,
-				objectRelationship.getObjectRelationshipId(), primaryKey);
+			try {
+				ObjectRelationshipUtil.setSkipPermissionWherePredicate(true);
+
+				objectRelatedModelsProvider.deleteRelatedModel(
+					PrincipalThreadLocal.getUserId(), groupId,
+					objectRelationship.getObjectRelationshipId(), primaryKey);
+			}
+			catch (Exception exception) {
+				ObjectRelationshipUtil.setSkipPermissionWherePredicate(false);
+
+				throw new PortalException(exception.getMessage());
+			}
+
+			ObjectRelationshipUtil.setSkipPermissionWherePredicate(false);
 		}
 	}
 
@@ -1944,7 +1955,11 @@ public class ObjectEntryLocalServiceImpl
 					objectDefinitionId2)
 			).and(
 				() -> {
-					if (PermissionThreadLocal.getPermissionChecker() == null) {
+					if (ObjectRelationshipUtil.
+							getSkipPermissionWherePredicate() ||
+						(PermissionThreadLocal.getPermissionChecker() ==
+							null)) {
+
 						return null;
 					}
 
@@ -2117,7 +2132,11 @@ public class ObjectEntryLocalServiceImpl
 				}
 			).and(
 				() -> {
-					if (PermissionThreadLocal.getPermissionChecker() == null) {
+					if (ObjectRelationshipUtil.
+							getSkipPermissionWherePredicate() ||
+						(PermissionThreadLocal.getPermissionChecker() ==
+							null)) {
+
 						return null;
 					}
 
