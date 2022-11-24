ed1179bd913b7da99ea39a8d1d80ad7ba3255eb6
Paulo Albuquerque <paulo.albuquerque@liferay.com>
2022-11-24 12:21:53 -0300

    LPS-168865 buildRest

--- modules/apps/object/object-admin-rest-api/src/main/java/com/liferay/object/admin/rest/resource/v1_0/ObjectFieldResource.java
+++ modules/apps/object/object-admin-rest-api/src/main/java/com/liferay/object/admin/rest/resource/v1_0/ObjectFieldResource.java
@@ -56,6 +56,12 @@ import org.osgi.annotation.versioning.ProviderType;
 @ProviderType
 public interface ObjectFieldResource {
 
+	public Page<ObjectField>
+			getObjectDefinitionByExternalReferenceCodeObjectDefinitionExternalReferenceCodeObjectFieldsPage(
+				String objectDefinitionExternalReferenceCode, String search,
+				Filter filter, Pagination pagination, Sort[] sorts)
+		throws Exception;
+
 	public Page<ObjectField> getObjectDefinitionObjectFieldsPage(
 			Long objectDefinitionId, String search, Filter filter,
 			Pagination pagination, Sort[] sorts)
