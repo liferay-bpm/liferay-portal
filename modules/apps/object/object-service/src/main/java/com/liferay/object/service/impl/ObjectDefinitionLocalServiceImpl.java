df0b2564dc2ca02f9c198e8a578081d97e69de40
Tamyris Torres <tamyris.bernardo@liferay.com>
2022-11-29 15:50:10 -0300

    LPS-168241 - Fix mensage

--- modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectDefinitionLocalServiceImpl.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectDefinitionLocalServiceImpl.java
@@ -1311,8 +1311,8 @@ public class ObjectDefinitionLocalServiceImpl
 			!accountEntryRestricted) {
 
 			throw new ObjectDefinitionAccountEntryRestrictedException(
-				"Account entry restriction cannot be changed when the object " +
-					"definition is published");
+				"Account entry restriction cannot be disabled when the " +
+				"object definition is published");
 		}
 	}
 
