a8eaf0c7d2180d8861e6696d58795598b290563d
Tamyris Torres <tamyris.bernardo@liferay.com>
2022-11-29 15:44:43 -0300

    LPS-168241 - SF

--- modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectDefinitionLocalServiceImpl.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectDefinitionLocalServiceImpl.java
@@ -1307,7 +1307,8 @@ public class ObjectDefinitionLocalServiceImpl
 		}
 
 		if (objectDefinition.isApproved() &&
-			objectDefinition.isAccountEntryRestricted() && !accountEntryRestricted) {
+			objectDefinition.isAccountEntryRestricted() &&
+			!accountEntryRestricted) {
 
 			throw new ObjectDefinitionAccountEntryRestrictedException(
 				"Account entry restriction cannot be changed when the object " +
