b998ed8cf9d0e35dc67b714c334c6a349cd230d0
Murilo Stodolni <murilo.stodolni@liferay.com>
2022-12-20 14:40:35 -0300

    LPS-169369 Validate Notification ObjectAction

--- modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectActionLocalServiceImpl.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectActionLocalServiceImpl.java
@@ -16,6 +16,8 @@ package com.liferay.object.service.impl;
 
 import com.liferay.dynamic.data.mapping.expression.CreateExpressionRequest;
 import com.liferay.dynamic.data.mapping.expression.DDMExpressionFactory;
+import com.liferay.notification.model.NotificationTemplate;
+import com.liferay.notification.service.NotificationTemplateLocalService;
 import com.liferay.object.action.executor.ObjectActionExecutorRegistry;
 import com.liferay.object.constants.ObjectActionConstants;
 import com.liferay.object.constants.ObjectActionExecutorConstants;
@@ -101,7 +103,7 @@ public class ObjectActionLocalServiceImpl
 
 		_validateParametersUnicodeProperties(
 			user.getCompanyId(), conditionExpression, objectActionExecutorKey,
-			objectActionTriggerKey, parametersUnicodeProperties);
+			objectActionTriggerKey, parametersUnicodeProperties, userId);
 
 		ObjectAction objectAction = objectActionPersistence.create(
 			counterLocalService.increment());
@@ -198,7 +200,7 @@ public class ObjectActionLocalServiceImpl
 		_validateParametersUnicodeProperties(
 			objectAction.getCompanyId(), conditionExpression,
 			objectActionExecutorKey, objectActionTriggerKey,
-			parametersUnicodeProperties);
+			parametersUnicodeProperties, objectAction.getUserId());
 
 		objectAction.setActive(active);
 		objectAction.setConditionExpression(conditionExpression);
@@ -359,7 +361,7 @@ public class ObjectActionLocalServiceImpl
 	private void _validateParametersUnicodeProperties(
 			long companyId, String conditionExpression,
 			String objectActionExecutorKey, String objectActionTriggerKey,
-			UnicodeProperties parametersUnicodeProperties)
+			UnicodeProperties parametersUnicodeProperties, long userId)
 		throws PortalException {
 
 		Map<String, Object> errorMessageKeys = new HashMap<>();
@@ -454,6 +456,44 @@ public class ObjectActionLocalServiceImpl
 				catch (ObjectScriptingException objectScriptingException) {
 					errorMessageKeys.put(
 						"script", objectScriptingException.getMessageKey());
+				}
+			}
+		}
+		else if (Objects.equals(
+					objectActionExecutorKey,
+					ObjectActionExecutorConstants.KEY_NOTIFICATION)) {
+
+			String notificationTemplateExternalReferenceCode =
+				GetterUtil.getString(
+					parametersUnicodeProperties.remove(
+						"notificationTemplateExternalReferenceCode"));
+
+			if (Validator.isNotNull(
+					notificationTemplateExternalReferenceCode)) {
+
+				NotificationTemplate existingNotificationTemplate =
+					_notificationTemplateLocalService.
+						fetchNotificationTemplateByExternalReferenceCode(
+							notificationTemplateExternalReferenceCode,
+							companyId);
+
+				String type = GetterUtil.getString(
+					parametersUnicodeProperties.remove("type"));
+
+				if ((existingNotificationTemplate == null) && (type != null)) {
+					existingNotificationTemplate =
+						_notificationTemplateLocalService.
+							addNotificationTemplate(
+								notificationTemplateExternalReferenceCode, type,
+								userId);
+				}
+
+				if (existingNotificationTemplate != null) {
+					parametersUnicodeProperties.put(
+						"notificationTemplateId",
+						String.valueOf(
+							existingNotificationTemplate.
+								getNotificationTemplateId()));
 				}
 			}
 		}
@@ -569,6 +609,9 @@ public class ObjectActionLocalServiceImpl
 
 	@Reference
 	private MessageBus _messageBus;
+
+	@Reference
+	private NotificationTemplateLocalService _notificationTemplateLocalService;
 
 	@Reference
 	private ObjectActionExecutorRegistry _objectActionExecutorRegistry;
