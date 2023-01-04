84a9c1f510dfbdb43368f0aa8ed684f772041508
Diogo Melo <diogo.melo@liferay.com>
2023-01-04 16:19:37 -0300

    LPS-170656 set ERC before objectEntryPersistence update

--- modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectEntryLocalServiceImpl.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectEntryLocalServiceImpl.java
@@ -2952,6 +2952,10 @@ public class ObjectEntryLocalServiceImpl
 				objectEntry.setExternalReferenceCode(externalReferenceCode);
 			}
 		}
+
+		if (Validator.isNull(objectEntry.getExternalReferenceCode())) {
+			objectEntry.setExternalReferenceCode(objectEntry.getUuid());
+		}
 	}
 
 	private void _startWorkflowInstance(
