186a1c4591c5719c8eb8891f95aa9c0e9e16984e
Murilo Stodolni <murilo.stodolni@liferay.com>
2023-01-17 14:30:22 -0300

    LPS-173009 buildService

--- modules/apps/list-type/list-type-api/src/main/java/com/liferay/list/type/service/ListTypeDefinitionServiceUtil.java
+++ modules/apps/list-type/list-type-api/src/main/java/com/liferay/list/type/service/ListTypeDefinitionServiceUtil.java
@@ -63,6 +63,15 @@ public class ListTypeDefinitionServiceUtil {
 		return getService().deleteListTypeDefinition(listTypeDefinitionId);
 	}
 
+	public static ListTypeDefinition
+			fetchListTypeDefinitionByExternalReferenceCode(
+				String externalReferenceCode, long companyId)
+		throws PortalException {
+
+		return getService().fetchListTypeDefinitionByExternalReferenceCode(
+			externalReferenceCode, companyId);
+	}
+
 	public static ListTypeDefinition getListTypeDefinition(
 			long listTypeDefinitionId)
 		throws PortalException {
