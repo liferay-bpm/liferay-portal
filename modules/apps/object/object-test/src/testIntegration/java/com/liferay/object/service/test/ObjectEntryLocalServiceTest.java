7b13f7d4b326d4d98fd9273fd05806aaf4e9f570
Murilo Stodolni <murilo.stodolni@liferay.com>
2023-02-01 13:21:08 -0300

    LPS-173109 Fix tests

--- modules/apps/object/object-test/src/testIntegration/java/com/liferay/object/service/test/ObjectEntryLocalServiceTest.java
+++ modules/apps/object/object-test/src/testIntegration/java/com/liferay/object/service/test/ObjectEntryLocalServiceTest.java
@@ -1259,7 +1259,8 @@ public class ObjectEntryLocalServiceTest {
 
 		ObjectDefinition objectDefinition =
 			_objectDefinitionLocalService.fetchObjectDefinitionByClassName(
-				TestPropsValues.getCompanyId(), User.class.getName());
+				User.class.getName() + StringPool.POUND +
+					TestPropsValues.getCompanyId());
 
 		_objectFieldLocalService.addCustomObjectField(
 			null, TestPropsValues.getUserId(), 0,
