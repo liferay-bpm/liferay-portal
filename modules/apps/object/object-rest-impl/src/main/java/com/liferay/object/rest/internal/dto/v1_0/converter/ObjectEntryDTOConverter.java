3b93e39c2352ecd4789d2c36c4700adeef73aa8c
Carolina Barbosa <carolina.barbosa@liferay.com>
2023-02-14 13:32:15 -0300

    LPS-174435 Get nestedField values from the DTOConverter for System Objects

--- modules/apps/object/object-rest-impl/src/main/java/com/liferay/object/rest/internal/dto/v1_0/converter/ObjectEntryDTOConverter.java
+++ modules/apps/object/object-rest-impl/src/main/java/com/liferay/object/rest/internal/dto/v1_0/converter/ObjectEntryDTOConverter.java
@@ -26,7 +26,6 @@ import com.liferay.object.constants.ObjectFieldSettingConstants;
 import com.liferay.object.constants.ObjectRelationshipConstants;
 import com.liferay.object.entry.util.ObjectEntryValuesUtil;
 import com.liferay.object.field.setting.util.ObjectFieldSettingUtil;
-import com.liferay.object.field.util.ObjectFieldFormulaEvaluatorUtil;
 import com.liferay.object.model.ObjectDefinition;
 import com.liferay.object.model.ObjectField;
 import com.liferay.object.model.ObjectRelationship;
@@ -43,6 +42,9 @@ import com.liferay.object.service.ObjectEntryLocalService;
 import com.liferay.object.service.ObjectFieldLocalService;
 import com.liferay.object.service.ObjectFieldSettingLocalService;
 import com.liferay.object.service.ObjectRelationshipLocalService;
+import com.liferay.object.system.JaxRsApplicationDescriptor;
+import com.liferay.object.system.SystemObjectDefinitionMetadata;
+import com.liferay.object.system.SystemObjectDefinitionMetadataRegistry;
 import com.liferay.petra.function.transform.TransformUtil;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.dao.orm.QueryUtil;
@@ -50,7 +52,9 @@ import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.language.Language;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.model.BaseModel;
 import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.service.GroupLocalService;
 import com.liferay.portal.kernel.service.UserLocalService;
 import com.liferay.portal.kernel.util.GetterUtil;
@@ -62,6 +66,7 @@ import com.liferay.portal.language.LanguageResources;
 import com.liferay.portal.util.PropsValues;
 import com.liferay.portal.vulcan.dto.converter.DTOConverter;
 import com.liferay.portal.vulcan.dto.converter.DTOConverterContext;
+import com.liferay.portal.vulcan.dto.converter.DTOConverterRegistry;
 import com.liferay.portal.vulcan.dto.converter.DefaultDTOConverterContext;
 import com.liferay.portal.vulcan.util.LocalizedMapUtil;
 
@@ -69,11 +74,13 @@ import java.io.Serializable;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 
+import javax.ws.rs.InternalServerErrorException;
 import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.core.UriInfo;
 
@@ -150,13 +157,12 @@ public class ObjectEntryDTOConverter
 				objectRelationship.getObjectDefinitionId1());
 
 		if (objectDefinition.isSystem()) {
-			value = ObjectFieldFormulaEvaluatorUtil.evaluate(
-				_ddmExpressionFactory,
-				_objectFieldLocalService.getObjectFields(
-					objectDefinition.getObjectDefinitionId()),
-				_objectFieldSettingLocalService, _userLocalService,
-				_objectEntryLocalService.getSystemModelAttributes(
-					objectDefinition, primaryKey));
+			value = _toDTO(
+				objectDefinition.getCompanyId(), primaryKey,
+				_systemObjectDefinitionMetadataRegistry.
+					getSystemObjectDefinitionMetadata(
+						objectDefinition.getName()),
+				dtoConverterContext.getUser());
 		}
 		else {
 			value = _toDTO(
@@ -370,6 +376,41 @@ public class ObjectEntryDTOConverter
 				};
 			}
 		};
+	}
+
+	private Object _toDTO(
+			long companyId, long primaryKey,
+			SystemObjectDefinitionMetadata systemObjectDefinitionMetadata,
+			User user)
+		throws Exception {
+
+		BaseModel<?> baseModel =
+			systemObjectDefinitionMetadata.getBaseModelByExternalReferenceCode(
+				systemObjectDefinitionMetadata.getExternalReferenceCode(
+					primaryKey),
+				companyId);
+
+		JaxRsApplicationDescriptor jaxRsApplicationDescriptor =
+			systemObjectDefinitionMetadata.getJaxRsApplicationDescriptor();
+
+		DTOConverter<BaseModel<?>, ?> dtoConverter =
+			(DTOConverter<BaseModel<?>, ?>)
+				_dtoConverterRegistry.getDTOConverter(
+					jaxRsApplicationDescriptor.getApplicationName(),
+					baseModel.getModelClassName(),
+					jaxRsApplicationDescriptor.getVersion());
+
+		if (dtoConverter == null) {
+			throw new InternalServerErrorException(
+				"No DTO converter found for " + baseModel.getModelClassName());
+		}
+
+		DefaultDTOConverterContext defaultDTOConverterContext =
+			new DefaultDTOConverterContext(
+				false, Collections.emptyMap(), _dtoConverterRegistry,
+				baseModel.getPrimaryKeyObj(), user.getLocale(), null, user);
+
+		return dtoConverter.toDTO(defaultDTOConverterContext, baseModel);
 	}
 
 	private ObjectEntry[] _toObjectEntries(
@@ -604,6 +645,9 @@ public class ObjectEntryDTOConverter
 	private DLURLHelper _dlURLHelper;
 
 	@Reference
+	private DTOConverterRegistry _dtoConverterRegistry;
+
+	@Reference
 	private GroupLocalService _groupLocalService;
 
 	@Reference
@@ -632,6 +676,10 @@ public class ObjectEntryDTOConverter
 
 	@Reference
 	private Portal _portal;
+
+	@Reference
+	private SystemObjectDefinitionMetadataRegistry
+		_systemObjectDefinitionMetadataRegistry;
 
 	@Reference
 	private UserLocalService _userLocalService;
