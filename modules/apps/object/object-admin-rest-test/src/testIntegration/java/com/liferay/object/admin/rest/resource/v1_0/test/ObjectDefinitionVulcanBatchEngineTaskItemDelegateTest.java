f349895592c3e0b23fe54e646358ed5f0676f93c
Mateus Santana <mateus.santana@liferay.com>
2023-02-14 19:42:41 -0300

    LPS-174541 Adjust Tests

--- modules/apps/object/object-admin-rest-test/src/testIntegration/java/com/liferay/object/admin/rest/resource/v1_0/test/ObjectDefinitionVulcanBatchEngineTaskItemDelegateTest.java
+++ modules/apps/object/object-admin-rest-test/src/testIntegration/java/com/liferay/object/admin/rest/resource/v1_0/test/ObjectDefinitionVulcanBatchEngineTaskItemDelegateTest.java
@@ -170,6 +170,7 @@ public class ObjectDefinitionVulcanBatchEngineTaskItemDelegateTest {
 
 		return new ObjectDefinition() {
 			{
+				boolean system = RandomTestUtil.randomBoolean();
 				accountEntryRestricted = false;
 				accountEntryRestrictedObjectFieldName = StringUtil.toLowerCase(
 					RandomTestUtil.randomString());
@@ -182,6 +183,7 @@ public class ObjectDefinitionVulcanBatchEngineTaskItemDelegateTest {
 					RandomTestUtil.randomString());
 				id = RandomTestUtil.randomLong();
 				label = Collections.singletonMap("en_US", "O" + sanitizedName);
+				modifiable = !system;
 				name = "O" + sanitizedName;
 				objectFields = new ObjectField[] {_createObjectField()};
 				panelAppOrder = StringUtil.toLowerCase(
@@ -206,7 +208,7 @@ public class ObjectDefinitionVulcanBatchEngineTaskItemDelegateTest {
 					storageType = StringPool.BLANK;
 				}
 
-				system = RandomTestUtil.randomBoolean();
+				this.system = system;
 				titleObjectFieldName = StringUtil.toLowerCase(
 					RandomTestUtil.randomString());
 			}
