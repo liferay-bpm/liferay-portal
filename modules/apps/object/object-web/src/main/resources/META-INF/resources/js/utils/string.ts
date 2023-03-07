ba74c34fc0def9acf163aae12472a482bd918cb6
Lucas Miranda <lucas.miranda@liferay.com>
2023-03-07 18:44:06 -0300

    LPS-176117 Removing validation firstLetterLowercase and firstLetterUpperCase

--- modules/apps/object/object-web/src/main/resources/META-INF/resources/js/utils/string.ts
+++ modules/apps/object/object-web/src/main/resources/META-INF/resources/js/utils/string.ts
@@ -13,20 +13,6 @@
  */
 
 /**
- * Transform first letter in lowercase
- */
-export function firstLetterLowercase(str: string): string {
-	return str.charAt(0).toLowerCase() + str.slice(1);
-}
-
-/**
- * Transform first letter in uppercase
- */
-export function firstLetterUppercase(str: string): string {
-	return str.charAt(0).toUpperCase() + str.slice(1);
-}
-
-/**
  *
  * Check if the first letter of a string is uppercase
  */
@@ -80,14 +66,12 @@ export function toCamelCase(
 	removeSpecialCharacters?: boolean
 ): string {
 	const split = str.split(' ');
-	const capitalizeFirstLetters = split.map((str: string) =>
-		firstLetterUppercase(str)
-	);
+	const capitalizeFirstLetters = split.map((str: string) => str);
 	const join = capitalizeFirstLetters.join('');
 
 	if (removeSpecialCharacters) {
-		return firstLetterLowercase(removeAllSpecialCharacters(join));
+		return removeAllSpecialCharacters(join);
 	}
 
-	return firstLetterLowercase(join);
+	return join;
 }
