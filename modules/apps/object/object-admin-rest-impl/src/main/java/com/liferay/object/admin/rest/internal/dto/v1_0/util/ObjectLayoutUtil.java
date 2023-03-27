c155fdab33b01f8e3d97d847e109a42cd4b744ed
Mateus Santana <mateus.santana@liferay.com>
2023-03-27 12:19:00 -0300

    LPS-179976 Add Object Layout during Object Definition creation or update

--- modules/apps/object/object-admin-rest-impl/src/main/java/com/liferay/object/admin/rest/internal/dto/v1_0/util/ObjectLayoutUtil.java
+++ modules/apps/object/object-admin-rest-impl/src/main/java/com/liferay/object/admin/rest/internal/dto/v1_0/util/ObjectLayoutUtil.java
@@ -23,8 +23,14 @@ import com.liferay.object.model.ObjectDefinition;
 import com.liferay.object.model.ObjectField;
 import com.liferay.object.service.ObjectDefinitionLocalService;
 import com.liferay.object.service.ObjectFieldLocalService;
+import com.liferay.object.service.ObjectLayoutService;
+import com.liferay.object.service.persistence.ObjectLayoutBoxPersistence;
+import com.liferay.object.service.persistence.ObjectLayoutColumnPersistence;
+import com.liferay.object.service.persistence.ObjectLayoutRowPersistence;
+import com.liferay.object.service.persistence.ObjectLayoutTabPersistence;
 import com.liferay.petra.function.transform.TransformUtil;
 import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.vulcan.util.LocalizedMapUtil;
 
 import java.util.Map;
@@ -100,6 +106,29 @@ public class ObjectLayoutUtil {
 		};
 	}
 
+	public static void toServiceBuilderObjectLayout(
+			long objectDefinitionId, ObjectLayout objectLayout,
+			ObjectLayoutService objectLayoutService,
+			ObjectLayoutBoxPersistence objectLayoutBoxPersistence,
+			ObjectLayoutColumnPersistence objectLayoutColumnPersistence,
+			ObjectFieldLocalService objectFieldLocalService,
+			ObjectLayoutRowPersistence objectLayoutRowPersistence,
+			ObjectLayoutTabPersistence objectLayoutTabPersistence)
+		throws PortalException {
+
+		objectLayoutService.addObjectLayout(
+			objectDefinitionId,
+			GetterUtil.getBoolean(objectLayout.getDefaultObjectLayout()),
+			LocalizedMapUtil.getLocalizedMap(objectLayout.getName()),
+			TransformUtil.transformToList(
+				objectLayout.getObjectLayoutTabs(),
+				objectLayoutTab -> _toServiceBuilderObjectLayoutTab(
+					objectDefinitionId, objectLayoutTab,
+					objectLayoutBoxPersistence, objectLayoutColumnPersistence,
+					objectFieldLocalService, objectLayoutRowPersistence,
+					objectLayoutTabPersistence)));
+	}
+
 	private static ObjectLayoutBox _toObjectLayoutBox(
 		ObjectFieldLocalService objectFieldLocalService,
 		com.liferay.object.model.ObjectLayoutBox objectLayoutBox) {
@@ -167,6 +196,111 @@ public class ObjectLayoutUtil {
 				priority = serviceBuilderObjectLayoutRow.getPriority();
 			}
 		};
+	}
+
+	private static com.liferay.object.model.ObjectLayoutBox
+		_toServiceBuilderObjectLayoutBox(
+			long objectDefinitionId, ObjectLayoutBox objectLayoutBox,
+			ObjectLayoutBoxPersistence objectLayoutBoxPersistence,
+			ObjectLayoutColumnPersistence objectLayoutColumnPersistence,
+			ObjectFieldLocalService objectFieldLocalService,
+			ObjectLayoutRowPersistence objectLayoutRowPersistence) {
+
+		com.liferay.object.model.ObjectLayoutBox serviceBuilderObjectLayoutBox =
+			objectLayoutBoxPersistence.create(0L);
+
+		serviceBuilderObjectLayoutBox.setCollapsable(
+			objectLayoutBox.getCollapsable());
+		serviceBuilderObjectLayoutBox.setNameMap(
+			LocalizedMapUtil.getLocalizedMap(objectLayoutBox.getName()));
+		serviceBuilderObjectLayoutBox.setObjectLayoutRows(
+			TransformUtil.transformToList(
+				objectLayoutBox.getObjectLayoutRows(),
+				objectLayoutRow -> _toServiceBuilderObjectLayoutRow(
+					objectDefinitionId, objectLayoutRow,
+					objectLayoutColumnPersistence, objectFieldLocalService,
+					objectLayoutRowPersistence)));
+		serviceBuilderObjectLayoutBox.setPriority(
+			objectLayoutBox.getPriority());
+		serviceBuilderObjectLayoutBox.setType(
+			objectLayoutBox.getTypeAsString());
+
+		return serviceBuilderObjectLayoutBox;
+	}
+
+	private static com.liferay.object.model.ObjectLayoutColumn
+		_toServiceBuilderObjectLayoutColumn(
+			long objectDefinitionId, ObjectLayoutColumn objectLayoutColumn,
+			ObjectFieldLocalService objectFieldLocalService,
+			ObjectLayoutColumnPersistence objectLayoutColumnPersistence) {
+
+		com.liferay.object.model.ObjectLayoutColumn
+			serviceBuilderObjectLayoutColumn =
+				objectLayoutColumnPersistence.create(0L);
+
+		ObjectField objectField = objectFieldLocalService.fetchObjectField(
+			objectDefinitionId, objectLayoutColumn.getObjectFieldName());
+
+		serviceBuilderObjectLayoutColumn.setObjectFieldId(
+			objectField.getObjectFieldId());
+
+		serviceBuilderObjectLayoutColumn.setPriority(
+			objectLayoutColumn.getPriority());
+		serviceBuilderObjectLayoutColumn.setSize(
+			GetterUtil.getInteger(objectLayoutColumn.getSize(), 12));
+
+		return serviceBuilderObjectLayoutColumn;
+	}
+
+	private static com.liferay.object.model.ObjectLayoutRow
+		_toServiceBuilderObjectLayoutRow(
+			long objectDefinitionId, ObjectLayoutRow objectLayoutRow,
+			ObjectLayoutColumnPersistence objectLayoutColumnPersistence,
+			ObjectFieldLocalService objectFieldLocalService,
+			ObjectLayoutRowPersistence objectLayoutRowPersistence) {
+
+		com.liferay.object.model.ObjectLayoutRow serviceBuilderObjectLayoutRow =
+			objectLayoutRowPersistence.create(0L);
+
+		serviceBuilderObjectLayoutRow.setObjectLayoutColumns(
+			TransformUtil.transformToList(
+				objectLayoutRow.getObjectLayoutColumns(),
+				objectLayoutColumn -> _toServiceBuilderObjectLayoutColumn(
+					objectDefinitionId, objectLayoutColumn,
+					objectFieldLocalService, objectLayoutColumnPersistence)));
+		serviceBuilderObjectLayoutRow.setPriority(
+			objectLayoutRow.getPriority());
+
+		return serviceBuilderObjectLayoutRow;
+	}
+
+	private static com.liferay.object.model.ObjectLayoutTab
+		_toServiceBuilderObjectLayoutTab(
+			long objectDefinitionId, ObjectLayoutTab objectLayoutTab,
+			ObjectLayoutBoxPersistence objectLayoutBoxPersistence,
+			ObjectLayoutColumnPersistence objectLayoutColumnPersistence,
+			ObjectFieldLocalService objectFieldLocalService,
+			ObjectLayoutRowPersistence objectLayoutRowPersistence,
+			ObjectLayoutTabPersistence objectLayoutTabPersistence) {
+
+		com.liferay.object.model.ObjectLayoutTab serviceBuilderObjectLayoutTab =
+			objectLayoutTabPersistence.create(0L);
+
+		serviceBuilderObjectLayoutTab.setNameMap(
+			LocalizedMapUtil.getLocalizedMap(objectLayoutTab.getName()));
+		serviceBuilderObjectLayoutTab.setObjectLayoutBoxes(
+			TransformUtil.transformToList(
+				objectLayoutTab.getObjectLayoutBoxes(),
+				objectLayoutBox -> _toServiceBuilderObjectLayoutBox(
+					objectDefinitionId, objectLayoutBox,
+					objectLayoutBoxPersistence, objectLayoutColumnPersistence,
+					objectFieldLocalService, objectLayoutRowPersistence)));
+		serviceBuilderObjectLayoutTab.setObjectRelationshipId(
+			GetterUtil.getLong(objectLayoutTab.getObjectRelationshipId()));
+		serviceBuilderObjectLayoutTab.setPriority(
+			objectLayoutTab.getPriority());
+
+		return serviceBuilderObjectLayoutTab;
 	}
 
 }
