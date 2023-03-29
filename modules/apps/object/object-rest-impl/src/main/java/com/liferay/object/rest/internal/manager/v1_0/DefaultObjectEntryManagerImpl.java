c0df053b433b0978d6613788a73070e421971eef
Englert Dániel <daniel.englert@liferay.com>
2023-03-29 13:51:55 +0200

    LPS-178473 Add getObjectRelationshipObjectDefinition1 and getSystemBaseModel methods and their implementations to interface and related classes

--- modules/apps/object/object-rest-impl/src/main/java/com/liferay/object/rest/internal/manager/v1_0/DefaultObjectEntryManagerImpl.java
+++ modules/apps/object/object-rest-impl/src/main/java/com/liferay/object/rest/internal/manager/v1_0/DefaultObjectEntryManagerImpl.java
@@ -602,6 +602,23 @@ public class DefaultObjectEntryManagerImpl
 	}
 
 	@Override
+	public ObjectDefinition getObjectRelationshipObjectDefinition1(
+			ObjectDefinition objectDefinition1, String objectField2Name)
+		throws Exception {
+
+		ObjectField objectField = _objectFieldLocalService.getObjectField(
+			objectDefinition1.getObjectDefinitionId(), objectField2Name);
+
+		ObjectRelationship objectRelationship =
+			_objectRelationshipLocalService.
+				fetchObjectRelationshipByObjectFieldId2(
+					objectField.getObjectFieldId());
+
+		return _objectDefinitionLocalService.getObjectDefinition(
+			objectRelationship.getObjectDefinitionId1());
+	}
+
+	@Override
 	public Page<Object> getRelatedSystemObjectEntries(
 			ObjectDefinition objectDefinition, Long objectEntryId,
 			String objectRelationshipName, Pagination pagination)
@@ -643,6 +660,32 @@ public class DefaultObjectEntryManagerImpl
 				serviceBuilderObjectEntry.getGroupId(),
 				objectRelationship.getObjectRelationshipId(),
 				serviceBuilderObjectEntry.getPrimaryKey()));
+	}
+
+	@Override
+	public Object getSystemBaseModel(
+			DTOConverterContext dtoConverterContext,
+			ObjectDefinition objectDefinition, long primaryKey)
+		throws Exception {
+
+		if (FeatureFlagManagerUtil.isEnabled("LPS-172094")) {
+			SystemObjectDefinitionMetadata systemObjectDefinitionMetadata =
+				_systemObjectDefinitionMetadataRegistry.
+					getSystemObjectDefinitionMetadata(
+						objectDefinition.getName());
+
+			return DTOConverterUtil.toDTO(
+				systemObjectDefinitionMetadata.
+					getBaseModelByExternalReferenceCode(
+						systemObjectDefinitionMetadata.getExternalReferenceCode(
+							primaryKey),
+						objectDefinition.getCompanyId()),
+				_dtoConverterRegistry, systemObjectDefinitionMetadata,
+				dtoConverterContext.getUser());
+		}
+
+		return _objectEntryLocalService.getSystemModelAttributes(
+			objectDefinition, primaryKey);
 	}
 
 	@Override
