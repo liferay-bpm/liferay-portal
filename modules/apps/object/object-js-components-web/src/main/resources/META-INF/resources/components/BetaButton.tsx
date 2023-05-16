df2f5cb63371cc3c3404f4f11a15af944c959359
Murilo Stodolni <murilo.stodolni@liferay.com>
2023-05-16 09:52:35 -0300

    LPS-184539 Create BetaButton

---
+++ modules/apps/object/object-js-components-web/src/main/resources/META-INF/resources/components/BetaButton.tsx
@@ -0,0 +1,64 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+import ClayButton from '@clayui/button';
+import ClayIcon from '@clayui/icon';
+import ClayPopover from '@clayui/popover';
+import {ClayTooltipProvider} from '@clayui/tooltip';
+import React from 'react';
+
+interface BetaButtonProps extends React.HTMLAttributes<HTMLDivElement> {
+	show: boolean;
+}
+
+export function BetaButton({show}: BetaButtonProps) {
+	return (
+		<ClayTooltipProvider>
+			<div
+				className="tooltip-container"
+				data-tooltip-align="top"
+				title={Liferay.Language.get('open-beta-definition')}
+			>
+				<ClayPopover
+					alignPosition="top"
+					disableScroll
+					header={Liferay.Language.get('beta-feature')}
+					show={show}
+					trigger={
+						<ClayButton
+							className="rounded-circle"
+							displayType="beta"
+							hidden={!show}
+							size="xs"
+						>
+							<span className="inline-item">Beta</span>
+
+							<span className="inline-item inline-item-after">
+								<ClayIcon symbol="info-panel-open" />
+							</span>
+						</ClayButton>
+					}
+				>
+					{Liferay.Language.get('this-feature-is-in-testing')}
+
+					<a className="alert-link" href="#" target="_blank">
+						{Liferay.Language.get(
+							'learn-more-about-proxy-objects-beta-feature'
+						)}
+					</a>
+				</ClayPopover>
+			</div>
+		</ClayTooltipProvider>
+	);
+}
