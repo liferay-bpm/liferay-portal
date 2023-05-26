5252b1b4f6582c6fb2aabba5a42764e3c715f10b
Nilton Vieira <nilton.vieira@liferay.com>
2023-05-26 10:05:04 -0300

    LPS-183396 rename

--- modules/apps/site-initializer/site-initializer-extender/site-initializer-extender/src/main/java/com/liferay/site/initializer/extender/internal/BundleSiteInitializer.java
+++ modules/apps/site-initializer/site-initializer-extender/site-initializer-extender/src/main/java/com/liferay/site/initializer/extender/internal/BundleSiteInitializer.java
@@ -544,8 +544,6 @@ public class BundleSiteInitializer implements SiteInitializer {
 							serviceContext,
 							siteNavigationMenuItemSettingsBuilder));
 
-
-
 			_invoke(
 				() -> _addOrUpdateNotificationTemplates(
 					documentsStringUtilReplaceValues,
@@ -1381,7 +1379,9 @@ public class BundleSiteInitializer implements SiteInitializer {
 				listTypeDefinitionIdsStringUtilReplaceValues,
 				objectDefinitionIdsStringUtilReplaceValues, serviceContext));
 
-		_invoke(() -> _enableObjectDefinitionAccountRestriction(serviceContext));
+		_invoke(
+			() -> _enableObjectDefinitionAccountEntryRestriction(
+				serviceContext));
 
 		Map<String, String> objectEntryIdsStringUtilReplaceValues = _invoke(
 			() -> _addOrUpdateObjectEntries(
@@ -4475,13 +4475,12 @@ public class BundleSiteInitializer implements SiteInitializer {
 		}
 	}
 
-	private void _enableObjectDefinitionAccountRestriction(
-		ServiceContext serviceContext)
+	private void _enableObjectDefinitionAccountEntryRestriction(
+			ServiceContext serviceContext)
 		throws Exception {
 
 		String json = SiteInitializerUtil.read(
-			"/site-initializer/" +
-			"object-definition-restrict-account-entry.json",
+			"/site-initializer/object-definition-restrict-account-entry.json",
 			_servletContext);
 
 		if (json == null) {
@@ -4495,17 +4494,19 @@ public class BundleSiteInitializer implements SiteInitializer {
 
 			com.liferay.object.model.ObjectDefinition
 				serviceBuildObjectDefinition =
-				_objectDefinitionLocalService.fetchObjectDefinition(
-					serviceContext.getCompanyId(), "C_" +
-					jsonObject.getString("objectDefinitionName"));
+					_objectDefinitionLocalService.fetchObjectDefinition(
+						serviceContext.getCompanyId(),
+						"C_" + jsonObject.getString("objectDefinitionName"));
 
-			if(Objects.equals(serviceBuildObjectDefinition.getStorageType(),
-				ObjectDefinitionConstants.STORAGE_TYPE_DEFAULT)) {
+			if (Objects.equals(
+					serviceBuildObjectDefinition.getStorageType(),
+					ObjectDefinitionConstants.STORAGE_TYPE_DEFAULT)) {
 
 				com.liferay.object.model.ObjectRelationship objectRelationship =
 					_objectRelationshipLocalService.
 						fetchObjectRelationshipByObjectDefinitionId(
-							serviceBuildObjectDefinition.getObjectDefinitionId(),
+							serviceBuildObjectDefinition.
+								getObjectDefinitionId(),
 							jsonObject.getString("objectRelationshipName"));
 
 				_objectDefinitionLocalService.enableAccountEntryRestricted(
@@ -4517,8 +4518,9 @@ public class BundleSiteInitializer implements SiteInitializer {
 						serviceBuildObjectDefinition.getObjectDefinitionId(),
 						jsonObject.getString("objectField"));
 
-				_objectDefinitionLocalService.enableSalesForceAccountEntryRestricted(
-					serviceBuildObjectField);
+				_objectDefinitionLocalService.
+					enableSalesForceAccountEntryRestricted(
+						serviceBuildObjectField);
 			}
 		}
 	}
