ca344de330a092b347a8b75891d9bcc776b865f5
Selton Guedes <selton.guedes@liferay.com>
2023-05-26 15:48:20 -0300

    LPS-185721 Localized object fields can not be required

--- modules/apps/object/object-web/src/main/resources/META-INF/resources/js/components/ObjectField/ObjectFieldFormBase.tsx
+++ modules/apps/object/object-web/src/main/resources/META-INF/resources/js/components/ObjectField/ObjectFieldFormBase.tsx
@@ -257,7 +257,7 @@ export default function ObjectFieldFormBase({
 			return true;
 		}
 
-		return disabled || values.state;
+		return disabled || values.state || (Liferay.FeatureFlags['LPS-172017'] && values.localized);
 	};
 
 	useEffect(() => {
