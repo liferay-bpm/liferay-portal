7ec09fe6df0ee011346fe9873437ec42e9524376
marcelabc <marcela.cunha@liferay.com>
2023-05-30 14:10:36 -0300

    LPS-181610 Fix condition

--- modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectEntryLocalServiceImpl.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectEntryLocalServiceImpl.java
@@ -3744,9 +3744,7 @@ public class ObjectEntryLocalServiceImpl
 				}
 			}
 
-			if ((listTypeEntry == null) &&
-				(!value.isEmpty() || objectField.isRequired())) {
-
+			if ((listTypeEntry == null) && objectField.isRequired()) {
 				throw new ObjectEntryValuesException.ListTypeEntry(
 					entry.getKey());
 			}
