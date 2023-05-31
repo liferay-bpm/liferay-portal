e577f2e8560fcc0c1e6a2fb9db98bbeac2310e68
Daniel Bonasser <danielbonasser@gmail.com>
2023-05-31 12:05:26 -0300

    LPS-186206 Do not use a specific type extended from a generic type in the Autocomplete component

--- modules/apps/object/object-web/src/main/resources/META-INF/resources/js/components/Layout/LayoutScreen/ModalAddObjectLayoutField.tsx
+++ modules/apps/object/object-web/src/main/resources/META-INF/resources/js/components/Layout/LayoutScreen/ModalAddObjectLayoutField.tsx
@@ -186,15 +186,15 @@ export default function ModalAddObjectLayoutField({
 									)}
 							</>
 						}
-						creationLanguageId={
-							selectedObjectField?.indexedLanguageId as Liferay.Language.Locale
-						}
 						emptyStateMessage={Liferay.Language.get(
 							'there-are-no-fields-for-this-object'
 						)}
 						error={errors.objectFieldName}
 						items={filteredObjectFields}
 						label={Liferay.Language.get('field')}
+						onActive={(item) =>
+							item.name === selectedObjectField?.name
+						}
 						onChangeQuery={setQuery}
 						onSelectItem={(item: ObjectField) => {
 							const readOnlySetting = item.objectFieldSettings?.find(
