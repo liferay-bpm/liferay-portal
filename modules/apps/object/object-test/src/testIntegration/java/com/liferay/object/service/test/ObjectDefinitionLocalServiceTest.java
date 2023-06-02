f3d48acd0947b3e80dd64fa91de87a2ecd1bdf07
Nilton Vieira <nilton.vieira@liferay.com>
2023-06-02 17:02:29 -0300

    LPS-184830 use ObjectFieldUtil

--- modules/apps/object/object-test/src/testIntegration/java/com/liferay/object/service/test/ObjectDefinitionLocalServiceTest.java
+++ modules/apps/object/object-test/src/testIntegration/java/com/liferay/object/service/test/ObjectDefinitionLocalServiceTest.java
@@ -1142,14 +1142,19 @@ public class ObjectDefinitionLocalServiceTest {
 				ObjectDefinitionConstants.STORAGE_TYPE_SALESFORCE,
 				Collections.emptyList());
 
-		ObjectField objectField1 =
-			_objectFieldLocalService.addCustomObjectField(
-				null, TestPropsValues.getUserId(), 0,
-				objectDefinition1.getObjectDefinitionId(),
-				ObjectFieldConstants.BUSINESS_TYPE_TEXT,
-				ObjectFieldConstants.DB_TYPE_STRING, false, false, null,
-				LocalizedMapUtil.getLocalizedMap("Charlie"), false, "charlie",
-				true, false, Collections.emptyList());
+		ObjectField objectField1 = ObjectFieldUtil.addCustomObjectField(
+			new TextObjectFieldBuilder(
+			).userId(
+				TestPropsValues.getUserId()
+			).labelMap(
+				LocalizedMapUtil.getLocalizedMap(RandomTestUtil.randomString())
+			).name(
+				StringUtil.randomId()
+			).objectDefinitionId(
+				objectDefinition1.getObjectDefinitionId()
+			).required(
+				true
+			).build());
 
 		objectDefinition1 =
 			_objectDefinitionLocalService.
@@ -1174,14 +1179,19 @@ public class ObjectDefinitionLocalServiceTest {
 			_objectDefinitionLocalService.addObjectDefinition(
 				RandomTestUtil.randomString(), user.getUserId());
 
-		ObjectField objectField2 =
-			_objectFieldLocalService.addCustomObjectField(
-				null, TestPropsValues.getUserId(), 0,
-				objectDefinition2.getObjectDefinitionId(),
-				ObjectFieldConstants.BUSINESS_TYPE_TEXT,
-				ObjectFieldConstants.DB_TYPE_STRING, false, false, null,
-				LocalizedMapUtil.getLocalizedMap("Charlie"), false, "charlie",
-				true, false, Collections.emptyList());
+		ObjectField objectField2 = ObjectFieldUtil.addCustomObjectField(
+			new TextObjectFieldBuilder(
+			).userId(
+				TestPropsValues.getUserId()
+			).labelMap(
+				LocalizedMapUtil.getLocalizedMap(RandomTestUtil.randomString())
+			).name(
+				StringUtil.randomId()
+			).objectDefinitionId(
+				objectDefinition2.getObjectDefinitionId()
+			).required(
+				true
+			).build());
 
 		objectDefinition2 =
 			_objectDefinitionLocalService.
@@ -1200,14 +1210,19 @@ public class ObjectDefinitionLocalServiceTest {
 				ObjectDefinitionConstants.STORAGE_TYPE_SALESFORCE,
 				Collections.emptyList());
 
-		ObjectField objectField3 =
-			_objectFieldLocalService.addCustomObjectField(
-				null, TestPropsValues.getUserId(), 0,
-				objectDefinition3.getObjectDefinitionId(),
-				ObjectFieldConstants.BUSINESS_TYPE_DATE,
-				ObjectFieldConstants.DB_TYPE_DATE, false, false, null,
-				LocalizedMapUtil.getLocalizedMap("Charlie"), false, "charlie",
-				true, false, Collections.emptyList());
+		ObjectField objectField3 = ObjectFieldUtil.addCustomObjectField(
+			new DateObjectFieldBuilder(
+			).userId(
+				TestPropsValues.getUserId()
+			).labelMap(
+				LocalizedMapUtil.getLocalizedMap(RandomTestUtil.randomString())
+			).name(
+				StringUtil.randomId()
+			).objectDefinitionId(
+				objectDefinition3.getObjectDefinitionId()
+			).required(
+				true
+			).build());
 
 		try {
 			objectDefinition3 =
