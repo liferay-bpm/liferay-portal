f91a887317cc80f7a0086801b5267a96680ec6eb
Nilton Vieira <nilton.vieira@liferay.com>
2023-06-06 15:17:29 -0300

    LPS-184830 allow switching restricted fields

--- modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectDefinitionLocalServiceImpl.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectDefinitionLocalServiceImpl.java
@@ -544,9 +544,7 @@ public class ObjectDefinitionLocalServiceImpl
 		ObjectDefinition objectDefinition = getObjectDefinition(
 			objectField.getObjectDefinitionId());
 
-		if (objectDefinition.isDefaultStorageType() ||
-			objectDefinition.isAccountEntryRestricted()) {
-
+		if (objectDefinition.isDefaultStorageType()) {
 			return objectDefinition;
 		}
 
