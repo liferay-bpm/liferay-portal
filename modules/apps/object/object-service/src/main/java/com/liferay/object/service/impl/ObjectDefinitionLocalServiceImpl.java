f8d9e172e04ddacac6e54fd4afceaf5ba0e176dd
Nilton Vieira <nilton.vieira@liferay.com>
2023-06-16 09:40:33 -0300

    LPS-184830 sort

--- modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectDefinitionLocalServiceImpl.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectDefinitionLocalServiceImpl.java
@@ -549,10 +549,10 @@ public class ObjectDefinitionLocalServiceImpl
 			throw new UnsupportedOperationException();
 		}
 
+		objectDefinition.setAccountEntryRestricted(true);
+
 		objectDefinition.setAccountEntryRestrictedObjectFieldId(
 			objectField.getObjectFieldId());
-
-		objectDefinition.setAccountEntryRestricted(true);
 
 		return objectDefinitionPersistence.update(objectDefinition);
 	}
