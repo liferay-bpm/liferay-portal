01aa3d2dcade8ee3f0631d7cc546230b6e7c27c6
José Abelenda <jose.abelenda@liferay.com>
2023-06-22 10:55:09 -0300

    LPS-188430 Upgrade step

--- modules/apps/object/object-service/src/main/java/com/liferay/object/internal/upgrade/registry/ObjectServiceUpgradeStepRegistrator.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/internal/upgrade/registry/ObjectServiceUpgradeStepRegistrator.java
@@ -312,7 +312,10 @@ public class ObjectServiceUpgradeStepRegistrator
 
 				@Override
 				protected String[][] getTableAndPrimaryKeyColumnNames() {
-					return new String[][] {{"ObjectView", "objectViewId"}};
+					return new String[][] {
+						{"ObjectLayout", "objectLayoutId"},
+						{"ObjectView", "objectViewId"}
+					};
 				}
 
 			});
