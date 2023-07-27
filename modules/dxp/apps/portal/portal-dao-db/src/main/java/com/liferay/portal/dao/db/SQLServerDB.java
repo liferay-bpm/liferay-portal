46ea2d854595a434375318c8ddfd5b19b919be9e
Pedro Tavares <pedro.tavares@liferay.com>
2023-07-27 13:44:49 -0300

    LPS-191310 Call runSQL method that takes a connection reference. Because the method without it would not complete the query

--- modules/dxp/apps/portal/portal-dao-db/src/main/java/com/liferay/portal/dao/db/SQLServerDB.java
+++ modules/dxp/apps/portal/portal-dao-db/src/main/java/com/liferay/portal/dao/db/SQLServerDB.java
@@ -494,6 +494,7 @@ public class SQLServerDB extends BaseDB {
 		}
 
 		runSQL(
+			connection,
 			StringBundler.concat(
 				"alter table ", tableName, " drop constraint ",
 				defaultConstraintName));
