192468bef17ef0abd25ee3515dfbd2aa428cdb80
kildarep <kildare.porciuncula@liferay.com>
2023-07-28 12:55:31 -0300

    LPS-190754 Adding import icon

--- modules/apps/object/object-web/src/main/java/com/liferay/object/web/internal/object/definitions/portlet/configuration/icon/ImportObjectDefinitionPortletConfigurationIcon.java
+++ modules/apps/object/object-web/src/main/java/com/liferay/object/web/internal/object/definitions/portlet/configuration/icon/ImportObjectDefinitionPortletConfigurationIcon.java
@@ -49,6 +49,11 @@ public class ImportObjectDefinitionPortletConfigurationIcon
 	}
 
 	@Override
+	public String getIconCssClass() {
+		return "download";
+	}
+
+	@Override
 	public String getJspPath() {
 		return "/object_definitions/configuration/icon" +
 			"/import_object_definition.jsp";
