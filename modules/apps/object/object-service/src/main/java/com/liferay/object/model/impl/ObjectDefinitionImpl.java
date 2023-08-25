9800689e7a98087b100e395214101107d770f75c
Carolina Barbosa <carolina.barbosa@liferay.com>
2023-08-25 08:43:13 -0300

    LPS-194148 Add new methods to avoid duplicated code

--- modules/apps/object/object-service/src/main/java/com/liferay/object/model/impl/ObjectDefinitionImpl.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/model/impl/ObjectDefinitionImpl.java
@@ -144,11 +144,29 @@ public class ObjectDefinitionImpl extends ObjectDefinitionBaseImpl {
 	}
 
 	@Override
+	public boolean isChildNode() {
+		if ((getRootObjectDefinitionId() > 0) && !isRootNode()) {
+			return true;
+		}
+
+		return false;
+	}
+
+	@Override
 	public boolean isDefaultStorageType() {
 		if (Objects.equals(
 				getStorageType(),
 				ObjectDefinitionConstants.STORAGE_TYPE_DEFAULT)) {
 
+			return true;
+		}
+
+		return false;
+	}
+
+	@Override
+	public boolean isRootNode() {
+		if (getObjectDefinitionId() == getRootObjectDefinitionId()) {
 			return true;
 		}
 
