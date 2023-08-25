d6283bde34dc73e5b12699ee034baba8bce5334b
artartur <artur.souza@liferay.com>
2023-08-25 16:18:24 -0300

    LPS-194407 Create MustBeLessThan75Characters class

--- modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectDefinitionLocalServiceImpl.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectDefinitionLocalServiceImpl.java
@@ -2032,6 +2032,17 @@ public class ObjectDefinitionLocalServiceImpl
 			throw new ObjectDefinitionExternalReferenceCodeException.
 				MustNotStartWithPrefix();
 		}
+<<<<<<< Updated upstream
+=======
+
+		char[] externalReferenceCodeCharArrayCharArray =
+			externalReferenceCode.toCharArray();
+
+		if (externalReferenceCodeCharArrayCharArray.length > 75) {
+			throw new ObjectDefinitionExternalReferenceCodeException.
+				MustBeLessThan75Characters(75);
+		}
+>>>>>>> Stashed changes
 	}
 
 	private void _validateLabel(Map<Locale, String> labelMap)
