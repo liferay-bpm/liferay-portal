29ab7f04e305d920ddb218e636d52bd4b7db01c3
Paulo Albuquerque <paulo.albuquerque@liferay.com>
2023-09-12 17:48:15 -0300

    LPS-195703 set rootObjectEntryId into objectEntry from a root descendant node ObjectDefinition

--- modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectEntryLocalServiceImpl.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectEntryLocalServiceImpl.java
@@ -35,6 +35,10 @@ import com.liferay.object.constants.ObjectFieldSettingConstants;
 import com.liferay.object.constants.ObjectFieldValidationConstants;
 import com.liferay.object.constants.ObjectFilterConstants;
 import com.liferay.object.constants.ObjectRelationshipConstants;
+import com.liferay.object.definition.tree.Edge;
+import com.liferay.object.definition.tree.Node;
+import com.liferay.object.definition.tree.Tree;
+import com.liferay.object.definition.tree.TreeFactory;
 import com.liferay.object.entry.util.ObjectEntryThreadLocal;
 import com.liferay.object.exception.NoSuchObjectFieldException;
 import com.liferay.object.exception.ObjectDefinitionScopeException;
@@ -128,6 +132,7 @@ import com.liferay.portal.kernel.model.SystemEventConstants;
 import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.model.Users_OrgsTable;
 import com.liferay.portal.kernel.model.role.RoleConstants;
+import com.liferay.portal.kernel.module.service.Snapshot;
 import com.liferay.portal.kernel.portletfilerepository.PortletFileRepository;
 import com.liferay.portal.kernel.repository.model.FileEntry;
 import com.liferay.portal.kernel.repository.model.Folder;
@@ -276,6 +281,8 @@ public class ObjectEntryLocalServiceImpl
 		ObjectEntry objectEntry = objectEntryPersistence.create(objectEntryId);
 
 		_setExternalReferenceCode(objectEntry, values);
+
+		_setRootObjectEntryId(objectDefinition, objectEntry, values);
 
 		objectEntry.setGroupId(groupId);
 		objectEntry.setCompanyId(user.getCompanyId());
@@ -1416,6 +1423,8 @@ public class ObjectEntryLocalServiceImpl
 		objectEntry = objectEntryPersistence.findByPrimaryKey(objectEntryId);
 
 		_setExternalReferenceCode(objectEntry, values);
+
+		_setRootObjectEntryId(objectDefinition, objectEntry, values);
 
 		objectEntry.setModifiedDate(serviceContext.getModifiedDate(null));
 		objectEntry.setTransientValues(transientValues);
@@ -3675,6 +3684,55 @@ public class ObjectEntryLocalServiceImpl
 		}
 	}
 
+	private void _setRootObjectEntryId(
+			ObjectDefinition objectDefinition, ObjectEntry objectEntry,
+			Map<String, Serializable> values)
+		throws PortalException {
+
+		if (objectDefinition.getRootObjectDefinitionId() == 0) {
+			objectEntry.setRootObjectEntryId(0);
+
+			return;
+		}
+
+		if (objectDefinition.isRootNode()) {
+			objectEntry.setRootObjectEntryId(objectEntry.getObjectEntryId());
+
+			return;
+		}
+
+		TreeFactory treeFactory = _treeFactorySnapshot.get();
+
+		Tree tree = treeFactory.create(
+			objectDefinition.getRootObjectDefinitionId());
+
+		Node node = tree.getNode(objectDefinition.getObjectDefinitionId());
+
+		ObjectEntry rootObjectEntry = null;
+
+		while (!node.isRoot()) {
+			Edge edge = node.getEdge();
+
+			ObjectRelationship objectRelationship =
+				_objectRelationshipPersistence.findByPrimaryKey(
+					edge.getObjectRelationshipId());
+
+			ObjectField objectField = _objectFieldLocalService.getObjectField(
+				objectRelationship.getObjectFieldId2());
+
+			if (rootObjectEntry != null) {
+				values = rootObjectEntry.getValues();
+			}
+
+			rootObjectEntry = getObjectEntry(
+				MapUtil.getLong(values, objectField.getName()));
+
+			node = tree.getNode(rootObjectEntry.getObjectDefinitionId());
+		}
+
+		objectEntry.setRootObjectEntryId(rootObjectEntry.getObjectEntryId());
+	}
+
 	private void _startWorkflowInstance(
 			long userId, ObjectEntry objectEntry, ServiceContext serviceContext)
 		throws PortalException {
@@ -4448,6 +4506,9 @@ public class ObjectEntryLocalServiceImpl
 		new CentralizedThreadLocal<>(
 			ObjectEntryLocalServiceImpl.class + "._skipModelListeners",
 			() -> false);
+	private static final Snapshot<TreeFactory> _treeFactorySnapshot =
+		new Snapshot<>(
+			ObjectEntryLocalServiceImpl.class, TreeFactory.class, null, true);
 
 	@Reference
 	private AccountEntryLocalService _accountEntryLocalService;
