a27cdd2a6d708e628e45fb4ac7c70e2b27a62422
Selton Guedes <selton.guedes@liferay.com>
2023-09-21 15:47:40 -0300

    LPS-196061 Only allowed bundles can update system object actions, and they cannot delete or update custom object actions

--- modules/apps/object/object-admin-rest-impl/src/main/java/com/liferay/object/admin/rest/internal/resource/v1_0/ObjectDefinitionResourceImpl.java
+++ modules/apps/object/object-admin-rest-impl/src/main/java/com/liferay/object/admin/rest/internal/resource/v1_0/ObjectDefinitionResourceImpl.java
@@ -36,6 +36,7 @@ import com.liferay.object.constants.ObjectRelationshipConstants;
 import com.liferay.object.definition.util.ObjectDefinitionUtil;
 import com.liferay.object.exception.ObjectDefinitionEnableLocalizationException;
 import com.liferay.object.exception.ObjectDefinitionStorageTypeException;
+import com.liferay.object.model.ObjectActionModel;
 import com.liferay.object.model.ObjectFieldModel;
 import com.liferay.object.model.ObjectFolder;
 import com.liferay.object.model.ObjectValidationRuleModel;
@@ -552,11 +553,16 @@ public class ObjectDefinitionResourceImpl
 					objectDefinition.getScope());
 		}
 
+		List<ObjectAction> objectActions = ListUtil.fromArray(
+			objectDefinition.getObjectActions());
 		List<ObjectField> objectFields = ListUtil.fromArray(
 			objectDefinition.getObjectFields());
 		List<ObjectValidationRule> objectValidationRules = ListUtil.fromArray(
 			objectDefinition.getObjectValidationRules());
 
+		List<com.liferay.object.model.ObjectAction>
+			serviceBuilderObjectActions = new ArrayList<>(
+				_objectActionLocalService.getObjectActions(objectDefinitionId));
 		List<com.liferay.object.model.ObjectField> serviceBuilderObjectFields =
 			new ArrayList<>(
 				_objectFieldLocalService.getObjectFields(objectDefinitionId));
@@ -569,12 +575,18 @@ public class ObjectDefinitionResourceImpl
 			serviceBuilderObjectDefinition.isSystem() &&
 			ObjectDefinitionUtil.isInvokerBundleAllowed()) {
 
+			objectActions.removeIf(
+				objectAction -> !GetterUtil.getBoolean(
+					objectAction.getSystem()));
 			objectFields.removeIf(
 				objectField -> !GetterUtil.getBoolean(objectField.getSystem()));
 			objectValidationRules.removeIf(
 				objectValidationRule -> !GetterUtil.getBoolean(
 					objectValidationRule.getSystem()));
 
+			serviceBuilderObjectActions.removeIf(
+				serviceBuilderObjectAction ->
+					!serviceBuilderObjectAction.isSystem());
 			serviceBuilderObjectFields.removeIf(
 				serviceBuilderObjectField ->
 					serviceBuilderObjectField.isMetadata() ||
@@ -584,12 +596,16 @@ public class ObjectDefinitionResourceImpl
 					!serviceBuilderObjectValidationRule.isSystem());
 		}
 		else {
+			objectActions.removeIf(
+				objectAction -> GetterUtil.getBoolean(
+					objectAction.getSystem()));
 			objectFields.removeIf(
 				objectField -> GetterUtil.getBoolean(objectField.getSystem()));
 			objectValidationRules.removeIf(
 				objectValidationRule -> GetterUtil.getBoolean(
 					objectValidationRule.getSystem()));
 
+			serviceBuilderObjectActions.removeIf(ObjectActionModel::isSystem);
 			serviceBuilderObjectFields.removeIf(ObjectFieldModel::isSystem);
 			serviceBuilderObjectValidationRules.removeIf(
 				ObjectValidationRuleModel::isSystem);
@@ -640,10 +656,16 @@ public class ObjectDefinitionResourceImpl
 				serviceBuilderObjectField);
 		}
 
-		ObjectAction[] objectActions = objectDefinition.getObjectActions();
+		Set<String> deleteObjectActionsERCs = SetUtil.asymmetricDifference(
+			transform(
+				serviceBuilderObjectActions,
+				ObjectActionModel::getExternalReferenceCode),
+			transform(objectActions, ObjectAction::getExternalReferenceCode));
 
-		if (objectActions != null) {
-			_objectActionLocalService.deleteObjectActions(objectDefinitionId);
+		for (String deleteObjectActionsERC : deleteObjectActionsERCs) {
+			_objectActionLocalService.deleteObjectAction(
+				_objectActionLocalService.fetchObjectAction(
+					deleteObjectActionsERC, objectDefinitionId));
 		}
 
 		ObjectLayout[] objectLayouts = objectDefinition.getObjectLayouts();
@@ -684,8 +706,9 @@ public class ObjectDefinitionResourceImpl
 		}
 
 		_addObjectDefinitionResources(
-			accountEntryRestrictedObjectRelationshipsNames, objectActions,
-			objectDefinitionId, objectLayouts, objectRelationships,
+			accountEntryRestrictedObjectRelationshipsNames,
+			objectActions.toArray(new ObjectAction[0]), objectDefinitionId,
+			objectLayouts, objectRelationships,
 			objectValidationRules.toArray(new ObjectValidationRule[0]),
 			objectViews);
 
@@ -748,6 +771,20 @@ public class ObjectDefinitionResourceImpl
 			).build();
 
 			for (ObjectAction objectAction : objectActions) {
+				com.liferay.object.model.ObjectAction
+					serviceBuilderObjectAction =
+						_objectActionLocalService.fetchObjectAction(
+							objectAction.getExternalReferenceCode(),
+							objectDefinitionId);
+
+				if (serviceBuilderObjectAction != null) {
+					objectActionResource.putObjectAction(
+						serviceBuilderObjectAction.getObjectActionId(),
+						objectAction);
+
+					continue;
+				}
+
 				objectActionResource.postObjectDefinitionObjectAction(
 					objectDefinitionId, objectAction);
 			}
