f96cf6384b1cb23ae62341efae5d36a38437e5a2
Mateus Santana <mateus.santana@liferay.com>
2023-09-27 11:42:02 -0300

    LPS-194850 Create updateObjectValidationRuleSettings method

--- modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectValidationRuleSettingLocalServiceImpl.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectValidationRuleSettingLocalServiceImpl.java
@@ -5,13 +5,20 @@
 
 package com.liferay.object.service.impl;
 
+import com.liferay.object.constants.ObjectValidationRuleSettingConstants;
 import com.liferay.object.model.ObjectValidationRuleSetting;
 import com.liferay.object.service.base.ObjectValidationRuleSettingLocalServiceBaseImpl;
 import com.liferay.object.service.persistence.ObjectValidationRulePersistence;
+import com.liferay.petra.function.transform.TransformUtil;
 import com.liferay.portal.aop.AopService;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.service.UserLocalService;
+import com.liferay.portal.kernel.util.SetUtil;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -51,6 +58,161 @@ public class ObjectValidationRuleSettingLocalServiceImpl
 
 		return objectValidationRuleSettingPersistence.update(
 			objectValidationRuleSetting);
+	}
+
+	@Override
+	public List<ObjectValidationRuleSetting> updateObjectValidationRuleSettings(
+			long objectValidationRuleId,
+			List<ObjectValidationRuleSetting> objectValidationRuleSettings,
+			long userId)
+		throws PortalException {
+
+		List<ObjectValidationRuleSetting> keyObjectValidationRuleSettings =
+			new ArrayList<>();
+
+		List<ObjectValidationRuleSetting> outputObjectValidationRuleSettings =
+			new ArrayList<>();
+
+		_getDeleteObjectValidationRuleSettings(
+			keyObjectValidationRuleSettings, objectValidationRuleSettings,
+			outputObjectValidationRuleSettings);
+
+		if (keyObjectValidationRuleSettings.size() < 2) {
+			throw new PortalException(
+				"Unique Composed Key Object Validation Rule must have at " +
+					"least two composing a unique composed key");
+		}
+
+		List<ObjectValidationRuleSetting> oldKeyObjectValidationRuleSettings =
+			objectValidationRuleSettingPersistence.findByOVRI_N(
+				objectValidationRuleId,
+				ObjectValidationRuleSettingConstants.NAME_KEY_OBJECT_FIELD_ID);
+
+		_deleteOldObjectValidationRuleSettings(
+			objectValidationRuleId,
+			ObjectValidationRuleSettingConstants.NAME_KEY_OBJECT_FIELD_ID,
+			keyObjectValidationRuleSettings,
+			oldKeyObjectValidationRuleSettings);
+
+		List<ObjectValidationRuleSetting>
+			oldOutputObjectValidationRuleSettings =
+				objectValidationRuleSettingPersistence.findByOVRI_N(
+					objectValidationRuleId,
+					ObjectValidationRuleSettingConstants.
+						NAME_OUTPUT_OBJECT_FIELD_ID);
+
+		_deleteOldObjectValidationRuleSettings(
+			objectValidationRuleId,
+			ObjectValidationRuleSettingConstants.NAME_OUTPUT_OBJECT_FIELD_ID,
+			outputObjectValidationRuleSettings,
+			oldOutputObjectValidationRuleSettings);
+
+		List<ObjectValidationRuleSetting> updatedObjectValidationRuleSettings =
+			new ArrayList<>();
+
+		_addOrUpdateObjectValidationRuleSetting(
+			objectValidationRuleId,
+			ObjectValidationRuleSettingConstants.NAME_KEY_OBJECT_FIELD_ID,
+			keyObjectValidationRuleSettings,
+			updatedObjectValidationRuleSettings, userId);
+
+		_addOrUpdateObjectValidationRuleSetting(
+			objectValidationRuleId,
+			ObjectValidationRuleSettingConstants.NAME_OUTPUT_OBJECT_FIELD_ID,
+			outputObjectValidationRuleSettings,
+			updatedObjectValidationRuleSettings, userId);
+
+		return updatedObjectValidationRuleSettings;
+	}
+
+	private void _addOrUpdateObjectValidationRuleSetting(
+			long objectValidationRuleId, String objectValidationRuleSettingName,
+			List<ObjectValidationRuleSetting> objectValidationRuleSettings,
+			List<ObjectValidationRuleSetting>
+				updatedObjectValidationRuleSettings,
+			long userId)
+		throws PortalException {
+
+		for (ObjectValidationRuleSetting objectValidationRuleSetting :
+				objectValidationRuleSettings) {
+
+			ObjectValidationRuleSetting
+				serviceBuilderObjectValidationRuleSetting =
+					objectValidationRuleSettingPersistence.fetchByOVRI_N_V(
+						objectValidationRuleId, objectValidationRuleSettingName,
+						objectValidationRuleSetting.getValue());
+
+			if (serviceBuilderObjectValidationRuleSetting != null) {
+				serviceBuilderObjectValidationRuleSetting.setName(
+					objectValidationRuleSetting.getName());
+				serviceBuilderObjectValidationRuleSetting.setValue(
+					objectValidationRuleSetting.getValue());
+
+				updatedObjectValidationRuleSettings.add(
+					updateObjectValidationRuleSetting(
+						serviceBuilderObjectValidationRuleSetting));
+
+				continue;
+			}
+
+			updatedObjectValidationRuleSettings.add(
+				addObjectValidationRuleSetting(
+					userId, objectValidationRuleId,
+					objectValidationRuleSetting.getName(),
+					objectValidationRuleSetting.getValue()));
+		}
+	}
+
+	private void _deleteOldObjectValidationRuleSettings(
+			long objectValidationRuleId, String objectValidationRuleSettingName,
+			List<ObjectValidationRuleSetting> objectValidationRuleSettings,
+			List<ObjectValidationRuleSetting> oldObjectValidationRuleSettings)
+		throws PortalException {
+
+		Set<String> deleteOldObjectObjectValidationRuleSettingValues =
+			SetUtil.asymmetricDifference(
+				TransformUtil.transform(
+					oldObjectValidationRuleSettings,
+					ObjectValidationRuleSetting::getValue),
+				TransformUtil.transform(
+					objectValidationRuleSettings,
+					ObjectValidationRuleSetting::getValue));
+
+		for (String deleteOldObjectObjectValidationRuleSettingValue :
+				deleteOldObjectObjectValidationRuleSettingValues) {
+
+			ObjectValidationRuleSetting
+				serviceBuilderObjectValidationRuleSetting =
+					objectValidationRuleSettingPersistence.findByOVRI_N_V(
+						objectValidationRuleId, objectValidationRuleSettingName,
+						deleteOldObjectObjectValidationRuleSettingValue);
+
+			deleteObjectValidationRuleSetting(
+				serviceBuilderObjectValidationRuleSetting.
+					getObjectValidationRuleSettingId());
+		}
+	}
+
+	private void _getDeleteObjectValidationRuleSettings(
+		List<ObjectValidationRuleSetting> keyObjectValidationRuleSettings,
+		List<ObjectValidationRuleSetting> objectValidationRuleSettings,
+		List<ObjectValidationRuleSetting> outputObjectValidationRuleSettings) {
+
+		for (ObjectValidationRuleSetting objectValidationRuleSetting :
+				objectValidationRuleSettings) {
+
+			if (objectValidationRuleSetting.compareName(
+					ObjectValidationRuleSettingConstants.
+						NAME_KEY_OBJECT_FIELD_ID)) {
+
+				keyObjectValidationRuleSettings.add(
+					objectValidationRuleSetting);
+
+				continue;
+			}
+
+			outputObjectValidationRuleSettings.add(objectValidationRuleSetting);
+		}
 	}
 
 	@Reference
