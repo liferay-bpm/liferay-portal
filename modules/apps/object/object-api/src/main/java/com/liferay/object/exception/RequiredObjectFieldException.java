02fd77f2e64ae95133cab821c4979f8df6a32f76
Larissa Cerqueira <larissa.cerqueira.ribeiro@gmail.com>
2023-09-27 19:34:06 -0300

    LPS-195659 Alter Mapper and Exception to work with inner classes

--- modules/apps/object/object-api/src/main/java/com/liferay/object/exception/RequiredObjectFieldException.java
+++ modules/apps/object/object-api/src/main/java/com/liferay/object/exception/RequiredObjectFieldException.java
@@ -5,23 +5,61 @@
 
 package com.liferay.object.exception;
 
+import com.liferay.object.model.ObjectDefinition;
 import com.liferay.portal.kernel.exception.PortalException;
+
+import java.util.Collections;
+import java.util.List;
 
 /**
  * @author Marco Leo
  */
 public class RequiredObjectFieldException extends PortalException {
 
-	public RequiredObjectFieldException() {
-		super("At least one object field must be added");
-
-		_messageKey = "at-least-one-object-field-must-be-added";
+	public List<Object> getArguments() {
+		return _arguments;
 	}
 
 	public String getMessageKey() {
 		return _messageKey;
 	}
 
-	private final String _messageKey;
+	public static class MustAddAtLeastOneField
+		extends RequiredObjectFieldException {
+
+		public MustAddAtLeastOneField() {
+			super(
+				"At least one object field must be added",
+				"at-least-one-object-field-must-be-added");
+		}
+
+	}
+
+	public static class MustIntroduceNewCustomField
+		extends RequiredObjectFieldException {
+
+		public MustIntroduceNewCustomField(ObjectDefinition objectDefinition) {
+			super(
+				"Deletion not allowed because a relationship field is being ",
+				"used in the object definition " +
+					objectDefinition.getShortName());
+
+			_arguments = Collections.singletonList(
+				objectDefinition.getShortName());
+			_messageKey =
+				"deletion-not-allowed-because-a-relationship-field-is-being-" +
+					"used-in-the-object-definition-x";
+		}
+
+	}
+
+	private RequiredObjectFieldException(String message, String messageKey) {
+		super(message);
+
+		_messageKey = messageKey;
+	}
+
+	private static List<Object> _arguments;
+	private static String _messageKey;
 
 }
