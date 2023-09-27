1ab2c2e51db5ac21d553c3aef4da6db218ed2852
Larissa Cerqueira <larissa.cerqueira.ribeiro@gmail.com>
2023-09-27 19:35:26 -0300

    LPS-195659 Use new exception when trying to delete relationship and change ocurrences of required field

--- modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectDefinitionLocalServiceImpl.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectDefinitionLocalServiceImpl.java
@@ -1771,7 +1771,7 @@ public class ObjectDefinitionLocalServiceImpl
 					objectDefinition.getObjectDefinitionId()),
 				objectField -> !objectField.isMetadata())) {
 
-			throw new RequiredObjectFieldException();
+			throw new RequiredObjectFieldException.MustAddAtLeastOneField();
 		}
 
 		objectDefinition.setActive(true);
