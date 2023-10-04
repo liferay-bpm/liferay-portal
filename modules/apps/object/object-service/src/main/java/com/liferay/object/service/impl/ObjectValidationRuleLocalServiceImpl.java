bcdd52bbe5245066b4e64e2b9d8fc56291978812
Mateus Santana <mateus.santana@liferay.com>
2023-10-04 13:32:30 -0300

    LPS-186955 Use new finder in ObjectValidationRuleLocalService

--- modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectValidationRuleLocalServiceImpl.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectValidationRuleLocalServiceImpl.java
@@ -227,6 +227,15 @@ public class ObjectValidationRuleLocalServiceImpl
 	}
 
 	@Override
+	public List<ObjectValidationRule> getObjectValidationRulesByODI_E(
+		long objectDefinitionId, String engine) {
+
+		return _getObjectValidationRules(
+			objectValidationRulePersistence.findByODI_E(
+				objectDefinitionId, engine));
+	}
+
+	@Override
 	public int getObjectValidationRulesCount(
 		long objectDefinitionId, boolean active) {
 
