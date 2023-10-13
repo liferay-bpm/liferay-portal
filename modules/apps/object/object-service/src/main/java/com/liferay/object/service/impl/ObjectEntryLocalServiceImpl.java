6fe5c762293ef8eb91e5e9e8970cdb6227f66ce9
MarinhoFeliphe <feliphemarinho18@gmail.com>
2023-10-13 15:37:44 -0300

    LPS-195703 Rename

--- modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectEntryLocalServiceImpl.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectEntryLocalServiceImpl.java
@@ -3798,7 +3798,7 @@ public class ObjectEntryLocalServiceImpl
 			MapUtil.getLong(values, objectField.getName()));
 
 		if (objectEntry.getRootObjectEntryId() != 0) {
-			_updateChildrenRootObjectEntryId(
+			_updateDescendantsRootObjectEntryId(
 				objectEntry, parentObjectEntry.getRootObjectEntryId(),
 				treeFactory);
 		}
@@ -3844,7 +3844,7 @@ public class ObjectEntryLocalServiceImpl
 			'.');
 	}
 
-	private void _updateChildrenRootObjectEntryId(
+	private void _updateDescendantsRootObjectEntryId(
 			ObjectEntry objectEntry, long rootObjectEntryId,
 			TreeFactory treeFactory)
 		throws PortalException {
