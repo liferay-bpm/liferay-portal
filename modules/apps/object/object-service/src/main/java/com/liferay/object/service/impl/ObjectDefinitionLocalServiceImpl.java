aaaf2d88dff46e185cc5b2a0bc292beba7b95b41
MarinhoFeliphe <feliphemarinho18@gmail.com>
2023-10-13 17:01:22 -0300

    LPS-195703 Rename and separate responsibilities

--- modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectDefinitionLocalServiceImpl.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectDefinitionLocalServiceImpl.java
@@ -832,7 +832,7 @@ public class ObjectDefinitionLocalServiceImpl
 					"are ineligible for publication");
 		}
 
-		Tree tree = _treeFactory.create(objectDefinitionId);
+		Tree tree = _treeFactory.createObjectDefinitionTree(objectDefinitionId);
 
 		Iterator<Node> iterator = tree.iterator();
 
@@ -991,7 +991,7 @@ public class ObjectDefinitionLocalServiceImpl
 			objectDefinitionLocalService.getObjectDefinition(
 				objectDefinitionId);
 
-		Tree tree = _treeFactory.create(
+		Tree tree = _treeFactory.createObjectDefinitionTree(
 			objectDefinition.getRootObjectDefinitionId());
 
 		Iterator<Node> iterator = tree.iterator(
--- modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectEntryLocalServiceImpl.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectEntryLocalServiceImpl.java
@@ -3781,7 +3781,7 @@ public class ObjectEntryLocalServiceImpl
 
 		TreeFactory treeFactory = _treeFactorySnapshot.get();
 
-		Tree tree = treeFactory.create(
+		Tree tree = treeFactory.createObjectDefinitionTree(
 			objectDefinition.getRootObjectDefinitionId());
 
 		Node node = tree.getNode(objectDefinition.getObjectDefinitionId());
@@ -3849,7 +3849,8 @@ public class ObjectEntryLocalServiceImpl
 			TreeFactory treeFactory)
 		throws PortalException {
 
-		Tree tree = treeFactory.create(objectEntry.getObjectEntryId(), false);
+		Tree tree = treeFactory.createObjectEntryTree(
+			objectEntry.getObjectEntryId());
 
 		Iterator<Node> iterator = tree.iterator();
 
