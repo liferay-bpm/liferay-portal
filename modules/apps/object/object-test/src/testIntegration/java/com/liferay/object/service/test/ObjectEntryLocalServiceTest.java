3f1bba1c2916b235ecdaf288ef97ba833f3e9fe7
MarinhoFeliphe <feliphemarinho18@gmail.com>
2023-10-16 10:38:46 -0300

    LPS-195703 Rename to rootObjectEntry1

--- modules/apps/object/object-test/src/testIntegration/java/com/liferay/object/service/test/ObjectEntryLocalServiceTest.java
+++ modules/apps/object/object-test/src/testIntegration/java/com/liferay/object/service/test/ObjectEntryLocalServiceTest.java
@@ -2744,14 +2744,14 @@ public class ObjectEntryLocalServiceTest {
 		_objectDefinitionLocalService.publishCustomObjectDefinition(
 			TestPropsValues.getUserId(), rootNode.getPrimaryKey());
 
-		ObjectEntry contextObjectEntry1 =
+		ObjectEntry rootObjectEntry1 =
 			_objectEntryLocalService.addObjectEntry(
 				TestPropsValues.getUserId(), 0, rootNode.getPrimaryKey(),
 				Collections.emptyMap(),
 				ServiceContextTestUtil.getServiceContext());
 
 		Map<Long, Long> objectEntriesId = HashMapBuilder.put(
-			rootNode.getPrimaryKey(), contextObjectEntry1.getObjectEntryId()
+			rootNode.getPrimaryKey(), rootObjectEntry1.getObjectEntryId()
 		).build();
 
 		while (iterator.hasNext()) {
@@ -2793,7 +2793,7 @@ public class ObjectEntryLocalServiceTest {
 							objectDefinition.getObjectDefinitionId()));
 
 				Assert.assertEquals(
-					contextObjectEntry1.getObjectEntryId(),
+					rootObjectEntry1.getObjectEntryId(),
 					objectEntry.getRootObjectEntryId());
 			});
 
@@ -2854,7 +2854,7 @@ public class ObjectEntryLocalServiceTest {
 					nodeObjectDefinition.getName())) {
 
 				Assert.assertEquals(
-					contextObjectEntry1.getObjectEntryId(),
+					rootObjectEntry1.getObjectEntryId(),
 					nodeObjectEntry.getRootObjectEntryId());
 			}
 			else {
