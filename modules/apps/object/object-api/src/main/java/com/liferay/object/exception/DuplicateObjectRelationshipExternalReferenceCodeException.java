7c1af18678348bcbed824d8bccf638368db49585
Leilany Ulisses <leilany.ulisses@liferay.com>
2023-10-30 13:16:36 -0300

    LPS-172418 buildRest

--- modules/apps/object/object-api/src/main/java/com/liferay/object/exception/DuplicateObjectRelationshipExternalReferenceCodeException.java
+++ modules/apps/object/object-api/src/main/java/com/liferay/object/exception/DuplicateObjectRelationshipExternalReferenceCodeException.java
@@ -2,6 +2,7 @@
  * SPDX-FileCopyrightText: (c) 2023 Liferay, Inc. https://liferay.com
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
+
 package com.liferay.object.exception;
 
 import com.liferay.portal.kernel.exception.DuplicateExternalReferenceCodeException;
@@ -9,20 +10,27 @@ import com.liferay.portal.kernel.exception.DuplicateExternalReferenceCodeExcepti
 /**
  * @author Marco Leo
  */
-public class DuplicateObjectRelationshipExternalReferenceCodeException extends DuplicateExternalReferenceCodeException {
+public class DuplicateObjectRelationshipExternalReferenceCodeException
+	extends DuplicateExternalReferenceCodeException {
 
 	public DuplicateObjectRelationshipExternalReferenceCodeException() {
 	}
 
-	public DuplicateObjectRelationshipExternalReferenceCodeException(String msg) {
+	public DuplicateObjectRelationshipExternalReferenceCodeException(
+		String msg) {
+
 		super(msg);
 	}
 
-	public DuplicateObjectRelationshipExternalReferenceCodeException(String msg, Throwable throwable) {
+	public DuplicateObjectRelationshipExternalReferenceCodeException(
+		String msg, Throwable throwable) {
+
 		super(msg, throwable);
 	}
 
-	public DuplicateObjectRelationshipExternalReferenceCodeException(Throwable throwable) {
+	public DuplicateObjectRelationshipExternalReferenceCodeException(
+		Throwable throwable) {
+
 		super(throwable);
 	}
 
