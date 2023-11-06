ff90f79b8b60752f579a3707f3d2600be70e3f5b
Mateus Santana <mateus.santana@liferay.com>
2023-11-06 17:25:58 -0300

    LPS-194851 Adapt RequiredObjectValidationRuleSettingException exception

--- modules/apps/object/object-api/src/main/java/com/liferay/object/exception/RequiredObjectValidationRuleSettingException.java
+++ modules/apps/object/object-api/src/main/java/com/liferay/object/exception/RequiredObjectValidationRuleSettingException.java
@@ -2,28 +2,42 @@
  * SPDX-FileCopyrightText: (c) 2023 Liferay, Inc. https://liferay.com
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
+
 package com.liferay.object.exception;
 
 import com.liferay.portal.kernel.exception.PortalException;
 
 /**
- * @author Marco Leo
+ * @author Mateus Santana
  */
-public class RequiredObjectValidationRuleSettingException extends PortalException {
+public class RequiredObjectValidationRuleSettingException
+	extends PortalException {
 
-	public RequiredObjectValidationRuleSettingException() {
+	public RequiredObjectValidationRuleSettingException(
+		String message, String messageKey) {
+
+		super(message);
+
+		_messageKey = messageKey;
 	}
 
-	public RequiredObjectValidationRuleSettingException(String msg) {
-		super(msg);
+	public String getMessageKey() {
+		return _messageKey;
 	}
 
-	public RequiredObjectValidationRuleSettingException(String msg, Throwable throwable) {
-		super(msg, throwable);
+	public static class MustNotDeleteObjectValidationRuleSettingCompositeKey
+		extends RequiredObjectValidationRuleSettingException {
+
+		public MustNotDeleteObjectValidationRuleSettingCompositeKey() {
+			super(
+				"Fields cannot be deleted from unique composite keys after " +
+					"the definition is published",
+				"fields-cannot-be-deleted-from-unique-composite-keys-after-" +
+					"the-definition-is-published");
+		}
+
 	}
 
-	public RequiredObjectValidationRuleSettingException(Throwable throwable) {
-		super(throwable);
-	}
+	private final String _messageKey;
 
 }
