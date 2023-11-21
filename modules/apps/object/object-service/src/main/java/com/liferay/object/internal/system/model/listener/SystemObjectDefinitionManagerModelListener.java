5034100b8b7babdd65c299754cc6d521e4d94c40
Carlos Correa <carlos.correa@liferay.com>
2023-11-21 13:24:58 +0100

    LPS-198290 Adapt to the supplier

--- modules/apps/object/object-service/src/main/java/com/liferay/object/internal/system/model/listener/SystemObjectDefinitionManagerModelListener.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/internal/system/model/listener/SystemObjectDefinitionManagerModelListener.java
@@ -165,13 +165,13 @@ public class SystemObjectDefinitionManagerModelListener<T extends BaseModel<T>>
 
 			long userId = _getUserId(baseModel);
 
+			JSONObject payloadJSONObject = _getPayloadJSONObject(
+				objectActionTriggerKey, objectDefinition, originalBaseModel,
+				baseModel, userId);
+
 			_objectActionEngine.executeObjectActions(
 				_modelClass.getName(), _getCompanyId(baseModel),
-				objectActionTriggerKey,
-				() -> _getPayloadJSONObject(
-					objectActionTriggerKey, objectDefinition, originalBaseModel,
-					baseModel, userId),
-				userId);
+				objectActionTriggerKey, () -> payloadJSONObject, userId);
 		}
 		catch (PortalException portalException) {
 			throw new ModelListenerException(portalException);
