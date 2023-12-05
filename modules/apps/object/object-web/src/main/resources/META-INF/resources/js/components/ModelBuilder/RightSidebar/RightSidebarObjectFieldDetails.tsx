f27eab3ce1b1bb252b0ac901db27ff49e505168f
Artur Souza <artur.souza@liferay.com>
2023-12-05 17:43:50 -0300

    LPS-198613 Leave the searchable toggle and character limit disabled in system fields

--- modules/apps/object/object-web/src/main/resources/META-INF/resources/js/components/ModelBuilder/RightSidebar/RightSidebarObjectFieldDetails.tsx
+++ modules/apps/object/object-web/src/main/resources/META-INF/resources/js/components/ModelBuilder/RightSidebar/RightSidebarObjectFieldDetails.tsx
@@ -86,15 +86,14 @@ export function RightSidebarObjectFieldDetails() {
 			let objectField: Partial<ObjectField>;
 
 			if (!editedObjectField) {
-				objectField = values;
+				objectField = {...values};
 			}
 			else {
-				objectField = editedObjectField;
+				objectField = {...editedObjectField};
 			}
 
 			delete objectField.defaultValue;
 			delete objectField.listTypeDefinitionId;
-			delete objectField.system;
 
 			try {
 				const updatedObjectFieldResponse = await API.save<ObjectField>({
