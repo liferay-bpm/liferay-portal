6e8f2794bc58beac53d0c33fc3097bd6f724a420
Thalles Montenegro <thalles.montenegro@liferay.com>
2024-01-04 15:56:47 -0300

    LPS-205028 Fix error message on form container

--- modules/apps/info/info-api/src/main/java/com/liferay/info/exception/InfoFormValidationException.java
+++ modules/apps/info/info-api/src/main/java/com/liferay/info/exception/InfoFormValidationException.java
@@ -57,6 +57,32 @@ public class InfoFormValidationException extends InfoFormException {
 
 	}
 
+	public static class ExceedsMaxEntries extends InfoFormValidationException {
+
+		public ExceedsMaxEntries(
+			String messageKey, String label) {
+
+			_messageKey = messageKey;
+			_label = label;
+		}
+
+		@Override
+		public String getLocalizedMessage(Locale locale) {
+			return LanguageUtil.format(
+				locale, _messageKey, new String[] {_label},
+				false);
+		}
+
+		@Override
+		public String getLocalizedMessage(String fieldLabel, Locale locale) {
+			return getLocalizedMessage(locale);
+		}
+
+		private final String _messageKey;
+		private final String _label;
+
+	}
+
 	public static class ExceedsMaxLength extends InvalidInfoFieldValue {
 
 		public ExceedsMaxLength(String infoFieldUniqueId, int maxLength) {
