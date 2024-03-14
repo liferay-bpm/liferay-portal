c1e7e7e51c1954ec036ada0aefe58960df391de5
Jair Medeiros <jair.medeiros@liferay.com>
2024-03-14 13:21:07 -0300

    LPD-20551 SF

--- modules/apps/object/object-rest-test/src/testIntegration/java/com/liferay/object/rest/internal/manager/v1_0/test/DefaultObjectEntryManagerImplTest.java
+++ modules/apps/object/object-rest-test/src/testIntegration/java/com/liferay/object/rest/internal/manager/v1_0/test/DefaultObjectEntryManagerImplTest.java
@@ -23,7 +23,6 @@ import com.liferay.document.library.util.DLURLHelper;
 import com.liferay.list.type.model.ListTypeEntry;
 import com.liferay.list.type.service.ListTypeEntryLocalService;
 import com.liferay.object.constants.ObjectActionKeys;
-import com.liferay.object.constants.ObjectConstants;
 import com.liferay.object.constants.ObjectDefinitionConstants;
 import com.liferay.object.constants.ObjectFieldSettingConstants;
 import com.liferay.object.constants.ObjectFieldValidationConstants;
@@ -1078,7 +1077,8 @@ public class DefaultObjectEntryManagerImplTest
 		_user = _addUser();
 
 		Role role = _addRoleUser(
-			new String[] {ObjectActionKeys.ADD_OBJECT_ENTRY}, _objectDefinition3, _user);
+			new String[] {ObjectActionKeys.ADD_OBJECT_ENTRY},
+			_objectDefinition3, _user);
 
 		Assert.assertNotNull(_addObjectEntry(accountEntry1));
 
@@ -3763,9 +3763,8 @@ public class DefaultObjectEntryManagerImplTest
 		}
 
 		_resourcePermissionLocalService.setResourcePermissions(
-			companyId, name,
-			ResourceConstants.SCOPE_COMPANY, String.valueOf(companyId),
-			role.getRoleId(), actionIds);
+			companyId, name, ResourceConstants.SCOPE_COMPANY,
+			String.valueOf(companyId), role.getRoleId(), actionIds);
 
 		_userLocalService.addRoleUser(role.getRoleId(), user);
 
