14d74d0a5fea15da02b62d65d3e92354a3f2b044
Diogo Melo <diogo.melo@liferay.com>
2024-04-16 20:14:15 -0300

    LPD-16433 Add new method testImportWithError

--- modules/apps/object/object-test/src/testIntegration/java/com/liferay/object/web/internal/portlet/action/test/BaseExportImportTestCase.java
+++ modules/apps/object/object-test/src/testIntegration/java/com/liferay/object/web/internal/portlet/action/test/BaseExportImportTestCase.java
@@ -5,14 +5,21 @@
 
 package com.liferay.object.web.internal.portlet.action.test;
 
+import com.liferay.object.constants.ObjectPortletKeys;
 import com.liferay.petra.string.StringBundler;
 import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.json.JSONFactoryUtil;
+import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.model.Layout;
 import com.liferay.portal.kernel.model.LayoutConstants;
+import com.liferay.portal.kernel.model.Portlet;
 import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.portlet.LiferayPortletConfig;
 import com.liferay.portal.kernel.portlet.LiferayPortletRequest;
+import com.liferay.portal.kernel.portlet.PortletConfigFactoryUtil;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCResourceCommand;
+import com.liferay.portal.kernel.service.PortletLocalService;
 import com.liferay.portal.kernel.test.ReflectionTestUtil;
 import com.liferay.portal.kernel.test.portlet.MockLiferayPortletActionRequest;
 import com.liferay.portal.kernel.test.portlet.MockLiferayPortletActionResponse;
@@ -23,6 +30,7 @@ import com.liferay.portal.kernel.test.util.TestPropsValues;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.ArrayUtil;
 import com.liferay.portal.kernel.util.File;
+import com.liferay.portal.kernel.util.JavaConstants;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.Portal;
 import com.liferay.portal.kernel.util.ProxyUtil;
@@ -35,10 +43,13 @@ import com.liferay.portal.upload.test.util.UploadTestUtil;
 
 import java.util.Objects;
 
+import org.junit.Assert;
+
 import org.skyscreamer.jsonassert.JSONAssert;
 import org.skyscreamer.jsonassert.JSONCompareMode;
 
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.mock.web.MockMultipartHttpServletRequest;
 
@@ -52,7 +63,109 @@ public abstract class BaseExportImportTestCase {
 			String externalReferenceCode, String name)
 		throws Exception {
 
+		MVCActionCommand mvcActionCommand = getMVCActionCommand();
+
+		MockLiferayPortletActionRequest mockLiferayPortletActionRequest =
+			_importRequest(actualFileName, externalReferenceCode, name);
+
+		mvcActionCommand.processAction(
+			mockLiferayPortletActionRequest,
+			new MockLiferayPortletActionResponse());
+
+		// MVCResourceCommand
+
+		MVCResourceCommand mvcResourceCommand = getMVCResourceCommand();
+
+		MockLiferayResourceRequest mockLiferayResourceRequest =
+			new MockLiferayResourceRequest();
+
+		Class<?> clazz = getClazz();
+
+		mockLiferayResourceRequest.addParameter(
+			getIdentifierName(), String.valueOf(getId(name)));
+		mockLiferayResourceRequest.setAttribute(
+			WebKeys.THEME_DISPLAY, _getThemeDisplay());
+
+		MockLiferayResourceResponse mockLiferayResourceResponse =
+			new MockLiferayResourceResponse();
+
+		mvcResourceCommand.serveResource(
+			mockLiferayResourceRequest, mockLiferayResourceResponse);
+
+		JSONAssert.assertEquals(
+			StringUtil.read(
+				clazz.getResourceAsStream("dependencies/" + expectedFileName)),
+			String.valueOf(
+				mockLiferayResourceResponse.getPortletOutputStream()),
+			JSONCompareMode.LENIENT);
+	}
+
+	public void testImportWithError(
+			String actualFileName, String expectedFileName,
+			String externalReferenceCode, String name)
+		throws Exception {
+
+		MVCActionCommand mvcActionCommand = getMVCActionCommand();
+
+		MockLiferayPortletActionRequest mockLiferayPortletActionRequest =
+			_importRequest(actualFileName, externalReferenceCode, name);
+
 		// MVCActionCommand
+
+		MockLiferayPortletActionResponse mockLiferayPortletActionResponse =
+			new MockLiferayPortletActionResponse();
+
+		mvcActionCommand.processAction(
+			mockLiferayPortletActionRequest, mockLiferayPortletActionResponse);
+
+		MockHttpServletResponse mockHttpServletResponse =
+			(MockHttpServletResponse)
+				mockLiferayPortletActionResponse.getHttpServletResponse();
+
+		JSONObject jsonObject = JSONFactoryUtil.createJSONObject(
+			mockHttpServletResponse.getContentAsString());
+
+		Assert.assertEquals(
+			"The object definition was imported without a custom view.",
+			jsonObject.get("title"));
+	}
+
+	protected abstract ClassLoader getClassLoader();
+
+	protected abstract Class<?> getClazz();
+
+	protected abstract long getId(String name) throws Exception;
+
+	protected abstract String getIdentifierName();
+
+	protected abstract String getJSONName();
+
+	protected abstract MVCActionCommand getMVCActionCommand();
+
+	protected abstract MVCResourceCommand getMVCResourceCommand();
+
+	protected User user;
+
+	private ThemeDisplay _getThemeDisplay() throws Exception {
+		ThemeDisplay themeDisplay = new ThemeDisplay();
+
+		Layout layout = new LayoutImpl();
+
+		layout.setType(LayoutConstants.TYPE_CONTROL_PANEL);
+
+		themeDisplay.setLayout(layout);
+
+		themeDisplay.setLocale(LocaleUtil.US);
+		themeDisplay.setScopeGroupId(TestPropsValues.getGroupId());
+		themeDisplay.setSiteDefaultLocale(LocaleUtil.US);
+		themeDisplay.setUser(user);
+
+		return themeDisplay;
+	}
+
+	private MockLiferayPortletActionRequest _importRequest(
+			String actualFileName, String externalReferenceCode, String name)
+		throws Exception {
 
 		MVCActionCommand mvcActionCommand = getMVCActionCommand();
 
@@ -100,6 +213,16 @@ public abstract class BaseExportImportTestCase {
 		mockLiferayPortletActionRequest.setAttribute(
 			WebKeys.THEME_DISPLAY, _getThemeDisplay());
 
+		Portlet portlet = _portletLocalService.getPortletById(
+			ObjectPortletKeys.OBJECT_DEFINITIONS);
+
+		LiferayPortletConfig liferayPortletConfig =
+			(LiferayPortletConfig)PortletConfigFactoryUtil.create(
+				portlet, null);
+
+		mockLiferayPortletActionRequest.setAttribute(
+			JavaConstants.JAVAX_PORTLET_CONFIG, liferayPortletConfig);
+
 		ReflectionTestUtil.setFieldValue(
 			mvcActionCommand, "_portal",
 			ProxyUtil.newProxyInstance(
@@ -123,66 +246,7 @@ public abstract class BaseExportImportTestCase {
 					return method.invoke(_portal, args);
 				}));
 
-		mvcActionCommand.processAction(
-			mockLiferayPortletActionRequest,
-			new MockLiferayPortletActionResponse());
-
-		// MVCResourceCommand
-
-		MVCResourceCommand mvcResourceCommand = getMVCResourceCommand();
-
-		MockLiferayResourceRequest mockLiferayResourceRequest =
-			new MockLiferayResourceRequest();
-
-		mockLiferayResourceRequest.addParameter(
-			getIdentifierName(), String.valueOf(getId(name)));
-		mockLiferayResourceRequest.setAttribute(
-			WebKeys.THEME_DISPLAY, _getThemeDisplay());
-
-		MockLiferayResourceResponse mockLiferayResourceResponse =
-			new MockLiferayResourceResponse();
-
-		mvcResourceCommand.serveResource(
-			mockLiferayResourceRequest, mockLiferayResourceResponse);
-
-		JSONAssert.assertEquals(
-			StringUtil.read(
-				clazz.getResourceAsStream("dependencies/" + expectedFileName)),
-			String.valueOf(
-				mockLiferayResourceResponse.getPortletOutputStream()),
-			JSONCompareMode.LENIENT);
-	}
-
-	protected abstract ClassLoader getClassLoader();
-
-	protected abstract Class<?> getClazz();
-
-	protected abstract long getId(String name) throws Exception;
-
-	protected abstract String getIdentifierName();
-
-	protected abstract String getJSONName();
-
-	protected abstract MVCActionCommand getMVCActionCommand();
-
-	protected abstract MVCResourceCommand getMVCResourceCommand();
-
-	protected User user;
-
-	private ThemeDisplay _getThemeDisplay() throws Exception {
-		ThemeDisplay themeDisplay = new ThemeDisplay();
-
-		Layout layout = new LayoutImpl();
-
-		layout.setType(LayoutConstants.TYPE_CONTROL_PANEL);
-
-		themeDisplay.setLayout(layout);
-
-		themeDisplay.setScopeGroupId(TestPropsValues.getGroupId());
-		themeDisplay.setSiteDefaultLocale(LocaleUtil.US);
-		themeDisplay.setUser(user);
-
-		return themeDisplay;
+		return mockLiferayPortletActionRequest;
 	}
 
 	@Inject
@@ -190,5 +254,8 @@ public abstract class BaseExportImportTestCase {
 
 	@Inject
 	private Portal _portal;
+
+	@Inject
+	private PortletLocalService _portletLocalService;
 
 }
