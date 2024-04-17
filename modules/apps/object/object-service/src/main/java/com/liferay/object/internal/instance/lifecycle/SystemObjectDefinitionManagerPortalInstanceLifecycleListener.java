0f8bc47e741dee86b28142766bce4cac54fe11dd
Carolina Barbosa <carolina.barbosa@liferay.com>
2024-04-17 14:06:05 -0300

    LPD-23201 Implement SingleFormVariationInfoCollectionProvider for system object definitions

--- modules/apps/object/object-service/src/main/java/com/liferay/object/internal/instance/lifecycle/SystemObjectDefinitionManagerPortalInstanceLifecycleListener.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/internal/instance/lifecycle/SystemObjectDefinitionManagerPortalInstanceLifecycleListener.java
@@ -5,6 +5,7 @@
 
 package com.liferay.object.internal.instance.lifecycle;
 
+import com.liferay.info.collection.provider.InfoCollectionProvider;
 import com.liferay.item.selector.ItemSelector;
 import com.liferay.item.selector.ItemSelectorView;
 import com.liferay.item.selector.ItemSelectorViewDescriptorRenderer;
@@ -17,6 +18,7 @@ import com.liferay.object.internal.notification.term.contributor.ObjectDefinitio
 import com.liferay.object.internal.related.models.SystemObject1toMObjectRelatedModelsProviderImpl;
 import com.liferay.object.internal.related.models.SystemObjectMtoMObjectRelatedModelsProviderImpl;
 import com.liferay.object.internal.rest.context.path.RESTContextPathResolverImpl;
+import com.liferay.object.internal.system.info.collection.provider.SystemObjectEntrySingleFormVariationInfoCollectionProvider;
 import com.liferay.object.model.ObjectDefinition;
 import com.liferay.object.model.ObjectFolder;
 import com.liferay.object.related.models.ObjectRelatedModelsProviderRegistrarHelper;
@@ -36,10 +38,12 @@ import com.liferay.osgi.service.tracker.collections.list.ServiceTrackerList;
 import com.liferay.osgi.service.tracker.collections.list.ServiceTrackerListFactory;
 import com.liferay.petra.lang.CentralizedThreadLocal;
 import com.liferay.petra.string.StringBundler;
+import com.liferay.petra.string.StringPool;
 import com.liferay.portal.instance.lifecycle.BasePortalInstanceLifecycleListener;
 import com.liferay.portal.instance.lifecycle.EveryNodeEveryStartup;
 import com.liferay.portal.instance.lifecycle.PortalInstanceLifecycleListener;
 import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.feature.flag.FeatureFlagManagerUtil;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.model.Company;
@@ -176,6 +180,25 @@ public class SystemObjectDefinitionManagerPortalInstanceLifecycleListener
 						addOrUpdateSystemObjectDefinition(
 							companyId, objectFolder.getObjectFolderId(),
 							systemObjectDefinitionManager);
+			}
+
+			if (FeatureFlagManagerUtil.isEnabled("LPD-17965")) {
+				String itemClassName =
+					objectDefinition.getClassName() + StringPool.POUND +
+						objectDefinition.getObjectDefinitionId();
+
+				_bundleContext.registerService(
+					InfoCollectionProvider.class,
+					new SystemObjectEntrySingleFormVariationInfoCollectionProvider(
+						itemClassName, objectDefinition,
+						systemObjectDefinitionManager),
+					HashMapDictionaryBuilder.<String, Object>put(
+						"class.name", objectDefinition.getClassName()
+					).put(
+						"company.id", objectDefinition.getCompanyId()
+					).put(
+						"item.class.name", itemClassName
+					).build());
 			}
 
 			_bundleContext.registerService(
