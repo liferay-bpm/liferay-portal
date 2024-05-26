eda4e0933cf6f437c3281e11c2b3326d9e72516f
Marcela Cunha <marcela.cunha@liferay.com>
2024-05-26 19:30:53 -0300

    LPD-19577 Create test for import and export a single object definition

--- modules/apps/object/object-test/src/testIntegration/java/com/liferay/object/web/internal/object/definitions/portlet/action/test/ObjectDefinitionExportImportTest.java
+++ modules/apps/object/object-test/src/testIntegration/java/com/liferay/object/web/internal/object/definitions/portlet/action/test/ObjectDefinitionExportImportTest.java
@@ -7,11 +7,14 @@ package com.liferay.object.web.internal.object.definitions.portlet.action.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.object.admin.rest.dto.v1_0.ObjectDefinition;
-import com.liferay.object.admin.rest.resource.v1_0.ObjectDefinitionResource;
+import com.liferay.object.test.util.ObjectDefinitionTestUtil;
 import com.liferay.object.web.internal.BaseExportImportTestCase;
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.json.JSONUtil;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCResourceCommand;
-import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.test.rule.FeatureFlags;
 import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 import com.liferay.portal.vulcan.pagination.Page;
@@ -19,7 +22,7 @@ import com.liferay.portal.vulcan.pagination.Pagination;
 
 import java.util.List;
 
-import org.junit.Before;
+import org.junit.Assert;
 import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
@@ -28,6 +31,7 @@ import org.junit.runner.RunWith;
 /**
  * @author Gabriel Albuquerque
  */
+@FeatureFlags("LPS-187142")
 @RunWith(Arquillian.class)
 public class ObjectDefinitionExportImportTest extends BaseExportImportTestCase {
 
@@ -35,18 +39,6 @@ public class ObjectDefinitionExportImportTest extends BaseExportImportTestCase {
 	@Rule
 	public static final LiferayIntegrationTestRule liferayIntegrationTestRule =
 		new LiferayIntegrationTestRule();
-
-	@Before
-	public void setUp() throws Exception {
-		user = TestPropsValues.getUser();
-
-		ObjectDefinitionResource.Builder builder =
-			_objectDefinitionResourceFactory.create();
-
-		_objectDefinitionResource = builder.user(
-			user
-		).build();
-	}
 
 	@Test
 	public void testExportImportObjectDefinition() throws Exception {
@@ -66,6 +58,100 @@ public class ObjectDefinitionExportImportTest extends BaseExportImportTestCase {
 			"test-object-definition.portuguese-default-locale.json",
 			"test-object-definition.site-default-locale.json",
 			"TESTOBJECTDEFINITIONPORTUGUESE", "TestObjectDefinitionPortuguese");
+
+		String externalReferenceCode = RandomTestUtil.randomString();
+		String name = ObjectDefinitionTestUtil.getRandomName();
+
+		String publishedObjectDefinitionJSON = jsonFactory.createJSONObject(
+			defaultObjectDefinitionJSON
+		).put(
+			"active", true
+		).put(
+			"externalReferenceCode", externalReferenceCode
+		).put(
+			"name", name
+		).put(
+			"status",
+			jsonFactory.createJSONObject(
+			).put(
+				"code", 0
+			).put(
+				"label", "approved"
+			).put(
+				"label_i18n", "Approved"
+			)
+		).toString();
+
+		testExportImportJSON(
+			publishedObjectDefinitionJSON, publishedObjectDefinitionJSON,
+			externalReferenceCode, name);
+
+		// Cannot modify the status of published object definition
+
+		testFailedImportJSON(
+			jsonFactory.createJSONObject(
+				publishedObjectDefinitionJSON
+			).put(
+				"status",
+				JSONUtil.put(
+					"code", 2
+				).put(
+					"label", "draft"
+				).put(
+					"label_i18n", "Draft"
+				)
+			).toString(),
+			read("test-object-definition.status-error-message.json"),
+			externalReferenceCode, name);
+
+		// Import bound object definitions one by one
+
+		String objectDefinition1JSON = jsonFactory.createJSONObject(
+			defaultObjectDefinitionJSON
+		).put(
+			"externalReferenceCode", "TESTOBJECTDEFINITION1"
+		).put(
+			"name", "TestObjectDefinition1"
+		).put(
+			"objectRelationships",
+			JSONUtil.put(
+				createOneToManyObjectRelationship(
+					"TESTOBJECTDEFINITION1", "TESTOBJECTDEFINITION2",
+					"TESTOBJECTDEFINITION2", "objectRelationship1"))
+		).put(
+			"rootObjectDefinitionExternalReferenceCode", "TESTOBJECTDEFINITION1"
+		).toString();
+
+		testExportImportJSON(
+			objectDefinition1JSON, objectDefinition1JSON,
+			"TESTOBJECTDEFINITION1", "TestObjectDefinition1");
+
+		Assert.assertNotNull(
+			objectDefinitionResource.getObjectDefinitionByExternalReferenceCode(
+				"TESTOBJECTDEFINITION2"));
+
+		JSONObject objectDefinition2JSONObject = jsonFactory.createJSONObject(
+			defaultObjectDefinitionJSON
+		).put(
+			"externalReferenceCode", "TESTOBJECTDEFINITION2"
+		).put(
+			"name", "TestObjectDefinition2"
+		).put(
+			"rootObjectDefinitionExternalReferenceCode", "TESTOBJECTDEFINITION1"
+		);
+
+		testExportImportJSON(
+			objectDefinition2JSONObject.toString(),
+			objectDefinition2JSONObject.put(
+				"objectFields",
+				JSONUtil.concat(
+					objectDefinition2JSONObject.getJSONArray("objectFields"),
+					JSONUtil.putAll(
+						JSONUtil.put(
+							"name",
+							"r_objectRelationship1_c_testObjectDefinition1Id")))
+			).toString(),
+			"TESTOBJECTDEFINITION2", "TestObjectDefinition2");
 	}
 
 	@Override
@@ -109,7 +195,7 @@ public class ObjectDefinitionExportImportTest extends BaseExportImportTestCase {
 		throws Exception {
 
 		Page<ObjectDefinition> page =
-			_objectDefinitionResource.getObjectDefinitionsPage(
+			objectDefinitionResource.getObjectDefinitionsPage(
 				name, null, null, Pagination.of(1, 1), null);
 
 		List<ObjectDefinition> items = (List<ObjectDefinition>)page.getItems();
@@ -126,10 +212,5 @@ public class ObjectDefinitionExportImportTest extends BaseExportImportTestCase {
 		filter = "mvc.command.name=/object_definitions/export_object_definition"
 	)
 	private MVCResourceCommand _mvcResourceCommand;
-
-	private ObjectDefinitionResource _objectDefinitionResource;
-
-	@Inject
-	private ObjectDefinitionResource.Factory _objectDefinitionResourceFactory;
 
 }
