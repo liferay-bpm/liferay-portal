e569a107b1f7639c3a8f446d4f2a0e9ce37e7ccf
Pedro Leite <pedro.leite@liferay.com>
2024-06-11 11:22:17 -0300

    LPD-27588 Apply epic FF - LPD-21580

--- modules/apps/notification/notification-service/src/main/java/com/liferay/notification/internal/type/EmailNotificationType.java
+++ modules/apps/notification/notification-service/src/main/java/com/liferay/notification/internal/type/EmailNotificationType.java
@@ -149,9 +149,7 @@ public class EmailNotificationType extends BaseNotificationType {
 			NotificationRecipientSettingConstants.NAME_FROM,
 			NotificationRecipientSettingConstants.NAME_FROM_NAME,
 			NotificationRecipientSettingConstants.NAME_SINGLE_RECIPIENT,
-			NotificationRecipientSettingConstants.NAME_TO,
-			NotificationRecipientSettingConstants.
-				NAME_USE_MOST_RELEVANT_LOCALE_FOR_GUEST_USERS);
+			NotificationRecipientSettingConstants.NAME_TO);
 
 		if (FeatureFlagManagerUtil.isEnabled("LPD-11165")) {
 			names.addAll(
@@ -159,6 +157,12 @@ public class EmailNotificationType extends BaseNotificationType {
 					NotificationRecipientSettingConstants.NAME_BCC_TYPE,
 					NotificationRecipientSettingConstants.NAME_CC_TYPE,
 					NotificationRecipientSettingConstants.NAME_TO_TYPE));
+		}
+
+		if (FeatureFlagManagerUtil.isEnabled("LPD-21580")) {
+			names.add(
+				NotificationRecipientSettingConstants.
+					NAME_USE_MOST_RELEVANT_LOCALE_FOR_GUEST_USERS);
 		}
 
 		return names;
