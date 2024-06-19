20073778468e3f35169efcb675a8522cd186f32e
Pedro Leite <pedro.leite@liferay.com>
2024-06-19 17:09:02 -0300

    LPD-27588 Create new upgrade process

--- modules/apps/notification/notification-service/src/main/java/com/liferay/notification/internal/upgrade/v3_10_2/NotificationRecipientSettingUpgradeProcess.java
+++ modules/apps/notification/notification-service/src/main/java/com/liferay/notification/internal/upgrade/v3_10_2/NotificationRecipientSettingUpgradeProcess.java
@@ -1,28 +1,29 @@
 /**
- * SPDX-FileCopyrightText: (c) 2023 Liferay, Inc. https://liferay.com
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.notification.internal.upgrade.v3_9_0;
+package com.liferay.notification.internal.upgrade.v3_10_2;
 
 import com.liferay.notification.constants.NotificationRecipientSettingConstants;
 import com.liferay.notification.internal.upgrade.BaseNotificationRecipientSettingUpgradeProcess;
 import com.liferay.petra.string.StringPool;
 
 /**
- * @author Selton Guedes
+ * @author Pedro Leite
  */
 public class NotificationRecipientSettingUpgradeProcess
 	extends BaseNotificationRecipientSettingUpgradeProcess {
 
 	@Override
 	protected String getNotificationRecipientSettingName() {
-		return NotificationRecipientSettingConstants.NAME_SINGLE_RECIPIENT;
+		return NotificationRecipientSettingConstants.
+			NAME_USE_MOST_RELEVANT_LOCALE_FOR_GUEST_USERS;
 	}
 
 	@Override
 	protected String getNotificationRecipientSettingValue() {
-		return StringPool.TRUE;
+		return StringPool.FALSE;
 	}
 
 }
