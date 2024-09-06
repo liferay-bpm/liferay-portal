54c2d59438ad71446ded1cb427bc00cd733c7366
Paulo Albuquerque <paulo.albuquerque@liferay.com>
2024-09-06 16:22:47 -0300

    LPD-30142 Primary key from modifiable system object definition have to start with l_ instead of c_

--- modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectDefinitionLocalServiceImpl.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectDefinitionLocalServiceImpl.java
@@ -1769,7 +1769,11 @@ public class ObjectDefinitionLocalServiceImpl
 			return pkObjectFieldName;
 		}
 
-		return pkObjectFieldName = "c_" + pkObjectFieldName;
+		if (modifiable && system) {
+			return "l_" + pkObjectFieldName;
+		}
+
+		return "c_" + pkObjectFieldName;
 	}
 
 	private boolean _hasObjectField(
