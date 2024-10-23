915d35f3f2172128a12b55ed4f2a2f2756444247
Richard Jeremias <richard.jeremias@liferay.com>
2024-10-23 16:15:42 -0300

    LPD-39817 Adapt actions on Resource Layer to consider an ERC

--- modules/apps/data-engine/data-engine-rest-impl/src/main/java/com/liferay/data/engine/rest/internal/dto/v2_0/util/DataDefinitionUtil.java
+++ modules/apps/data-engine/data-engine-rest-impl/src/main/java/com/liferay/data/engine/rest/internal/dto/v2_0/util/DataDefinitionUtil.java
@@ -17,6 +17,7 @@ import com.liferay.dynamic.data.mapping.service.DDMStructureLayoutLocalService;
 import com.liferay.dynamic.data.mapping.service.DDMStructureLocalService;
 import com.liferay.dynamic.data.mapping.spi.converter.SPIDDMFormRuleConverter;
 import com.liferay.petra.function.transform.TransformUtil;
+import com.liferay.portal.kernel.feature.flag.FeatureFlagManagerUtil;
 import com.liferay.portal.kernel.language.LanguageUtil;
 
 import javax.servlet.http.HttpServletRequest;
@@ -79,6 +80,11 @@ public class DataDefinitionUtil {
 				setDescription(
 					() -> LocalizedValueUtil.toStringObjectMap(
 						ddmStructure.getDescriptionMap()));
+
+				if (FeatureFlagManagerUtil.isEnabled("LPD-34651")) {
+					setExternalReferenceCode(() -> ddmStructure.getExternalReferenceCode());
+				}
+
 				setId(ddmStructure::getStructureId);
 				setName(
 					() -> LocalizedValueUtil.toStringObjectMap(
