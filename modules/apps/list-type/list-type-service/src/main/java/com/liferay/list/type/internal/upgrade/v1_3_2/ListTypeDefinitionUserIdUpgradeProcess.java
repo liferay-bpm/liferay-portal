07e68f07c98752bafa591a768f2e55de57c3c8b1
Igor Costa <igor.costa@liferay.com>
2024-11-22 17:29:45 -0300

    LPD-22831 Create upgrade process for list type with deleted user reference

---
+++ modules/apps/list-type/list-type-service/src/main/java/com/liferay/list/type/internal/upgrade/v1_3_2/ListTypeDefinitionUserIdUpgradeProcess.java
@@ -0,0 +1,27 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.list.type.internal.upgrade.v1_3_2;
+
+import com.liferay.portal.kernel.service.UserLocalService;
+
+/**
+ * @author Igor Costa
+ */
+public class ListTypeDefinitionUserIdUpgradeProcess
+	extends BaseUserIdUpgradeProcess {
+
+	public ListTypeDefinitionUserIdUpgradeProcess(
+		UserLocalService userLocalService) {
+
+		super(userLocalService);
+	}
+
+	@Override
+	protected void doUpgrade() throws Exception {
+		upgradeUserId("listTypeDefinitionId", "ListTypeDefinition");
+	}
+
+}
