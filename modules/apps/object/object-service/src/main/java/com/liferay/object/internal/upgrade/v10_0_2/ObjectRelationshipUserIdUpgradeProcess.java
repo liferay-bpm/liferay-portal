80efddd262b60e7157c22e42b9d7f6769249da92
Igor Costa <igor.costa@liferay.com>
2024-11-22 15:16:03 -0300

    LPD-22831 Create upgrade process for objects with deleted user reference

---
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/internal/upgrade/v10_0_2/ObjectRelationshipUserIdUpgradeProcess.java
@@ -0,0 +1,27 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.object.internal.upgrade.v10_0_2;
+
+import com.liferay.portal.kernel.service.UserLocalService;
+
+/**
+ * @author Igor Costa
+ */
+public class ObjectRelationshipUserIdUpgradeProcess
+	extends BaseUserIdUpgradeProcess {
+
+	public ObjectRelationshipUserIdUpgradeProcess(
+		UserLocalService userLocalService) {
+
+		super(userLocalService);
+	}
+
+	@Override
+	protected void doUpgrade() throws Exception {
+		upgradeUserId("objectRelationshipId", "ObjectRelationship");
+	}
+
+}
