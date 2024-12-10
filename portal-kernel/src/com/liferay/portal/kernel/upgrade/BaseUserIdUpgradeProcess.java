a17cea703dbb5dd4e75208b8a6ea4a49d1a59098
Igor Costa <igor.costa@liferay.com>
2024-12-10 08:00:12 -0300

    LPD-22831 Move BaseUserIdUpgradeProcess to portal-kernel

---
+++ portal-kernel/src/com/liferay/portal/kernel/upgrade/BaseUserIdUpgradeProcess.java
@@ -0,0 +1,66 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.portal.kernel.upgrade;
+
+import com.liferay.petra.string.StringBundler;
+import com.liferay.portal.kernel.dao.jdbc.AutoBatchPreparedStatementUtil;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.service.UserLocalService;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+/**
+ * @author Igor Costa
+ */
+public abstract class BaseUserIdUpgradeProcess extends UpgradeProcess {
+
+	public BaseUserIdUpgradeProcess(UserLocalService userLocalService) {
+		_userLocalService = userLocalService;
+	}
+
+	protected void upgradeUserId(String columnName, String tableName)
+		throws Exception {
+
+		try (PreparedStatement preparedStatement1 = connection.prepareStatement(
+				StringBundler.concat(
+					"select ", columnName, ", companyId, userId from ",
+					tableName));
+			PreparedStatement preparedStatement2 =
+				AutoBatchPreparedStatementUtil.concurrentAutoBatch(
+					connection,
+					StringBundler.concat(
+						"update ", tableName, " set userId = ? where ",
+						columnName, " = ?"));
+			ResultSet resultSet = preparedStatement1.executeQuery()) {
+
+			while (resultSet.next()) {
+				if (_userLocalService.fetchUser(resultSet.getLong("userId")) !=
+						null) {
+
+					continue;
+				}
+
+				User defaultServiceAccountUser =
+					_userLocalService.fetchUserByScreenName(
+						resultSet.getLong("companyId"),
+						"default-service-account");
+
+				preparedStatement2.setLong(
+					1, defaultServiceAccountUser.getUserId());
+
+				preparedStatement2.setLong(2, resultSet.getLong(columnName));
+
+				preparedStatement2.addBatch();
+			}
+
+			preparedStatement2.executeBatch();
+		}
+	}
+
+	private final UserLocalService _userLocalService;
+
+}
