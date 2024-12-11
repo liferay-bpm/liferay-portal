9769e4ac40dc72581a796cb6381d62277c7c16a8
Richard Jeremias <richard.jeremias@liferay.com>
2024-12-11 10:27:17 -0300

    LPD-44743 Update constructor to receive groupId
    # breaking
    
    ## What modules/apps/object/object-api/src/main/java/com/liferay/object/field/filter/parser/ObjectFieldFilterContext.java
    
    Adapt constructor of Object Filter to receive the groupID from clients
    
    ## Why
    
    The groupID is now necessary for getting objectEntries, so it is necessary to have this
    values on classes like OneToManyObjectFieldFilterStrategy.
    
    ----

--- modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectViewFilterColumnLocalServiceImpl.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectViewFilterColumnLocalServiceImpl.java
@@ -148,7 +148,7 @@ public class ObjectViewFilterColumnLocalServiceImpl
 				_objectFieldFilterContributorRegistry.
 					getObjectFieldFilterContributor(
 						new ObjectFieldFilterContext(
-							null, objectField.getObjectDefinitionId(),
+							0, null, objectField.getObjectDefinitionId(),
 							objectViewFilterColumn));
 
 			objectFieldFilterContributor.validate();
