cab12817a786dfde22363a1c0b4e5785748fca3d
victorKammerer <vko@cin.ufpe.br>
2025-01-29 11:52:51 -0300

    LPD-46875 Test to cover that user cannot delete metadata object field

--- modules/apps/object/object-test/src/testIntegration/java/com/liferay/object/service/test/ObjectFieldLocalServiceTest.java
+++ modules/apps/object/object-test/src/testIntegration/java/com/liferay/object/service/test/ObjectFieldLocalServiceTest.java
@@ -1356,7 +1356,7 @@ public class ObjectFieldLocalServiceTest {
 	@Test
 	public void testDeleteObjectField() throws Exception {
 
-		// Delete object field from custom object definition
+		// Delete metadata object field
 
 		ObjectDefinition customObjectDefinition =
 			ObjectDefinitionTestUtil.addCustomObjectDefinition(
@@ -1365,6 +1365,21 @@ public class ObjectFieldLocalServiceTest {
 					ObjectFieldUtil.createObjectField(
 						ObjectFieldConstants.BUSINESS_TYPE_TEXT,
 						ObjectFieldConstants.DB_TYPE_STRING, "able")));
+
+		ObjectField externalReferenceCodeObjectField =
+			_objectFieldLocalService.fetchObjectField(
+				customObjectDefinition.getObjectDefinitionId(),
+				"externalReferenceCode");
+
+		AssertUtils.assertFailure(
+			RequiredObjectFieldException.class,
+			String.format(
+				"The object field \"%s\" cannot be deleted",
+				externalReferenceCodeObjectField.getName()),
+			() -> _objectFieldLocalService.deleteObjectField(
+				externalReferenceCodeObjectField));
+
+		// Delete object field from custom object definition
 
 		_assertDeleteObjectField(false, customObjectDefinition, "able");
 
