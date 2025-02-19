2412b280ae25437c0c2d01b30301ab778e7eb145
Gabriel Albuquerque <gabriel.albuquerque@liferay.com>
2025-02-19 16:29:57 -0300

    LPD-49577 Implement the resource methods from the validation endpoint

--- modules/apps/object/object-rest-impl/src/main/java/com/liferay/object/rest/internal/resource/v1_0/ObjectEntryResourceImpl.java
+++ modules/apps/object/object-rest-impl/src/main/java/com/liferay/object/rest/internal/resource/v1_0/ObjectEntryResourceImpl.java
@@ -5,9 +5,13 @@
 
 package com.liferay.object.rest.internal.resource.v1_0;
 
+import com.liferay.object.exception.ObjectValidationRuleEngineException;
 import com.liferay.object.model.ObjectDefinition;
 import com.liferay.object.model.ObjectRelationship;
 import com.liferay.object.rest.dto.v1_0.ObjectEntry;
+import com.liferay.object.rest.dto.v1_0.ValidateError;
+import com.liferay.object.rest.dto.v1_0.ValidateRequest;
+import com.liferay.object.rest.dto.v1_0.ValidateResult;
 import com.liferay.object.rest.manager.v1_0.DefaultObjectEntryManager;
 import com.liferay.object.rest.manager.v1_0.DefaultObjectEntryManagerProvider;
 import com.liferay.object.rest.manager.v1_0.ObjectEntryManager;
@@ -37,6 +41,7 @@ import com.liferay.portal.vulcan.pagination.Pagination;
 
 import java.io.Serializable;
 
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Map;
 
@@ -353,6 +358,21 @@ public class ObjectEntryResourceImpl extends BaseObjectEntryResourceImpl {
 	}
 
 	@Override
+	public ValidateResult postScopeScopeKeyValidate(
+			String scopeKey, ValidateRequest validateRequest)
+		throws Exception {
+
+		return _validateObjectEntry(scopeKey, validateRequest);
+	}
+
+	@Override
+	public ValidateResult postValidate(ValidateRequest validateRequest)
+		throws Exception {
+
+		return _validateObjectEntry(null, validateRequest);
+	}
+
+	@Override
 	public ObjectEntry putByExternalReferenceCode(
 			String externalReferenceCode, ObjectEntry objectEntry)
 		throws Exception {
@@ -651,6 +671,55 @@ public class ObjectEntryResourceImpl extends BaseObjectEntryResourceImpl {
 		}
 
 		return null;
+	}
+
+	private ValidateResult _validateObjectEntry(
+			String scopeKey, ValidateRequest validateRequest)
+		throws Exception {
+
+		ObjectEntryManager objectEntryManager =
+			_objectEntryManagerRegistry.getObjectEntryManager(
+				_objectDefinition.getStorageType());
+
+		try {
+			objectEntryManager.validateObjectEntry(
+				_getDTOConverterContext(null), _objectDefinition,
+				validateRequest.getEntry(),
+				Arrays.asList(validateRequest.getValidationKeys()), scopeKey);
+		}
+		catch (ObjectValidationRuleEngineException
+					objectValidationRuleEngineException) {
+
+			return new ValidateResult() {
+				{
+					setSuccess(() -> false);
+					setValidateErrors(
+						() -> transformToArray(
+							objectValidationRuleEngineException.
+								getObjectValidationRuleResults(),
+							objectValidationRuleResult -> new ValidateError() {
+								{
+									setErrorMessage(
+										objectValidationRuleResult::
+											getErrorMessage);
+									setObjectFieldName(
+										objectValidationRuleResult::
+											getObjectFieldName);
+									setValidationKey(
+										objectValidationRuleResult::
+											getValidationKey);
+								}
+							},
+							ValidateError.class));
+				}
+			};
+		}
+
+		return new ValidateResult() {
+			{
+				setSuccess(() -> true);
+			}
+		};
 	}
 
 	private final DTOConverterRegistry _dtoConverterRegistry;
