eff561c2776227028e36863ea707db3ca98a3677
Carolina Barbosa <carolina.barbosa@liferay.com>
2025-03-17 08:22:36 -0300

    LPD-51438 Display the default language object title if no translation exists

--- modules/apps/object/object-web/src/main/java/com/liferay/object/web/internal/layout/display/page/ObjectEntryLayoutDisplayPageObjectProvider.java
+++ modules/apps/object/object-web/src/main/java/com/liferay/object/web/internal/layout/display/page/ObjectEntryLayoutDisplayPageObjectProvider.java
@@ -16,6 +16,7 @@ import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.PortalUtil;
+import com.liferay.portal.kernel.util.Validator;
 
 import java.util.Locale;
 
@@ -90,7 +91,15 @@ public class ObjectEntryLayoutDisplayPageObjectProvider
 		}
 
 		try {
-			return _objectEntry.getTitleValue(LocaleUtil.toLanguageId(locale));
+			String titleValue = _objectEntry.getTitleValue(
+				LocaleUtil.toLanguageId(locale));
+
+			if (Validator.isNull(titleValue)) {
+				titleValue = _objectEntry.getTitleValue(
+					_objectEntry.getDefaultLanguageId());
+			}
+
+			return titleValue;
 		}
 		catch (PortalException portalException) {
 			if (_log.isDebugEnabled()) {
