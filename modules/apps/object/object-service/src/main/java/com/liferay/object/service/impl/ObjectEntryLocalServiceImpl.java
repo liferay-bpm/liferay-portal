ce50a5449bbc6fd6e535b07ff30a0861b47d5147
Marcela Cunha <marcela.cunha@liferay.com>
2025-03-25 09:58:09 -0300

    LPD-50184 Use the correct method when deleting an object entry

--- modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectEntryLocalServiceImpl.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectEntryLocalServiceImpl.java
@@ -641,7 +641,7 @@ public class ObjectEntryLocalServiceImpl
 			objectEntry.getGroupId(), objectDefinition.getObjectDefinitionId(),
 			objectEntry.getPrimaryKey());
 
-		_objectEntryVersionLocalService.deleteObjectEntryVersion(
+		_objectEntryVersionLocalService.deleteObjectEntryVersions(
 			objectEntry.getObjectEntryId());
 
 		if (!objectDefinition.isActive() ||
