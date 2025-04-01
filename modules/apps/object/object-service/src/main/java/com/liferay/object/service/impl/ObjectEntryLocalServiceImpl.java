14fcefaa57a242bbe83433cab4617053b37a224d
Feliphe Marinho <feliphemarinho18@gmail.com>
2025-04-01 16:59:18 -0300

    LPD-52432 test: test validateRequiredValues method using boolean object field used case

--- modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectEntryLocalServiceImpl.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectEntryLocalServiceImpl.java
@@ -5938,16 +5938,11 @@ public class ObjectEntryLocalServiceImpl
 				continue;
 			}
 
+			_validateRequiredValues(
+				objectField, serviceContext,
+				localizedValues.get(objectDefinition.getDefaultLanguageId()));
+
 			for (Map.Entry<String, String> entry : localizedValues.entrySet()) {
-				if (Objects.equals(
-						objectDefinition.getDefaultLanguageId(),
-						entry.getKey())) {
-
-					_validateRequiredValues(
-						objectField, serviceContext,
-						values.get(objectField.getName()));
-				}
-
 				_validateValues(
 					dlFileEntryIds, existingObjectEntry, guestUser, groupId,
 					objectDefinition, objectEntryId, objectField,
