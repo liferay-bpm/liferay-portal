0b032373bb1b3b9a5e8f94fd62ebf5d3616937a6
Pedro Leite <pedro.leite@liferay.com>
2025-04-11 15:08:36 -0300

    LPD-52474 Build Rest

--- modules/apps/headless/headless-admin-workflow/headless-admin-workflow-api/src/main/java/com/liferay/headless/admin/workflow/dto/v1_0/WorkflowLog.java
+++ modules/apps/headless/headless-admin-workflow/headless-admin-workflow-api/src/main/java/com/liferay/headless/admin/workflow/dto/v1_0/WorkflowLog.java
@@ -968,9 +968,9 @@ public class WorkflowLog implements Serializable {
 	@GraphQLName("Type")
 	public static enum Type {
 
-		NODE_ENTRY("NodeEntry"), TASK_ASSIGN("TaskAssign"),
-		TASK_COMPLETION("TaskCompletion"), TASK_UPDATE("TaskUpdate"),
-		TRANSITION("Transition");
+		INSTANCE_FAIL("InstanceFail"), NODE_ENTRY("NodeEntry"),
+		TASK_ASSIGN("TaskAssign"), TASK_COMPLETION("TaskCompletion"),
+		TASK_UPDATE("TaskUpdate"), TRANSITION("Transition");
 
 		@JsonCreator
 		public static Type create(String value) {
