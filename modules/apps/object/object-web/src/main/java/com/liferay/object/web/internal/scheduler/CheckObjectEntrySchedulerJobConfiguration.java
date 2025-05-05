1328fcfc3eb02d8ebe6be710c3cd70c9d69f89d9
Jhosseph Gonzalez <jhossephgonzalez@gmail.com>
2025-05-05 10:20:14 -0300

    LPD-50159 Create CheckObjectEntrySchedulerJobConfiguration class

---
+++ modules/apps/object/object-web/src/main/java/com/liferay/object/web/internal/scheduler/CheckObjectEntrySchedulerJobConfiguration.java
@@ -0,0 +1,53 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.object.web.internal.scheduler;
+
+import com.liferay.object.configuration.ObjectEntryScheduleConfiguration;
+import com.liferay.petra.function.UnsafeRunnable;
+import com.liferay.portal.configuration.metatype.bnd.util.ConfigurableUtil;
+import com.liferay.portal.kernel.scheduler.SchedulerJobConfiguration;
+import com.liferay.portal.kernel.scheduler.TimeUnit;
+import com.liferay.portal.kernel.scheduler.TriggerConfiguration;
+
+import java.util.Map;
+
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Jhosseph Gonzalez
+ */
+@Component(
+	configurationPid = "com.liferay.object.configuration.ObjectEntryScheduleConfiguration",
+	service = SchedulerJobConfiguration.class
+)
+public class CheckObjectEntrySchedulerJobConfiguration
+	implements SchedulerJobConfiguration {
+
+	@Override
+	public UnsafeRunnable<Exception> getJobExecutorUnsafeRunnable() {
+		return null;
+	}
+
+	@Override
+	public TriggerConfiguration getTriggerConfiguration() {
+		return _triggerConfiguration;
+	}
+
+	@Activate
+	protected void activate(Map<String, Object> properties) {
+		_objectEntryScheduleConfiguration = ConfigurableUtil.createConfigurable(
+			ObjectEntryScheduleConfiguration.class, properties);
+
+		_triggerConfiguration = TriggerConfiguration.createTriggerConfiguration(
+			_objectEntryScheduleConfiguration.checkInterval(), TimeUnit.MINUTE);
+	}
+
+	private volatile ObjectEntryScheduleConfiguration
+		_objectEntryScheduleConfiguration;
+	private TriggerConfiguration _triggerConfiguration;
+
+}
