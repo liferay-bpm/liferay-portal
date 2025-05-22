977bf65eb644e45f4d9a3612d21725c341cbc63d
Mario Gomes <mario.gomes@liferay.com>
2025-05-22 16:37:31 -0300

    LPP-58919 : Change isURL behavior for whitespace acceptance

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-evaluator-impl/src/main/java/com/liferay/dynamic/data/mapping/form/evaluator/internal/function/IsURLFunction.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-evaluator-impl/src/main/java/com/liferay/dynamic/data/mapping/form/evaluator/internal/function/IsURLFunction.java
@@ -23,7 +23,14 @@ public class IsURLFunction
 	@Override
 	public Boolean apply(Object parameter) {
 		if ((parameter == null) ||
-			Objects.equals(parameter.toString(), Http.HTTP_WITH_SLASH) ||
+			parameter.toString(
+			).trim(
+			).isEmpty()) {
+
+			return true;
+		}
+
+		if (Objects.equals(parameter.toString(), Http.HTTP_WITH_SLASH) ||
 			Objects.equals(parameter.toString(), Http.HTTPS_WITH_SLASH)) {
 
 			return false;
