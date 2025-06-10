e70c458c0be0b17886c4b31a0647adcc12be06d2
Jhosseph Gonzalez <jhossephgonzalez@gmail.com>
2025-06-10 16:11:58 -0300

    LPD-57301 feature: create _checkObjectEntriesByDisplayDate and apply

--- modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectEntryLocalServiceImpl.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectEntryLocalServiceImpl.java
@@ -604,6 +604,8 @@ public class ObjectEntryLocalServiceImpl
 			key -> new Date(
 				date.getTime() - _getObjectEntryCheckInterval(companyId)));
 
+		_checkObjectEntriesByDisplayDate(companyId, date);
+
 		_checkObjectEntriesByExpirationDate(companyId, date);
 
 		_checkObjectEntriesByReviewDate(companyId, date);
@@ -2517,6 +2519,36 @@ public class ObjectEntryLocalServiceImpl
 					setStrictAdd(true);
 				}
 			});
+	}
+
+	private void _checkObjectEntriesByDisplayDate(
+			long companyId, Date currentDate)
+		throws PortalException {
+
+		List<ObjectEntry> objectEntries = objectEntryPersistence.dslQuery(
+			DSLQueryFactoryUtil.select(
+				ObjectEntryTable.INSTANCE
+			).from(
+				ObjectEntryTable.INSTANCE
+			).where(
+				ObjectEntryTable.INSTANCE.companyId.eq(
+					companyId
+				).and(
+					ObjectEntryTable.INSTANCE.displayDate.gte(
+						_companyIdPreviousCheckDate.get(companyId))
+				).and(
+					ObjectEntryTable.INSTANCE.displayDate.lte(currentDate)
+				).and(
+					ObjectEntryTable.INSTANCE.status.eq(
+						WorkflowConstants.STATUS_SCHEDULED)
+				)
+			));
+
+		for (ObjectEntry objectEntry : objectEntries) {
+			updateStatus(
+				objectEntry.getUserId(), objectEntry,
+				WorkflowConstants.STATUS_APPROVED, new ServiceContext());
+		}
 	}
 
 	private void _checkObjectEntriesByExpirationDate(long companyId, Date date)
