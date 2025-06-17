89fca6b6c0a39ace0f278d9ef052ee80d8fb6bae
Marcos Martins <marcos.martins@liferay.com>
2025-06-17 17:21:56 -0300

    LPD-50670 | LPD-57256 add support to an array of groupIds

--- modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectEntryLocalServiceImpl.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectEntryLocalServiceImpl.java
@@ -1286,9 +1286,9 @@ public class ObjectEntryLocalServiceImpl
 	}
 
 	public List<Long> getPrimaryKeys(
-			long groupId, long companyId, long userId, long objectDefinitionId,
-			Predicate predicate, String search, int start, int end,
-			Sort[] sorts)
+			Long[] groupIds, long companyId, long userId,
+			long objectDefinitionId, Predicate predicate, String search,
+			int start, int end, Sort[] sorts)
 		throws PortalException {
 
 		DynamicObjectDefinitionLocalizationTable
@@ -1332,18 +1332,35 @@ public class ObjectEntryLocalServiceImpl
 				objectDefinitionId
 			).and(
 				() -> {
-					if (groupId == 0) {
+					if (ArrayUtil.isEmpty(groupIds)) {
 						return null;
 					}
 
-					return ObjectEntryTable.INSTANCE.groupId.eq(groupId);
+					return ObjectEntryTable.INSTANCE.groupId.in(groupIds);
 				}
 			).and(
 				Predicate.withParentheses(
 					_fillPredicate(objectDefinitionId, predicate, search))
 			).and(
-				_getPermissionWherePredicate(
-					dynamicObjectDefinitionTable, groupId)
+				() -> {
+					Predicate permissionWherePredicate = null;
+
+					for (Long groupId : groupIds) {
+						if (permissionWherePredicate == null) {
+							permissionWherePredicate =
+								_getPermissionWherePredicate(
+									dynamicObjectDefinitionTable, groupId);
+						}
+						else {
+							permissionWherePredicate =
+								permissionWherePredicate.or(
+									_getPermissionWherePredicate(
+										dynamicObjectDefinitionTable, groupId));
+						}
+					}
+
+					return permissionWherePredicate;
+				}
 			)
 		).limit(
 			start, end
@@ -1595,15 +1612,14 @@ public class ObjectEntryLocalServiceImpl
 
 		return TransformUtil.transform(
 			getPrimaryKeys(
-				groupId, companyId, userId, objectDefinitionId, predicate,
-				search, start, end, sorts),
+				new Long[] {groupId}, companyId, userId, objectDefinitionId,
+				predicate, search, start, end, sorts),
 			this::getValues);
 	}
 
-	@Override
 	public int getValuesListCount(
-			long groupId, long companyId, long userId, long objectDefinitionId,
-			Predicate predicate, String search)
+			Long[] groupIds, long companyId, long userId,
+			long objectDefinitionId, Predicate predicate, String search)
 		throws PortalException {
 
 		DynamicObjectDefinitionLocalizationTable
@@ -1647,18 +1663,35 @@ public class ObjectEntryLocalServiceImpl
 				objectDefinitionId
 			).and(
 				() -> {
-					if (groupId == 0) {
+					if (ArrayUtil.isEmpty(groupIds)) {
 						return null;
 					}
 
-					return ObjectEntryTable.INSTANCE.groupId.eq(groupId);
+					return ObjectEntryTable.INSTANCE.groupId.in(groupIds);
 				}
 			).and(
 				Predicate.withParentheses(
 					_fillPredicate(objectDefinitionId, predicate, search))
 			).and(
-				_getPermissionWherePredicate(
-					dynamicObjectDefinitionTable, groupId)
+				() -> {
+					Predicate permissionWherePredicate = null;
+
+					for (Long groupId : groupIds) {
+						if (permissionWherePredicate == null) {
+							permissionWherePredicate =
+								_getPermissionWherePredicate(
+									dynamicObjectDefinitionTable, groupId);
+						}
+						else {
+							permissionWherePredicate =
+								permissionWherePredicate.or(
+									_getPermissionWherePredicate(
+										dynamicObjectDefinitionTable, groupId));
+						}
+					}
+
+					return permissionWherePredicate;
+				}
 			)
 		);
 
