4f6ee7d19a7c3c8ac6db90ee9376f6cb6e0a654b
Carolina Barbosa <carolina.barbosa@liferay.com>
2025-06-19 15:39:15 -0300

    LPD-58490 When importing an entry, if the Role related to a permission does not exist, we need to create a dummy reference with “incomplete” status for the Role

--- modules/apps/object/object-rest-impl/src/main/java/com/liferay/object/rest/internal/manager/v1_0/DefaultObjectEntryManagerImpl.java
+++ modules/apps/object/object-rest-impl/src/main/java/com/liferay/object/rest/internal/manager/v1_0/DefaultObjectEntryManagerImpl.java
@@ -78,6 +78,7 @@ import com.liferay.portal.kernel.model.ExternalReferenceCodeModel;
 import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.model.GroupedModel;
 import com.liferay.portal.kernel.model.PersistedModel;
+import com.liferay.portal.kernel.model.role.RoleConstants;
 import com.liferay.portal.kernel.search.BooleanClauseOccur;
 import com.liferay.portal.kernel.search.Field;
 import com.liferay.portal.kernel.search.Sort;
@@ -121,9 +122,12 @@ import com.liferay.portal.vulcan.fields.NestedFieldsSupplier;
 import com.liferay.portal.vulcan.pagination.Page;
 import com.liferay.portal.vulcan.pagination.Pagination;
 import com.liferay.portal.vulcan.permission.ModelPermissionsUtil;
+import com.liferay.portal.vulcan.permission.Permission;
 import com.liferay.portal.vulcan.util.ActionUtil;
 import com.liferay.portal.vulcan.util.ObjectMapperUtil;
 import com.liferay.portal.vulcan.util.SearchUtil;
+import com.liferay.roles.admin.role.type.contributor.RoleTypeContributor;
+import com.liferay.roles.admin.role.type.contributor.provider.RoleTypeContributorProvider;
 
 import jakarta.ws.rs.BadRequestException;
 import jakarta.ws.rs.core.MultivaluedMap;
@@ -1288,6 +1292,40 @@ public class DefaultObjectEntryManagerImpl
 			ObjectDefinition objectDefinition, ObjectEntry objectEntry)
 		throws Exception {
 
+		if (objectEntry.getPermissions() == null) {
+			return ServiceContextUtil.createServiceContext(
+				objectDefinition.getCompanyId(),
+				dtoConverterContext.getLocale(), null, objectEntry,
+				dtoConverterContext.getUserId());
+		}
+
+		if (LazyReferencingThreadLocal.isEnabled()) {
+			for (Permission permission : objectEntry.getPermissions()) {
+				if (Validator.isNull(
+						permission.getRoleExternalReferenceCode())) {
+
+					continue;
+				}
+
+				String className = StringPool.BLANK;
+
+				RoleTypeContributor roleTypeContributor =
+					_roleTypeContributorProvider.getRoleTypeContributor(
+						RoleConstants.getLabelType(permission.getRoleType()));
+
+				if (roleTypeContributor != null) {
+					className = roleTypeContributor.getClassName();
+				}
+
+				_roleLocalService.getOrAddIncompleteRole(
+					permission.getRoleExternalReferenceCode(),
+					objectDefinition.getCompanyId(),
+					dtoConverterContext.getUserId(), className, 0,
+					permission.getRoleName(),
+					RoleConstants.getLabelType(permission.getRoleType()));
+			}
+		}
+
 		ModelPermissions modelPermissions =
 			ModelPermissionsUtil.toModelPermissions(
 				objectDefinition.getCompanyId(), objectEntry.getPermissions(),
@@ -2410,6 +2448,9 @@ public class DefaultObjectEntryManagerImpl
 
 	@Reference
 	private RoleLocalService _roleLocalService;
+
+	@Reference
+	private RoleTypeContributorProvider _roleTypeContributorProvider;
 
 	@Reference
 	private SearchRequestBuilderFactory _searchRequestBuilderFactory;
