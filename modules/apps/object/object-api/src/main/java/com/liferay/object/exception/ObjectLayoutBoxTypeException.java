3cd5d8940a979a57afa3a8e6b98c01450f9a8290
Jhosseph Gonzalez <jhossephgonzalez@gmail.com>
2025-10-13 16:12:15 -0300

    LPD-68036 buildService
    # breaking
    
    ## What modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectLayoutLocalServiceImpl.java
    
    Rename exception class
    
    ## Why
    
    Turn it flexible for both types: Categorization and SEO
    
    ----

--- modules/apps/object/object-api/src/main/java/com/liferay/object/exception/ObjectLayoutBoxTypeException.java
+++ modules/apps/object/object-api/src/main/java/com/liferay/object/exception/ObjectLayoutBoxTypeException.java
@@ -1,5 +1,5 @@
 /**
- * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
@@ -10,23 +10,20 @@ import com.liferay.portal.kernel.exception.PortalException;
 /**
  * @author Marco Leo
  */
-public class ObjectLayoutBoxCategorizationTypeException
-	extends PortalException {
+public class ObjectLayoutBoxTypeException extends PortalException {
 
-	public ObjectLayoutBoxCategorizationTypeException() {
+	public ObjectLayoutBoxTypeException() {
 	}
 
-	public ObjectLayoutBoxCategorizationTypeException(String msg) {
+	public ObjectLayoutBoxTypeException(String msg) {
 		super(msg);
 	}
 
-	public ObjectLayoutBoxCategorizationTypeException(
-		String msg, Throwable throwable) {
-
+	public ObjectLayoutBoxTypeException(String msg, Throwable throwable) {
 		super(msg, throwable);
 	}
 
-	public ObjectLayoutBoxCategorizationTypeException(Throwable throwable) {
+	public ObjectLayoutBoxTypeException(Throwable throwable) {
 		super(throwable);
 	}
 
