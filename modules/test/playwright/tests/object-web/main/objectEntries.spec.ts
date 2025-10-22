fa850f50dc72bf79e43de4d1be21ddb6d0287951
Jhosseph Gonzalez <jhossephgonzalez@gmail.com>
2025-10-22 15:08:58 -0300

    LPD-43541 remove FF from remaining usages of CMS dependency in tests

--- modules/test/playwright/tests/object-web/main/objectEntries.spec.ts
+++ modules/test/playwright/tests/object-web/main/objectEntries.spec.ts
@@ -80,8 +80,7 @@ const assigneeTest = test;
 const cmsTest = mergeTests(
 	test,
 	featureFlagsTest({
-		'LPD-17564': {enabled: true},
-		'LPD-32050': {enabled: true},
+		'LPD-17564': {enabled: true}
 	})
 );
 
