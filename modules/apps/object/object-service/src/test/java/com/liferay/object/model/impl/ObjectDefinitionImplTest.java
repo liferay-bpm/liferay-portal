80fd75f4ca616790e53acee85c63f0d66124ba72
Jürgen Kappler <jurgen.kappler@liferay.com>
2025-10-30 17:26:26 +0100

    LPD-70093 Adds unit test

--- modules/apps/object/object-service/src/test/java/com/liferay/object/model/impl/ObjectDefinitionImplTest.java
+++ modules/apps/object/object-service/src/test/java/com/liferay/object/model/impl/ObjectDefinitionImplTest.java
@@ -6,19 +6,26 @@
 package com.liferay.object.model.impl;
 
 import com.liferay.object.constants.ObjectDefinitionConstants;
+import com.liferay.object.constants.ObjectFolderConstants;
 import com.liferay.object.constants.ObjectPortletKeys;
 import com.liferay.object.model.ObjectDefinition;
+import com.liferay.object.model.ObjectFolder;
+import com.liferay.object.service.ObjectFolderLocalServiceUtil;
+import com.liferay.petra.function.UnsafeConsumer;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.kernel.util.PropsUtil;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.TextFormatter;
 import com.liferay.portal.test.rule.LiferayUnitTestRule;
 
+import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
 
+import org.mockito.MockedStatic;
 import org.mockito.Mockito;
 
 /**
@@ -30,6 +37,11 @@ public class ObjectDefinitionImplTest {
 	@Rule
 	public static final LiferayUnitTestRule liferayUnitTestRule =
 		LiferayUnitTestRule.INSTANCE;
+
+	@AfterClass
+	public static void tearDownClass() {
+		_objectFolderLocalServiceUtilMockedStatic.close();
+	}
 
 	@Test
 	public void testGetPortletId() {
@@ -74,6 +86,64 @@ public class ObjectDefinitionImplTest {
 		}
 	}
 
+	@Test
+	public void testIsCMS() throws Exception {
+		_assertIsCMS(
+			ObjectFolderConstants.EXTERNAL_REFERENCE_CODE_CONTENT_STRUCTURES,
+			"false", isCMS -> Assert.assertFalse(isCMS));
+		_assertIsCMS(
+			ObjectFolderConstants.EXTERNAL_REFERENCE_CODE_CONTENT_STRUCTURES,
+			"true", isCMS -> Assert.assertTrue(isCMS));
+		_assertIsCMS(
+			ObjectFolderConstants.EXTERNAL_REFERENCE_CODE_FILE_TYPES, "false",
+			isCMS -> Assert.assertFalse(isCMS));
+		_assertIsCMS(
+			ObjectFolderConstants.EXTERNAL_REFERENCE_CODE_FILE_TYPES, "true",
+			isCMS -> Assert.assertTrue(isCMS));
+		_assertIsCMS(
+			RandomTestUtil.randomString(), "true",
+			isCMS -> Assert.assertFalse(isCMS));
+	}
+
+	private void _assertIsCMS(
+			String externalReferenceCode, String featureFlagEnabled,
+			UnsafeConsumer<Boolean, Exception> unsafeConsumer)
+		throws Exception {
+
+		long objectFolderId = RandomTestUtil.randomLong();
+
+		ObjectFolder objectFolder = Mockito.mock(ObjectFolder.class);
+
+		Mockito.when(
+			objectFolder.getExternalReferenceCode()
+		).thenReturn(
+			externalReferenceCode
+		);
+
+		_objectFolderLocalServiceUtilMockedStatic.when(
+			() -> ObjectFolderLocalServiceUtil.fetchObjectFolder(objectFolderId)
+		).thenReturn(
+			objectFolder
+		);
+
+		ObjectDefinition objectDefinition = new ObjectDefinitionImpl();
+
+		objectDefinition.setObjectFolderId(objectFolderId);
+
+		String featureFlagKey = "feature.flag.LPD-17564";
+
+		String originalValue = PropsUtil.get(featureFlagKey);
+
+		try {
+			PropsUtil.set(featureFlagKey, featureFlagEnabled);
+
+			unsafeConsumer.accept(objectDefinition.isCMS());
+		}
+		finally {
+			PropsUtil.set(featureFlagKey, originalValue);
+		}
+	}
+
 	private ObjectDefinition _createObjectDefinition(
 		boolean modifiable, String name, boolean system) {
 
@@ -97,5 +167,9 @@ public class ObjectDefinitionImplTest {
 		Assert.assertEquals(
 			expectedRESTContextPath, objectDefinition.getRESTContextPath());
 	}
+
+	private static final MockedStatic<ObjectFolderLocalServiceUtil>
+		_objectFolderLocalServiceUtilMockedStatic = Mockito.mockStatic(
+			ObjectFolderLocalServiceUtil.class);
 
 }
