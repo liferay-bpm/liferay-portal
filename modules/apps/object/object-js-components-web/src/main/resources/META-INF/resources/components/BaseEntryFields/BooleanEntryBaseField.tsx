d53f149f28694ebd9427c9e29623a79d2bcf295e
Rafael Uen <rafael.uen@liferay.com>
2025-11-10 23:32:50 -0300

    LPD-49587 Implement enable default values for boolean fields in object admin

---
+++ modules/apps/object/object-js-components-web/src/main/resources/META-INF/resources/components/BaseEntryFields/BooleanEntryBaseField.tsx
@@ -0,0 +1,45 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+import React from 'react';
+
+import {SingleSelect} from '../Select/SingleSelect';
+
+interface BooleanEntryBaseFieldProps {
+	error?: string;
+	label: string;
+	onChange: (selected: string | undefined) => void;
+	placeholder?: string;
+	required?: boolean;
+	selectedBooleanItem?: string | undefined;
+}
+
+export function BooleanEntryBaseField({
+	error,
+	label,
+	onChange,
+	placeholder,
+	required,
+	selectedBooleanItem,
+}: BooleanEntryBaseFieldProps) {
+	const booleanItems = [
+		{label: Liferay.Language.get('true'), value: 'true'},
+		{label: Liferay.Language.get('false'), value: 'false'},
+	];
+
+	return (
+		<SingleSelect
+			error={error}
+			items={booleanItems}
+			label={label}
+			onSelectionChange={(value) =>
+				onChange(typeof value === 'string' ? value : undefined)
+			}
+			placeholder={placeholder}
+			required={required}
+			selectedKey={selectedBooleanItem}
+		/>
+	);
+}
