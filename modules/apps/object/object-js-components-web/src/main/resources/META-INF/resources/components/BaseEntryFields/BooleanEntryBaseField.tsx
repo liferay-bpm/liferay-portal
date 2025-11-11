2e857de304e195a27a514e7246ffa44b64a760b6
Rafael Uen <rafael.uen@liferay.com>
2025-11-10 23:32:50 -0300

    LPD-49587 Implement enable default values for boolean fields in object admin

---
+++ modules/apps/object/object-js-components-web/src/main/resources/META-INF/resources/components/BaseEntryFields/BooleanEntryBaseField.tsx
@@ -0,0 +1,50 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+import React, {useMemo} from 'react';
+
+import {SingleSelect} from '../Select/SingleSelect';
+
+interface BooleanEntryBaseFieldProps {
+	error?: string;
+	label: string;
+	onChange: (selected: string | undefined) => void;
+	placeholder?: string;
+	required?: boolean;
+	selectedBooleanItem?: string | undefined;
+}
+
+export function BooleanEntryBaseField({
+	error,
+	label,
+	onChange,
+	placeholder,
+	required,
+	selectedBooleanItem,
+}: BooleanEntryBaseFieldProps) {
+	const booleanItems = useMemo(
+		() => [
+			{label: Liferay.Language.get('true'), value: 'true'},
+			{label: Liferay.Language.get('false'), value: 'false'},
+		],
+		[]
+	);
+
+	return (
+		<SingleSelect
+			error={error}
+			items={booleanItems}
+			label={label}
+			onSelectionChange={(booleanValue) =>
+				onChange(
+					typeof booleanValue === 'string' ? booleanValue : undefined
+				)
+			}
+			placeholder={placeholder}
+			required={required}
+			selectedKey={selectedBooleanItem}
+		/>
+	);
+}
--- modules/apps/object/object-web/src/main/resources/META-INF/resources/js/components/ObjectField/Tabs/Advanced/AdvancedTab.tsx
+++ modules/apps/object/object-web/src/main/resources/META-INF/resources/js/components/ObjectField/Tabs/Advanced/AdvancedTab.tsx
@@ -47,6 +47,8 @@ export function AdvancedTab({
 		(values.businessType === 'Relationship' && isRootDescendantNode) ||
 		values.required ||
 		values.system;
+	const hasDefaultValues =
+		values.businessType === 'Boolean' || values.businessType === 'Picklist';
 
 	return (
 		<>
@@ -71,7 +73,7 @@ export function AdvancedTab({
 				</ContainerWrapper>
 			)}
 
-			{values.businessType === 'Picklist' && (
+			{hasDefaultValues && (
 				<ContainerWrapper
 					collapsable
 					defaultExpanded
