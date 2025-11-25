829198ccacf4928cc7da893c0a6ca2ce7a962b13
Yuri Monteiro <yuri.monteiro@liferay.com>
2025-11-25 14:37:05 -0300

    LPD-59106 Fixing exception usages
     # breaking
    
     ## What modules/apps/object/object-api/bnd.bnd
    
    Modifications on ObjectEntryGroupIdException to not allow relationships of object entries of different sites
    
     ## Why
    
    To not allow relationships of object entries of different sites
    
    ----

--- modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectEntryLocalServiceImpl.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectEntryLocalServiceImpl.java
@@ -6842,10 +6842,8 @@ public class ObjectEntryLocalServiceImpl
 			_objectScopeProviderRegistry.getObjectScopeProvider(scope);
 
 		if (!objectScopeProvider.isValidGroupId(groupId)) {
-			throw new ObjectEntryGroupIdException(
-				StringBundler.concat(
-					"Group ID ", groupId, " is not valid for scope \"", scope,
-					"\""));
+			throw new ObjectEntryGroupIdException.InvalidGroupIdForScope(
+				groupId, scope);
 		}
 
 		if (!StringUtil.equals(scope, ObjectDefinitionConstants.SCOPE_DEPOT) ||
