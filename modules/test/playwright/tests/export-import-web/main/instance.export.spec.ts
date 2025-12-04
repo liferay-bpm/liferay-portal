b21e24092a674465970533206d23927ff9953ba6
Pedro Leite <pedro.leite@liferay.com>
2025-12-04 09:17:22 -0300

    LPD-70252 Add integration test

--- modules/test/playwright/tests/export-import-web/main/instance.export.spec.ts
+++ modules/test/playwright/tests/export-import-web/main/instance.export.spec.ts
@@ -3,7 +3,9 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-import {ObjectDefinitionAPI} from '@liferay/object-admin-rest-client-js';
+import {
+	ObjectDefinitionAPI,
+} from '@liferay/object-admin-rest-client-js';
 import {expect, mergeTests} from '@playwright/test';
 
 import {applicationsMenuPageTest} from '../../../fixtures/applicationsMenuPageTest';
