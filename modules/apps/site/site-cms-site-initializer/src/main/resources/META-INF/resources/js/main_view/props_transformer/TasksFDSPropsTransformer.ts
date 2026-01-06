150064ba9b9c2d2ece13ccbf393251d48d0f6a55
Pedro Leite <pedro.leite@liferay.com>
2026-01-06 07:44:50 -0300

    LPD-75348 Create FDSPropsTransformer

---
+++ modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/js/main_view/props_transformer/TasksFDSPropsTransformer.ts
@@ -0,0 +1,39 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+import {IInternalRenderer} from '@liferay/frontend-data-set-web';
+
+import StateLabel from '../../common/components/StateLabel';
+import AuthorRenderer from './cell_renderers/AuthorRenderer';
+import SimpleActionLinkRenderer from './cell_renderers/SimpleActionLinkRenderer';
+
+export default function TasksFDSPropsTransformer({
+	...otherProps
+}: {
+	otherProps: any;
+}) {
+	return {
+		...otherProps,
+		customRenderers: {
+			tableCell: [
+				{
+					component: AuthorRenderer,
+					name: 'authorTableCellRenderer',
+					type: 'internal',
+				} as IInternalRenderer,
+				{
+					component: SimpleActionLinkRenderer,
+					name: 'simpleActionLinkTableCellRenderer',
+					type: 'internal',
+				} as IInternalRenderer,
+				{
+					component: ({value}) => StateLabel(value),
+					name: 'stateTableCellRenderer',
+					type: 'internal',
+				} as IInternalRenderer,
+			],
+		},
+	};
+}
