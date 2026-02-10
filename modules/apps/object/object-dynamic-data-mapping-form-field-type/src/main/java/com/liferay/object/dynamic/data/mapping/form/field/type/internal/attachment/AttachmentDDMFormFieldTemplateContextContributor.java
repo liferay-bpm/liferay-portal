93985ec3dc18fe4ce2df5b5dd56702b753645876
Alberto Sousa <alberto.sousa@liferay.com>
2026-02-10 14:46:51 -0300

    LPD-77926 Add CMS File Attachment properties to Context Contributor

--- modules/apps/object/object-dynamic-data-mapping-form-field-type/src/main/java/com/liferay/object/dynamic/data/mapping/form/field/type/internal/attachment/AttachmentDDMFormFieldTemplateContextContributor.java
+++ modules/apps/object/object-dynamic-data-mapping-form-field-type/src/main/java/com/liferay/object/dynamic/data/mapping/form/field/type/internal/attachment/AttachmentDDMFormFieldTemplateContextContributor.java
@@ -20,9 +20,11 @@ import com.liferay.object.configuration.ObjectConfiguration;
 import com.liferay.object.constants.ObjectFieldSettingConstants;
 import com.liferay.object.dynamic.data.mapping.form.field.type.constants.ObjectDDMFormFieldTypeConstants;
 import com.liferay.object.field.attachment.AttachmentManager;
+import com.liferay.object.field.setting.util.ObjectFieldSettingUtil;
 import com.liferay.object.field.util.ObjectFieldUtil;
 import com.liferay.object.model.ObjectDefinition;
 import com.liferay.object.model.ObjectEntry;
+import com.liferay.object.model.ObjectField;
 import com.liferay.object.service.ObjectDefinitionLocalService;
 import com.liferay.object.service.ObjectEntryLocalService;
 import com.liferay.object.service.ObjectEntryService;
@@ -30,11 +32,13 @@ import com.liferay.object.service.ObjectFieldLocalService;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.configuration.metatype.bnd.util.ConfigurableUtil;
 import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.feature.flag.FeatureFlagManagerUtil;
 import com.liferay.portal.kernel.json.JSONFactory;
 import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.language.Language;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.portlet.RequestBackedPortletURLFactory;
 import com.liferay.portal.kernel.portlet.RequestBackedPortletURLFactoryUtil;
 import com.liferay.portal.kernel.portlet.url.builder.PortletURLBuilder;
@@ -92,7 +96,7 @@ public class AttachmentDDMFormFieldTemplateContextContributor
 
 		DDMForm ddmForm = ddmFormField.getDDMForm();
 
-		return HashMapBuilder.<String, Object>put(
+		Map<String, Object> parameters = HashMapBuilder.<String, Object>put(
 			ObjectFieldSettingConstants.NAME_ACCEPTED_FILE_EXTENSIONS,
 			ddmFormField.getProperty(
 				ObjectFieldSettingConstants.NAME_ACCEPTED_FILE_EXTENSIONS)
@@ -160,6 +164,23 @@ public class AttachmentDDMFormFieldTemplateContextContributor
 				getLocalizationParameters(
 					ddmFormField, ddmForm.getDefaultLocale())
 		).build();
+
+		ObjectField objectField = _objectFieldLocalService.fetchObjectField(
+			GetterUtil.getLong(ddmFormField.getProperty("objectFieldId")));
+
+		if (FeatureFlagManagerUtil.isEnabled(
+				objectField.getCompanyId(), "LPD-74813")) {
+
+			parameters.put(
+				"storageDepot", _getGroupExternalReferenceCode(objectField));
+			parameters.put(
+				"storageLibraryPath",
+				ObjectFieldSettingUtil.getValue(
+					ObjectFieldSettingConstants.NAME_STORAGE_LIBRARY_PATH,
+					objectField));
+		}
+
+		return parameters;
 	}
 
 	@Activate
@@ -258,6 +279,20 @@ public class AttachmentDDMFormFieldTemplateContextContributor
 
 			return new HashMap<>();
 		}
+	}
+
+	private String _getGroupExternalReferenceCode(ObjectField objectField) {
+		String groupId = ObjectFieldSettingUtil.getValue(
+			ObjectFieldSettingConstants.NAME_STORAGE_DEPOT, objectField);
+
+		Group group = _groupLocalService.fetchGroup(
+			GetterUtil.getLong(groupId));
+
+		if (group != null) {
+			return group.getExternalReferenceCode();
+		}
+
+		return null;
 	}
 
 	private long _getGroupId(
