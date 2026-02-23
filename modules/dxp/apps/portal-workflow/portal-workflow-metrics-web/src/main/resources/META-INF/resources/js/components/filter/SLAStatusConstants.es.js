27668bf0d448147fbeb2a3f2f2d8d749a4eaca68
Julien Mourad <62948119+julienmouraddotcom@users.noreply.github.com>
2026-02-23 15:46:03 -0500

    LPD-78255 Fix circular dependency unit test

---
+++ modules/dxp/apps/portal-workflow/portal-workflow-metrics-web/src/main/resources/META-INF/resources/js/components/filter/SLAStatusConstants.es.js
@@ -0,0 +1,7 @@
+const slaStatusConstants = {
+	onTime: 'OnTime',
+	overdue: 'Overdue',
+	untracked: 'Untracked',
+};
+
+export default slaStatusConstants;
--- modules/dxp/apps/portal-workflow/portal-workflow-metrics-web/src/main/resources/META-INF/resources/js/components/filter/SLAStatusFilter.es.js
+++ modules/dxp/apps/portal-workflow/portal-workflow-metrics-web/src/main/resources/META-INF/resources/js/components/filter/SLAStatusFilter.es.js
@@ -5,16 +5,11 @@
 
 import React, {useMemo} from 'react';
 
+import slaStatusConstants from './SLAStatusConstants.es';
 import Filter from '../../shared/components/filter/Filter.es';
 import {useFilterName} from '../../shared/components/filter/hooks/useFilterName.es';
 import {useFilterStatic} from '../../shared/components/filter/hooks/useFilterStatic.es';
 import filterConstants from '../../shared/components/filter/util/filterConstants.es';
-
-const slaStatusConstants = {
-	onTime: 'OnTime',
-	overdue: 'Overdue',
-	untracked: 'Untracked',
-};
 
 const slaStatuses = [
 	{
@@ -71,5 +66,3 @@ export default function SLAStatusFilter({
 		/>
 	);
 }
-
-export {slaStatusConstants};
