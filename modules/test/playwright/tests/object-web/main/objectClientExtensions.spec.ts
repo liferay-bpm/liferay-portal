b8891dac5b25f847548ef53e844e950e44a06cc4
Victor Kammerer <vko@cin.ufpe.com>
2026-03-23 10:49:35 -0300

    LPD-80345 Remove ticket code and lower case test name

--- modules/test/playwright/tests/object-web/main/objectClientExtensions.spec.ts
+++ modules/test/playwright/tests/object-web/main/objectClientExtensions.spec.ts
@@ -343,44 +343,43 @@ test('Can trigger object validation as a client extension', async ({
 	).toBeVisible();
 });
 
-test(
-	'LPD-78504 Can trigger action with unmodifiable system object definition using client extension',
-	{tag: '@LPD-78504'},
-	async ({apiHelpers, editObjectActionPage, viewObjectActionsPage}) => {
-		await viewObjectActionsPage.goto('User');
+test('can trigger action with unmodifiable system object definition using client extension', async ({
+	apiHelpers,
+	editObjectActionPage,
+	viewObjectActionsPage,
+}) => {
+	await viewObjectActionsPage.goto('User');
 
-		await editObjectActionPage.addNewAction({
-			thenOption:
-				'object-action-executor[function#liferay-sample-etc-spring-boot-object-action-2]',
-			whenOption: 'On After Add',
-		});
+	await editObjectActionPage.addNewAction({
+		thenOption:
+			'object-action-executor[function#liferay-sample-etc-spring-boot-object-action-2]',
+		whenOption: 'On After Add',
+	});
 
-		const randomNumber = getRandomInt();
+	const randomNumber = getRandomInt();
 
-		const userAccount =
-			await apiHelpers.headlessAdminUser.postUserAccount({
-				alternateName: 'usersn' + randomNumber,
-				emailAddress: 'userea' + randomNumber + '@liferay.com',
-				familyName: 'userln' + randomNumber,
-				givenName: 'userfn' + randomNumber,
-			});
+	const userAccount = await apiHelpers.headlessAdminUser.postUserAccount({
+		alternateName: 'usersn' + randomNumber,
+		emailAddress: 'userea' + randomNumber + '@liferay.com',
+		familyName: 'userln' + randomNumber,
+		givenName: 'userfn' + randomNumber,
+	});
 
-		await expect
-			.poll(
-				async () => {
-					const updatedUser =
-						await apiHelpers.headlessAdminUser.getUserAccountByEmailAddress(
-							userAccount.emailAddress
-						);
+	await expect
+		.poll(
+			async () => {
+				const updatedUser =
+					await apiHelpers.headlessAdminUser.getUserAccountByEmailAddress(
+						userAccount.emailAddress
+					);
 
-					return updatedUser.alternateName;
-				},
-				{
-					message:
-						'Expected user alternateName to be updated to givenName by client extension action',
-					timeout: 10000,
-				}
-			)
-			.toBe('userfn' + randomNumber);
-	}
-);
+				return updatedUser.alternateName;
+			},
+			{
+				message:
+					'Expected user alternateName to be updated to givenName by client extension action',
+				timeout: 10000,
+			}
+		)
+		.toBe('userfn' + randomNumber);
+});
