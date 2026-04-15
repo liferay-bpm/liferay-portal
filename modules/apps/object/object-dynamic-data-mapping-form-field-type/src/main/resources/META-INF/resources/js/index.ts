a23cab07dd5e7b391a5b1f131217ef4fafdbff24
Kevin Tan <kevin.tan@liferay.com>
2026-04-15 13:24:38 -0700

    LPD-70691 Move frontend components to object-dynamic-data-mapping-form-field-type

--- modules/apps/object/object-dynamic-data-mapping-form-field-type/src/main/resources/META-INF/resources/js/index.ts
+++ modules/apps/object/object-dynamic-data-mapping-form-field-type/src/main/resources/META-INF/resources/js/index.ts
@@ -11,5 +11,7 @@ export {
 export {default as AssigneeAvatar} from './Assignee/Avatar';
 export {default as Attachment} from './Attachment/Attachment';
 export {default as AutoIncrement} from './AutoIncrement/AutoIncrement';
+export {default as LocalizablePhoneNumber} from './LocalizablePhoneNumber/LocalizablePhoneNumber';
 export {default as MultiselectPicklist} from './MultiselectPicklist/MultiselectPicklist';
 export {default as ObjectRelationship} from './ObjectRelationship/ObjectRelationship';
+export {default as PhoneNumber} from './PhoneNumber/PhoneNumber';
