d90869d5b696f71f5b2ddd67697664b0a0c06e2c
Guilherme Camacho <guilherme.camacho@liferay.com>
2026-07-22 16:27:11 -0300

    LPD-97811 Test the task-asset link audit events

---
+++ modules/apps/site/site-cmp-site-initializer-test/src/testIntegration/java/com/liferay/site/cmp/site/initializer/internal/model/listener/test/CMPTaskAssetRelationshipModelListenerTest.java
@@ -0,0 +1,234 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.site.cmp.site.initializer.internal.model.listener.test;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.depot.constants.DepotConstants;
+import com.liferay.depot.model.DepotEntry;
+import com.liferay.depot.service.DepotEntryLocalService;
+import com.liferay.object.model.ObjectDefinition;
+import com.liferay.object.model.ObjectEntry;
+import com.liferay.object.service.ObjectDefinitionLocalService;
+import com.liferay.object.service.ObjectEntryLocalService;
+import com.liferay.portal.kernel.audit.AuditMessage;
+import com.liferay.portal.kernel.audit.AuditRouter;
+import com.liferay.portal.kernel.json.JSONUtil;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.model.ModelListener;
+import com.liferay.portal.kernel.service.GroupLocalService;
+import com.liferay.portal.kernel.service.ServiceContextThreadLocal;
+import com.liferay.portal.kernel.test.ReflectionTestUtil;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.HashMapBuilder;
+import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.util.ProxyUtil;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.test.rule.FeatureFlag;
+import com.liferay.portal.test.rule.FeatureFlags;
+import com.liferay.portal.test.rule.Inject;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+import com.liferay.portal.test.rule.PermissionCheckerMethodTestRule;
+import com.liferay.site.cmp.site.initializer.test.util.CMPTestUtil;
+
+import java.io.Serializable;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.skyscreamer.jsonassert.JSONAssert;
+import org.skyscreamer.jsonassert.JSONCompareMode;
+
+/**
+ * @author Guilherme Camacho
+ */
+@FeatureFlags(
+	featureFlags = {@FeatureFlag("LPD-17564"), @FeatureFlag("LPD-58677")}
+)
+@RunWith(Arquillian.class)
+public class CMPTaskAssetRelationshipModelListenerTest {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new AggregateTestRule(
+			new LiferayIntegrationTestRule(),
+			PermissionCheckerMethodTestRule.INSTANCE);
+
+	@Before
+	public void setUp() throws Exception {
+		_auditRouter = (AuditRouter)ReflectionTestUtil.getAndSetFieldValue(
+			_modelListener, "_auditRouter",
+			ProxyUtil.newProxyInstance(
+				AuditRouter.class.getClassLoader(),
+				new Class<?>[] {AuditRouter.class},
+				(proxy, method, arguments) -> {
+					_auditMessages.add((AuditMessage)arguments[0]);
+
+					return null;
+				}));
+
+		CMPTestUtil.getOrAddGroup(
+			CMPTaskAssetRelationshipModelListenerTest.class);
+
+		_taskObjectEntry = CMPTestUtil.addTaskObjectEntry();
+
+		ServiceContextThreadLocal.pushServiceContext(
+			ServiceContextTestUtil.getServiceContext());
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		ReflectionTestUtil.setFieldValue(
+			_modelListener, "_auditRouter", _auditRouter);
+
+		_objectEntryLocalService.deleteObjectEntry(_taskObjectEntry);
+
+		ServiceContextThreadLocal.popServiceContext();
+	}
+
+	@Test
+	public void testOnAfterCreate() throws Exception {
+		String title = RandomTestUtil.randomString();
+
+		_addTaskAssetRelationshipObjectEntry(_addAssetObjectEntry(title));
+
+		_assertAuditMessage("CMP_ADD_ASSET", title);
+	}
+
+	@Test
+	public void testOnAfterRemove() throws Exception {
+		String title = RandomTestUtil.randomString();
+
+		ObjectEntry taskAssetRelationshipObjectEntry =
+			_addTaskAssetRelationshipObjectEntry(_addAssetObjectEntry(title));
+
+		_assertAuditMessage("CMP_ADD_ASSET", title);
+
+		_objectEntryLocalService.deleteObjectEntry(
+			taskAssetRelationshipObjectEntry);
+
+		_assertAuditMessage("CMP_REMOVE_ASSET", title);
+	}
+
+	private ObjectEntry _addAssetObjectEntry(String title) throws Exception {
+		DepotEntry depotEntry = _depotEntryLocalService.addDepotEntry(
+			HashMapBuilder.put(
+				LocaleUtil.getDefault(), StringUtil.randomString()
+			).build(),
+			HashMapBuilder.put(
+				LocaleUtil.getDefault(), StringUtil.randomString()
+			).build(),
+			DepotConstants.TYPE_SPACE,
+			ServiceContextTestUtil.getServiceContext());
+
+		ObjectDefinition objectDefinition =
+			_objectDefinitionLocalService.
+				getObjectDefinitionByExternalReferenceCode(
+					"L_CMS_BASIC_WEB_CONTENT", depotEntry.getCompanyId());
+
+		return _objectEntryLocalService.addObjectEntry(
+			depotEntry.getGroupId(), depotEntry.getUserId(),
+			objectDefinition.getObjectDefinitionId(), 0, "en_US",
+			HashMapBuilder.<String, Serializable>put(
+				"title_i18n",
+				HashMapBuilder.put(
+					"en_US", title
+				).build()
+			).build(),
+			ServiceContextTestUtil.getServiceContext());
+	}
+
+	private ObjectEntry _addTaskAssetRelationshipObjectEntry(
+			ObjectEntry assetObjectEntry)
+		throws Exception {
+
+		ObjectDefinition assetObjectDefinition =
+			_objectDefinitionLocalService.getObjectDefinition(
+				assetObjectEntry.getObjectDefinitionId());
+
+		Group group = _groupLocalService.getGroup(
+			assetObjectEntry.getGroupId());
+
+		ObjectDefinition objectDefinition =
+			_objectDefinitionLocalService.
+				getObjectDefinitionByExternalReferenceCode(
+					"L_CMP_TASK_ASSET_RELATIONSHIP",
+					_taskObjectEntry.getCompanyId());
+
+		return _objectEntryLocalService.addObjectEntry(
+			_taskObjectEntry.getGroupId(), _taskObjectEntry.getUserId(),
+			objectDefinition.getObjectDefinitionId(), 0, null,
+			HashMapBuilder.<String, Serializable>put(
+				"classExternalReferenceCode",
+				assetObjectEntry.getExternalReferenceCode()
+			).put(
+				"className", assetObjectDefinition.getClassName()
+			).put(
+				"groupExternalReferenceCode", group.getExternalReferenceCode()
+			).put(
+				"r_cmpTaskToCMPTaskAssetRelationships_c_cmpTaskId",
+				_taskObjectEntry.getObjectEntryId()
+			).build(),
+			ServiceContextTestUtil.getServiceContext());
+	}
+
+	private void _assertAuditMessage(
+			String expectedEventType, String expectedTitle)
+		throws Exception {
+
+		AuditMessage auditMessage = _auditMessages.poll();
+
+		JSONAssert.assertEquals(
+			JSONUtil.put(
+				"attributes",
+				JSONUtil.putAll(JSONUtil.put("name", expectedTitle))
+			).toString(),
+			String.valueOf(auditMessage.getAdditionalInfo()),
+			JSONCompareMode.STRICT_ORDER);
+		Assert.assertEquals(
+			_taskObjectEntry.getModelClassName(), auditMessage.getClassName());
+		Assert.assertEquals(
+			_taskObjectEntry.getObjectEntryId(),
+			GetterUtil.getLong(auditMessage.getClassPK()));
+		Assert.assertEquals(expectedEventType, auditMessage.getEventType());
+
+		_auditMessages.clear();
+	}
+
+	private final Queue<AuditMessage> _auditMessages = new LinkedList<>();
+	private AuditRouter _auditRouter;
+
+	@Inject
+	private DepotEntryLocalService _depotEntryLocalService;
+
+	@Inject
+	private GroupLocalService _groupLocalService;
+
+	@Inject(
+		filter = "component.name=com.liferay.site.cms.site.initializer.internal.model.listener.CMPTaskAssetRelationshipModelListener"
+	)
+	private ModelListener<ObjectEntry> _modelListener;
+
+	@Inject
+	private ObjectDefinitionLocalService _objectDefinitionLocalService;
+
+	@Inject
+	private ObjectEntryLocalService _objectEntryLocalService;
+
+	private ObjectEntry _taskObjectEntry;
+
+}
